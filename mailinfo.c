53be6733f58a20ebf1713f5a594bbb87d011e62a
Linus Torvalds <torvalds@ppc970.osdl.org>
2005-04-12 01:40:20 -0700

    Add "applypatch" and "dotest" scripts to tie it all together.
    This should be getting it all pretty close to a working setup.
    
    
    Former-commit-id: 853916ff7f1a5b34bd2728fe81059e6270bac134

--- mailinfo.c
+++ mailinfo.c
@@ -30,6 +30,7 @@ end_include
 begin_decl_stmt
 DECL|variable|cmitmsg
 DECL|variable|patchfile
+DECL|variable|filelist
 specifier|static
 name|FILE
 modifier|*
@@ -37,6 +38,9 @@ name|cmitmsg
 decl_stmt|,
 modifier|*
 name|patchfile
+decl_stmt|,
+modifier|*
+name|filelist
 decl_stmt|;
 end_decl_stmt
 
@@ -889,6 +893,7 @@ case|case
 literal|'\n'
 case|:
 break|break;
+comment|/* patch tends to special-case these things.. */
 case|case
 literal|'~'
 case|:
@@ -927,9 +932,11 @@ operator|!
 name|len
 condition|)
 return|return;
-name|printf
+name|fprintf
 argument_list|(
-literal|"filename: %.*s\n"
+name|filelist
+argument_list|,
+literal|"%.*s\n"
 argument_list|,
 name|len
 argument_list|,
@@ -1258,7 +1265,7 @@ name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
-literal|"mailinfo msg-file path-file< email\n"
+literal|"mailinfo msg-file path-file filelist-file< email\n"
 argument_list|)
 expr_stmt|;
 name|exit
@@ -1287,7 +1294,7 @@ if|if
 condition|(
 name|argc
 operator|!=
-literal|3
+literal|4
 condition|)
 name|usage
 argument_list|()
@@ -1347,6 +1354,38 @@ argument_list|(
 name|argv
 index|[
 literal|2
+index|]
+argument_list|)
+expr_stmt|;
+name|exit
+argument_list|(
+literal|1
+argument_list|)
+expr_stmt|;
+block|}
+name|filelist
+operator|=
+name|fopen
+argument_list|(
+name|argv
+index|[
+literal|3
+index|]
+argument_list|,
+literal|"w"
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+operator|!
+name|filelist
+condition|)
+block|{
+name|perror
+argument_list|(
+name|argv
+index|[
+literal|3
 index|]
 argument_list|)
 expr_stmt|;
