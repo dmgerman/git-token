60dcbe93291012fa4b81149f9c79d16dc74ec9f2
Linus Torvalds <torvalds@ppc970.osdl.org>
2005-04-21 11:21:33 -0700

    Include <limits.h> in commit.c for ULONG_MAX. Remove old "revision.h".
    The old revision.h helper header isn't used any more, but I never noticed
    it until I started grepping for ULONG_MAX users.
    
    
    Former-commit-id: b2c007186786c39693d42d18a1e491aa2e7defce

--- commit.c
+++ commit.c
@@ -17,6 +17,12 @@ directive|include
 file|<string.h>
 end_include
 
+begin_include
+include|#
+directive|include
+file|<limits.h>
+end_include
+
 begin_decl_stmt
 DECL|variable|commit_type
 specifier|const
