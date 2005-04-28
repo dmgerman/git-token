ce8ca58587723454e5a19580e3f499c33db3eaeb
Daniel Barkalow <barkalow@iabervon.org>
2005-04-28 07:46:33 -0700

    [PATCH] Mark blobs as parsed when they're actually parsed
    This eliminates the special case for blobs versus other types of
    objects. Now the scheme is entirely regular and I won't introduce stupid
    bugs. (And fsck-cache doesn't have to do the do-nothing parse)
    
    Signed-Off-By: Daniel Barkalow <barkalow@iabervon.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>
    
    Former-commit-id: a510bfaa8c001413f363e0d0b9c6cf15dbcbb7b3

--- blob.h
+++ blob.h
@@ -55,6 +55,18 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_function_decl
+name|int
+name|parse_blob
+parameter_list|(
+name|struct
+name|blob
+modifier|*
+name|item
+parameter_list|)
+function_decl|;
+end_function_decl
+
 begin_endif
 endif|#
 directive|endif
