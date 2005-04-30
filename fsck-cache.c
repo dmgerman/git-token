159355d6587693473b7fa50a253dcfb08e4d7b17
Linus Torvalds <torvalds@ppc970.osdl.org>
2005-04-30 09:59:31 -0700

    Fix up d_type handling - we need to include <dirent.h> before we play with the d_type compatibility macros.
    Former-commit-id: 4b18242190c40653549ec0663eea211bd87fcaed

--- fsck-cache.c
+++ fsck-cache.c
@@ -2,12 +2,6 @@ begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_include
 include|#
 directive|include
-file|"cache.h"
-end_include
-
-begin_include
-include|#
-directive|include
 file|<sys/types.h>
 end_include
 
@@ -15,6 +9,12 @@ begin_include
 include|#
 directive|include
 file|<dirent.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|"cache.h"
 end_include
 
 begin_include
