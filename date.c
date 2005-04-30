9b974495daaf5b9ab1b81ba73549ca3b7e133be0
Linus Torvalds <torvalds@ppc970.osdl.org>
2005-04-30 15:21:57 -0700

    date.c: only use the TZ names if we don't have anything better.
    Also, add EEST (hey, it's Finland).
    
    
    Former-commit-id: 92e2311b6c10bf1e4f3e1231d26a6e71d81c5f47

--- date.c
+++ date.c
@@ -596,6 +596,16 @@ block|, }
 block|,
 comment|/* Eastern Europe, USSR Zone 1 */
 block|{
+literal|"EEST"
+block|,
+operator|+
+literal|2
+block|,
+literal|1
+block|, }
+block|,
+comment|/* Eastern European Daylight */
+block|{
 literal|"WAST"
 block|,
 operator|+
@@ -976,6 +986,15 @@ index|]
 operator|.
 name|dst
 expr_stmt|;
+comment|/* Only use the tz name offset if we don't have anything better */
+if|if
+condition|(
+operator|*
+name|offset
+operator|==
+operator|-
+literal|1
+condition|)
 operator|*
 name|offset
 operator|=
