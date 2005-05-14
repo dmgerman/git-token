d071633cd53318dc6d9fada00edf5b66634fdbbd
Junio C Hamano <junkio@cox.net>
2005-05-13 18:41:36 -0700

    [PATCH 3/3] Rename git-diff-tree-helper to git-diff-helper (part 2).
    It used to be that diff-tree needed helper support to parse its
    raw output to generate diffs, but these days git-diff-* family
    produces the same output and the helper is not tied to diff-tree
    anymore.  Drop "tree" from its name.
    
    This follows the "rename only" commit to adjust the contents of
    the files involved.
    
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    Signed-off-by: Petr Baudis <pasky@ucw.cz>
    
    Former-commit-id: 902b92e00e491a60d55c4b2bce122903b8347f34

--- diff-helper.c
+++ diff-helper.c
@@ -273,10 +273,10 @@ block|}
 end_function
 
 begin_function
-DECL|function|parse_diff_tree_output
+DECL|function|parse_diff_raw_output
 specifier|static
 name|int
-name|parse_diff_tree_output
+name|parse_diff_raw_output
 parameter_list|(
 specifier|const
 name|char
@@ -680,14 +680,14 @@ block|}
 end_function
 
 begin_decl_stmt
-DECL|variable|diff_tree_helper_usage
+DECL|variable|diff_helper_usage
 specifier|static
 specifier|const
 name|char
 modifier|*
-name|diff_tree_helper_usage
+name|diff_helper_usage
 init|=
-literal|"diff-tree-helper [-R] [-z] paths..."
+literal|"git-diff-helper [-R] [-z] paths..."
 decl_stmt|;
 end_decl_stmt
 
@@ -779,7 +779,7 @@ expr_stmt|;
 else|else
 name|usage
 argument_list|(
-name|diff_tree_helper_usage
+name|diff_helper_usage
 argument_list|)
 expr_stmt|;
 name|ac
@@ -817,7 +817,7 @@ condition|)
 break|break;
 name|status
 operator|=
-name|parse_diff_tree_output
+name|parse_diff_raw_output
 argument_list|(
 name|sb
 operator|.
