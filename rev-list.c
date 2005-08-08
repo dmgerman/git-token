2ca7667349a161cfe7a0949c54f40da5312d8b0b
Johannes Schindelin <Johannes.Schindelin@gmx.de>
2005-08-08 11:37:21 +0200

    [PATCH] add *--no-merges* flag to suppress display of merge commits
    As requested by Junio (who suggested --single-parents-only, but this
    could forget a no-parent root).
    
    Also, adds a few missing options to the usage string.
    
    Signed-off-by: Johannes Schindelin <Johannes.Schindelin@gmx.de>
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    
    Former-commit-id: 76cd8eb619cf2896b5171d46d6bbf8e97d19e728

--- rev-list.c
+++ rev-list.c
@@ -79,12 +79,15 @@ literal|"git-rev-list [OPTION] commit-id<commit-id>\n"
 literal|"  --max-count=nr\n"
 literal|"  --max-age=epoch\n"
 literal|"  --min-age=epoch\n"
+literal|"  --parents\n"
 literal|"  --bisect\n"
 literal|"  --objects\n"
 literal|"  --unpacked\n"
 literal|"  --header\n"
 literal|"  --pretty\n"
-literal|"  --merge-order [ --show-breaks ]"
+literal|"  --no-merges\n"
+literal|"  --merge-order [ --show-breaks ]\n"
+literal|"  --topo-order"
 decl_stmt|;
 end_decl_stmt
 
@@ -261,6 +264,16 @@ DECL|variable|topo_order
 specifier|static
 name|int
 name|topo_order
+init|=
+literal|0
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+DECL|variable|no_merges
+specifier|static
+name|int
+name|no_merges
 init|=
 literal|0
 decl_stmt|;
@@ -547,6 +560,25 @@ operator|--
 condition|)
 return|return
 name|STOP
+return|;
+if|if
+condition|(
+name|no_merges
+operator|&&
+operator|(
+name|commit
+operator|->
+name|parents
+operator|&&
+name|commit
+operator|->
+name|parents
+operator|->
+name|next
+operator|)
+condition|)
+return|return
+name|CONTINUE
 return|;
 return|return
 name|DO
@@ -2550,6 +2582,25 @@ expr_stmt|;
 name|prefix
 operator|=
 literal|"commit "
+expr_stmt|;
+continue|continue;
+block|}
+if|if
+condition|(
+operator|!
+name|strncmp
+argument_list|(
+name|arg
+argument_list|,
+literal|"--no-merges"
+argument_list|,
+literal|11
+argument_list|)
+condition|)
+block|{
+name|no_merges
+operator|=
+literal|1
 expr_stmt|;
 continue|continue;
 block|}
