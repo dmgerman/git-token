96fbccf1df97ae754a5fca4711c1ba58c64706d1
James Bottomley <James.Bottomley@SteelEye.com>
2005-08-13 10:29:32 -0500

    [PATCH] fix git-checkout-cache segfault on parisc
    The index cleanup code is executed via atexit() which is *after* main
    has completed, so the stack allocated cache_file has gone out of scope.
    Parisc seems to use stack in the destructor functions, so cache_file
    gets partially overwritten leading to the predictable disastrous
    consequences.
    
    [jc: Just to make sure, I audited other users of the function
     hold_index_file_for_update() to make sure they do not have this
     problem; everybody else uses non-stack cache_file structure and
     is fine.  Thanks, James.]
    
    Signed-off-by: James Bottomley <James.Bottomley@SteelEye.com>
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    
    Former-commit-id: 31f584c242e7af28018ff920b6c8d1952beadbd4

--- checkout-cache.c
+++ checkout-cache.c
@@ -229,6 +229,15 @@ literal|"git-checkout-cache [-u] [-q] [-a] [-f] [-n] [--prefix=<string>] [--]<fi
 decl_stmt|;
 end_decl_stmt
 
+begin_decl_stmt
+DECL|variable|cache_file
+specifier|static
+name|struct
+name|cache_file
+name|cache_file
+decl_stmt|;
+end_decl_stmt
+
 begin_function
 DECL|function|main
 name|int
@@ -249,10 +258,6 @@ decl_stmt|,
 name|force_filename
 init|=
 literal|0
-decl_stmt|;
-name|struct
-name|cache_file
-name|cache_file
 decl_stmt|;
 name|int
 name|newfd
