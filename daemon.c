4dec7d8833c014845ac6445de86169fbe853343d
Randal L. Schwartz <merlyn@stonehenge.com>
2005-10-25 16:29:09 -0700

    fix daemon.c to compile on OpenBSD
    I can confirm that the following patch lets the current origin
    compile on OpenBSD.  If you could apply this until you sort out the
    rest of the namespace issue, I would be happy.  Thanks.
    
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    
    Former-commit-id: 979e32fa1483a32faa4ec331e29b357e5eb5ef25

--- daemon.c
+++ daemon.c
@@ -2,18 +2,6 @@ begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_include
 include|#
 directive|include
-file|"cache.h"
-end_include
-
-begin_include
-include|#
-directive|include
-file|"pkt-line.h"
-end_include
-
-begin_include
-include|#
-directive|include
 file|<signal.h>
 end_include
 
@@ -63,6 +51,18 @@ begin_include
 include|#
 directive|include
 file|<syslog.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|"pkt-line.h"
+end_include
+
+begin_include
+include|#
+directive|include
+file|"cache.h"
 end_include
 
 begin_decl_stmt
