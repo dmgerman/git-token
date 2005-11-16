b19c2743090f9515d608b433f9b5d93f8fd531ef
Junio C Hamano <junkio@cox.net>
2005-11-16 14:12:56 -0800

    git-apply: fail if a patch cannot be applied.
    Recently we fixed 'git-apply --stat' not to barf on a binary
    differences.  But it accidentally broke the error detection when
    we actually attempt to apply them.
    
    This commit fixes the problem and adds test cases.
    
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    
    Former-commit-id: 92927ed0aac56a86f85049215791fcd106af4b62

--- apply.c
+++ apply.c
@@ -4438,12 +4438,6 @@ if|if
 condition|(
 operator|!
 name|patchsize
-operator|&&
-operator|!
-name|metadata_changes
-argument_list|(
-name|patch
-argument_list|)
 condition|)
 block|{
 specifier|static
@@ -4494,20 +4488,27 @@ name|is_binary
 operator|=
 literal|1
 expr_stmt|;
+comment|/* Empty patch cannot be applied if: 		 * - it is a binary patch or 		 * - metadata does not change and is not a binary patch. 		 */
 if|if
 condition|(
+operator|(
+name|apply
+operator|||
+name|check
+operator|)
+operator|&&
+operator|(
 name|patch
 operator|->
 name|is_binary
-operator|&&
+operator|||
 operator|!
-name|apply
-operator|&&
-operator|!
-name|check
+name|metadata_changes
+argument_list|(
+name|patch
+argument_list|)
+operator|)
 condition|)
-empty_stmt|;
-else|else
 name|die
 argument_list|(
 literal|"patch with only garbage at line %d"
