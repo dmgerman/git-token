bdf0a3f2f7446cf2ecb3ae796a1d7c8c76fa73fc
Linus Torvalds <torvalds@osdl.org>
2005-11-21 12:38:31 -0800

    git-repack: Properly abort in corrupt repository
    In a corrupt repository, git-repack produces a pack that does not
    contain needed objects without complaining, and the result of this
    combined with -d flag can be very painful -- e.g. a lossage of one
    tree object can lead to lossage of blobs reachable only through that
    tree.
    
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    
    Former-commit-id: ef07618fdd660057b80fe75007405c62502a5820

--- pack-objects.c
+++ pack-objects.c
@@ -2995,7 +2995,9 @@ argument_list|)
 condition|)
 name|die
 argument_list|(
-literal|"expected sha1, got garbage"
+literal|"expected sha1, got garbage:\n %s"
+argument_list|,
+name|line
 argument_list|)
 expr_stmt|;
 name|hash
