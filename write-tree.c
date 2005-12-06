e6ee7da82a984c2005ece98f80a2cd1fe31bcff9
Junio C Hamano <junkio@cox.net>
2005-12-05 22:30:07 -0800

    write-tree: check extra arguments and die but be a bit more helpful.
    "git-write-tree junk" complains and dies, but it does not say
    what option it supports.  Die with the usage string in such a
    case.
    
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    
    Former-commit-id: 75a46f6b0ed3c3aa6fe4ff53a8c20cb58cbb3732

--- write-tree.c
+++ write-tree.c
@@ -411,6 +411,18 @@ return|;
 block|}
 end_function
 
+begin_decl_stmt
+DECL|variable|write_tree_usage
+specifier|static
+specifier|const
+name|char
+name|write_tree_usage
+index|[]
+init|=
+literal|"git-write-tree [--missing-ok]"
+decl_stmt|;
+end_decl_stmt
+
 begin_function
 DECL|function|main
 name|int
@@ -475,12 +487,7 @@ expr_stmt|;
 else|else
 name|die
 argument_list|(
-literal|"unknown option %s"
-argument_list|,
-name|argv
-index|[
-literal|1
-index|]
+name|write_tree_usage
 argument_list|)
 expr_stmt|;
 block|}
