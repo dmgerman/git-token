7abcb44ad8936ee0cc04f008d06ad1d053c09508
Alex Riesen <raa.lkml@gmail.com>
2005-12-15 08:47:30 +0100

    \n usage in stderr output
    fprintf and die sometimes have missing/excessive "\n" in their arguments,
    correct the strings where I think it would be appropriate.
    
    Signed-off-by: Alex Riesen <raa.lkml@gmail.com>
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    
    Former-commit-id: 7246ed438c541650c2cd50e4e98b43226f60da0c

--- sha1_file.c
+++ sha1_file.c
@@ -6518,7 +6518,7 @@ name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
-literal|"unable to write sha1 filename %s: %s"
+literal|"unable to write sha1 filename %s: %s\n"
 argument_list|,
 name|filename
 argument_list|,
@@ -6685,7 +6685,7 @@ name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
-literal|"sha1 file %s: %s"
+literal|"sha1 file %s: %s\n"
 argument_list|,
 name|filename
 argument_list|,
@@ -6733,7 +6733,7 @@ name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
-literal|"unable to create temporary sha1 filename %s: %s"
+literal|"unable to create temporary sha1 filename %s: %s\n"
 argument_list|,
 name|tmpfile
 argument_list|,
@@ -7192,7 +7192,7 @@ name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
-literal|"write closed"
+literal|"write closed\n"
 argument_list|)
 expr_stmt|;
 block|}
