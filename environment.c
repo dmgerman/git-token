32b9aa01b1ba763427db95c232846bfa5e0ba34b
Johannes Schindelin <Johannes.Schindelin@gmx.de>
2005-12-22 23:13:56 +0100

    Introduce core.sharedrepository
    If the config variable 'core.sharedrepository' is set, the directories
    
    	$GIT_DIR/objects/
    	$GIT_DIR/objects/??
    	$GIT_DIR/objects/pack
    	$GIT_DIR/refs
    	$GIT_DIR/refs/heads
    	$GIT_DIR/refs/heads/tags
    
    are set group writable (and g+s, since the git group may be not the primary
    group of all users).
    
    Since all files are written as lock files first, and then moved to
    their destination, they do not have to be group writable.  Indeed, if
    this leads to problems you found a bug.
    
    Note that -- as in my first attempt -- the config variable is set in the
    function which checks the repository format. If this were done in
    git_default_config instead, a lot of programs would need to be modified
    to call git_config(git_default_config) first.
    
    [jc: git variables should be in environment.c unless there is a
     compelling reason to do otherwise.]
    
    Signed-off-by: Johannes Schindelin <Johannes.Schindelin@gmx.de>
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    
    Former-commit-id: 457f06d68e427bbf4f1a921877441a622a05e5c4

--- environment.c
+++ environment.c
@@ -69,6 +69,15 @@ decl_stmt|;
 end_decl_stmt
 
 begin_decl_stmt
+DECL|variable|shared_repository
+name|int
+name|shared_repository
+init|=
+literal|0
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
 DECL|variable|git_dir
 DECL|variable|git_object_dir
 DECL|variable|git_index_file
