08e616fc9994fc4a3d417f5ad27ab294082b9706
Jason Riedy <ejr@EECS.Berkeley.EDU>
2006-01-25 12:38:36 -0800

    Add compat/unsetenv.c .
    Implement a (slow) unsetenv() for older systems.
    
    Signed-off-by: Jason Riedy <ejr@cs.berkeley.edu>
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    
    Former-commit-id: 731043fd4d55b1561ebb5a53daa4b9ff6dfac046

--- connect.c
+++ connect.c
@@ -2,6 +2,12 @@ begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_include
 include|#
 directive|include
+file|"git-compat-util.h"
+end_include
+
+begin_include
+include|#
+directive|include
 file|"cache.h"
 end_include
 
