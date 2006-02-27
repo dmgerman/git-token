cdbdccd54232cbd18f2cfb0dd2b322f325035aec
Junio C Hamano <junkio@cox.net>
2006-02-27 14:47:45 -0800

    apply --whitespace: configuration option.
    The new configuration option apply.whitespace can take one of
    "warn", "error", "error-all", or "strip".  When git-apply is run
    to apply the patch to the index, they are used as the default
    value if there is no command line --whitespace option.
    
    Andrew can now tell people who feed him git trees to update to
    this version and say:
    
    	git repo-config apply.whitespace error
    
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    
    Former-commit-id: 383e20b6144d01286c90c0c82d88f92173a038f6

--- cache.h
+++ cache.h
@@ -1042,6 +1042,10 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_comment
+comment|/* Environment bits from configuration mechanism */
+end_comment
+
 begin_decl_stmt
 specifier|extern
 name|int
@@ -1067,6 +1071,15 @@ begin_decl_stmt
 specifier|extern
 name|int
 name|shared_repository
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+specifier|extern
+specifier|const
+name|char
+modifier|*
+name|apply_default_whitespace
 decl_stmt|;
 end_decl_stmt
 
