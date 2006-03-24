cbdc776ded1e385d6a2e61acfd29027e7d9a8117
Junio C Hamano <junkio@cox.net>
2006-03-23 23:41:18 -0800

    sha1_name: warning ambiguous refs.
    This makes sure that many commands that take refs on the command
    line to honor core.warnambiguousrefs configuration.  Earlier,
    the commands affected by this patch did not read the
    configuration file.
    
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    
    Former-commit-id: 84a9b58c421f9b2d1cc6c195ed441fac48e60392

--- unpack-file.c
+++ unpack-file.c
@@ -175,6 +175,11 @@ expr_stmt|;
 name|setup_git_directory
 argument_list|()
 expr_stmt|;
+name|git_config
+argument_list|(
+name|git_default_config
+argument_list|)
+expr_stmt|;
 name|puts
 argument_list|(
 name|create_temp_file
