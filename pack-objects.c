3c58c038160901bd226e942927f81324d807be44
Junio C Hamano <junkio@cox.net>
2006-04-03 23:41:09 -0700

    safe_fgets() - even more anal fgets()
    This is from Linus -- the previous round forgot to clear error
    after EINTR case.
    
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    
    Former-commit-id: 687dd75c95f9212244b6cf4fe60b40db44de01ba

--- pack-objects.c
+++ pack-objects.c
@@ -4807,10 +4807,9 @@ expr_stmt|;
 if|if
 condition|(
 name|errno
-operator|==
+operator|!=
 name|EINTR
 condition|)
-continue|continue;
 name|die
 argument_list|(
 literal|"fgets: %s"
@@ -4821,6 +4820,12 @@ name|errno
 argument_list|)
 argument_list|)
 expr_stmt|;
+name|clearerr
+argument_list|(
+name|stdin
+argument_list|)
+expr_stmt|;
+continue|continue;
 block|}
 if|if
 condition|(
