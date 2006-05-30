1261ae1861580b81bda4dd7472973401f5461e01
Dmitry V. Levin <ldv@altlinux.org>
2006-05-30 18:58:52 +0400

    execv_git_cmd: Fix stack buffer overflow.
    Signed-off-by: Dmitry V. Levin <ldv@altlinux.org>
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    
    Former-commit-id: d6859901012535b3781e5332923ede51d5977395

--- exec_cmd.c
+++ exec_cmd.c
@@ -139,8 +139,6 @@ literal|1
 index|]
 decl_stmt|;
 name|int
-name|len
-decl_stmt|,
 name|i
 decl_stmt|;
 specifier|const
@@ -177,6 +175,12 @@ operator|++
 name|i
 control|)
 block|{
+name|size_t
+name|len
+decl_stmt|;
+name|int
+name|rc
+decl_stmt|;
 specifier|const
 name|char
 modifier|*
@@ -225,14 +229,15 @@ argument_list|(
 name|stderr
 argument_list|,
 literal|"git: cannot determine "
-literal|"current directory\n"
-argument_list|)
-expr_stmt|;
-name|exit
+literal|"current directory: %s\n"
+argument_list|,
+name|strerror
 argument_list|(
-literal|1
+name|errno
+argument_list|)
 argument_list|)
 expr_stmt|;
+break|break;
 block|}
 name|len
 operator|=
@@ -270,6 +275,8 @@ name|exec_dir
 operator|++
 expr_stmt|;
 block|}
+name|rc
+operator|=
 name|snprintf
 argument_list|(
 name|git_command
@@ -288,9 +295,60 @@ argument_list|,
 name|exec_dir
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
+name|rc
+operator|<
+literal|0
+operator|||
+name|rc
+operator|>=
+sizeof|sizeof
+argument_list|(
+name|git_command
+argument_list|)
+operator|-
+name|len
+condition|)
+block|{
+name|fprintf
+argument_list|(
+name|stderr
+argument_list|,
+literal|"git: command name given "
+literal|"is too long.\n"
+argument_list|)
+expr_stmt|;
+break|break;
+block|}
 block|}
 else|else
 block|{
+if|if
+condition|(
+name|strlen
+argument_list|(
+name|exec_dir
+argument_list|)
+operator|+
+literal|1
+operator|>
+sizeof|sizeof
+argument_list|(
+name|git_command
+argument_list|)
+condition|)
+block|{
+name|fprintf
+argument_list|(
+name|stderr
+argument_list|,
+literal|"git: command name given "
+literal|"is too long.\n"
+argument_list|)
+expr_stmt|;
+break|break;
+block|}
 name|strcpy
 argument_list|(
 name|git_command
@@ -306,8 +364,8 @@ argument_list|(
 name|git_command
 argument_list|)
 expr_stmt|;
-name|len
-operator|+=
+name|rc
+operator|=
 name|snprintf
 argument_list|(
 name|git_command
@@ -331,11 +389,17 @@ argument_list|)
 expr_stmt|;
 if|if
 condition|(
+name|rc
+operator|<
+literal|0
+operator|||
+name|rc
+operator|>=
 sizeof|sizeof
 argument_list|(
 name|git_command
 argument_list|)
-operator|<=
+operator|-
 name|len
 condition|)
 block|{
