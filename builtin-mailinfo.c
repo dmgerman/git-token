0ee1ed809284c3548d299649564596206740bd65
Michael S. Tsirkin <mst@mellanox.co.il>
2006-07-27 17:03:43 +0300

    mailinfo: accept >From in message header
    Mail I get sometimes has multiple From lines, like this:
    
        From Majordomo@vger.kernel.org  Thu Jul 27 16:39:36 2006
        >From mtsirkin  Thu Jul 27 16:39:36 2006
        Received: from yok.mtl.com [10.0.8.11]
        ...
    
    which confuses git-mailinfo since that does not recognize >From
    as a valid header line.
    
    This patch makes it recognize >From XXX as a valid header line.
    
    Signed-off-by: Michael S. Tsirkin <mst@mellanox.co.il>
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    
    Former-commit-id: c2c487cf3a6c37bc9b4e3f0e10ad08d9ce048404

--- builtin-mailinfo.c
+++ builtin-mailinfo.c
@@ -2303,6 +2303,7 @@ condition|(
 operator|!
 name|ofs
 operator|&&
+operator|(
 operator|!
 name|memcmp
 argument_list|(
@@ -2312,6 +2313,17 @@ literal|"From "
 argument_list|,
 literal|5
 argument_list|)
+operator|||
+operator|!
+name|memcmp
+argument_list|(
+name|line
+argument_list|,
+literal|">From "
+argument_list|,
+literal|6
+argument_list|)
+operator|)
 condition|)
 name|ofs
 operator|=
