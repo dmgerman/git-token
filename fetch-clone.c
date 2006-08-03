dda6a87ec0f752025338f6e8a9fe28319a37dca6
Junio C Hamano <junkio@cox.net>
2006-08-03 16:01:58 -0700

    sideband: do not use color, just say "remote:"
    ... per suggestion by Andrew.
    
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    
    Former-commit-id: 2de196fec1feba8350f272c567989595901bb67e

--- fetch-clone.c
+++ fetch-clone.c
@@ -703,6 +703,15 @@ name|safe_write
 argument_list|(
 literal|2
 argument_list|,
+literal|"remote: "
+argument_list|,
+literal|8
+argument_list|)
+expr_stmt|;
+name|safe_write
+argument_list|(
+literal|2
+argument_list|,
 name|buf
 operator|+
 literal|1
@@ -710,11 +719,13 @@ argument_list|,
 name|len
 argument_list|)
 expr_stmt|;
-name|fprintf
+name|safe_write
 argument_list|(
-name|stderr
+literal|2
 argument_list|,
 literal|"\n"
+argument_list|,
+literal|1
 argument_list|)
 expr_stmt|;
 name|exit
@@ -725,14 +736,13 @@ expr_stmt|;
 case|case
 literal|2
 case|:
-comment|/* color sideband */
 name|safe_write
 argument_list|(
 literal|2
 argument_list|,
-literal|"\033[44;37;1m"
+literal|"remote: "
 argument_list|,
-literal|10
+literal|8
 argument_list|)
 expr_stmt|;
 name|safe_write
@@ -744,15 +754,6 @@ operator|+
 literal|1
 argument_list|,
 name|len
-argument_list|)
-expr_stmt|;
-name|safe_write
-argument_list|(
-literal|2
-argument_list|,
-literal|"\033[m"
-argument_list|,
-literal|3
 argument_list|)
 expr_stmt|;
 continue|continue;
