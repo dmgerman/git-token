cfadb507a071139473897bb927afa0a13e8f9e27
Junio C Hamano <junkio@cox.net>
2006-08-03 21:55:41 -0700

    Further clean-up: usage() vs die()
    This hopefully finishes the clean-up Ramsay started with recent
    commit c85a9ba1d46e0e1fce18b69e0e3d70e0e9c5af93 [formerly 15e593e4d37d1d350fef20ab666d58f6881c7f5f] and commit
    bd3629f2531a1ad16916f6020e743178aa361ba7 [formerly 8cdf33643dc0b21d9ea922a3fdd7f64226c421aa].
    
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    
    Former-commit-id: cba05fa840b637db2708014142a753680d930082

--- builtin-rm.c
+++ builtin-rm.c
@@ -632,7 +632,7 @@ argument_list|)
 condition|)
 name|die
 argument_list|(
-literal|"git rm: unable to remove %s"
+literal|"git-rm: unable to remove %s"
 argument_list|,
 name|path
 argument_list|)
@@ -713,7 +713,7 @@ name|removed
 condition|)
 name|die
 argument_list|(
-literal|"git rm: %s: %s"
+literal|"git-rm: %s: %s"
 argument_list|,
 name|path
 argument_list|,
