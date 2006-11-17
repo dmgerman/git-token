51861c723b915f8ac808b78ba1a4135df61b46a5
Linus Torvalds <torvalds@osdl.org>
2006-11-16 22:57:20 -0800

    "git fmt-merge-msg" SIGSEGV
    Ok, this is a _really_ stupid case, and I don't think it matters, but hey,
    we should never SIGSEGV.
    
    Steps to reproduce:
    
    	mkdir duh
    	cd duh
    	git init-db
    	git-fmt-merge-msg < /dev/null
    
    will cause a SIGSEGV in cmd_fmt_merge_msg(), because we're doing a
    strncmp() with a NULL current_branch.
    
    And yeah, it's an insane schenario, and no, it doesn't really matter. The
    only reason I noticed was that a broken version of my "git pull" into an
    empty directory would cause this.
    
    This silly patch just replaces the SIGSEGV with a controlled exit with an
    error message.
    
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    
    Former-commit-id: 6b1f8c32b1a602734f14b9cf8a9f59a767752677

--- builtin-fmt-merge-msg.c
+++ builtin-fmt-merge-msg.c
@@ -1781,6 +1781,16 @@ expr_stmt|;
 if|if
 condition|(
 operator|!
+name|current_branch
+condition|)
+name|die
+argument_list|(
+literal|"No current branch"
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+operator|!
 name|strncmp
 argument_list|(
 name|current_branch
