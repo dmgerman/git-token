d93a9a06fb99b697f3853a25d552732fe063db72
Junio C Hamano <junkio@cox.net>
2006-12-17 18:53:24 -0800

    show-ref: fix --quiet --verify
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    
    Former-commit-id: dd9142993c0954e2fd325431235bc7556189a01c

--- builtin-show-ref.c
+++ builtin-show-ref.c
@@ -1227,6 +1227,12 @@ argument_list|,
 name|NULL
 argument_list|)
 condition|)
+block|{
+if|if
+condition|(
+operator|!
+name|quiet
+condition|)
 name|printf
 argument_list|(
 literal|"%s %s\n"
@@ -1240,6 +1246,7 @@ operator|*
 name|pattern
 argument_list|)
 expr_stmt|;
+block|}
 elseif|else
 if|if
 condition|(
