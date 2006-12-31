5d8dc8a98fb9ef3dd1b34b31deba397332c91ec7
Shawn O. Pearce <spearce@spearce.org>
2006-12-30 23:28:53 -0500

    Use PATH_MAX constant for --bare.
    For easier portability we prefer PATH_MAX over seemingly random
    constants like 1024.  Make it so.
    
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    
    Former-commit-id: ef5ddb2fe0cdb3bd09acb7229512b6ae9193423f

--- git.c
+++ git.c
@@ -393,7 +393,9 @@ specifier|static
 name|char
 name|git_dir
 index|[
-literal|1024
+name|PATH_MAX
+operator|+
+literal|1
 index|]
 decl_stmt|;
 name|setenv
@@ -404,7 +406,10 @@ name|getcwd
 argument_list|(
 name|git_dir
 argument_list|,
-literal|1024
+sizeof|sizeof
+argument_list|(
+name|git_dir
+argument_list|)
 argument_list|)
 argument_list|,
 literal|1
