c2e4fab4aac01b4a5438380ba370b4008783c5ad
Junio C Hamano <junkio@cox.net>
2007-01-02 11:22:08 -0800

    fetch-pack: do not use lockfile structure on stack.
    They are used in atexit() for clean-up, and you will be
    accessing unallocated memory at that point.
    
    See 31f584c2 for the fix for a similar problem.
    
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    
    Former-commit-id: 54b9e0225ad150b4528d7a12f2196ad46f058302

--- fetch-pack.c
+++ fetch-pack.c
@@ -3336,6 +3336,15 @@ return|;
 block|}
 end_function
 
+begin_decl_stmt
+DECL|variable|lock
+specifier|static
+name|struct
+name|lock_file
+name|lock
+decl_stmt|;
+end_decl_stmt
+
 begin_function
 DECL|function|main
 name|int
@@ -3379,10 +3388,6 @@ decl_stmt|;
 name|struct
 name|stat
 name|st
-decl_stmt|;
-name|struct
-name|lock_file
-name|lock
 decl_stmt|;
 name|setup_git_directory
 argument_list|()
