6bdfa7ff4deaaf9008a802076d0f7e5377ceb73c
Shawn O. Pearce <spearce@spearce.org>
2007-02-01 15:52:27 -0500

    Correct comment in prepare_packed_git_one.
    After staring at the comment and the associated for loop, I
    realized the comment was completely bogus.  The section of
    code its talking about is trying to avoid duplicate mapping
    of the same packfile.
    
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    
    Former-commit-id: 54a15a8df269315adf7110a87391bd5bef7e90f0

--- sha1_file.c
+++ sha1_file.c
@@ -4726,7 +4726,7 @@ literal|".idx"
 argument_list|)
 condition|)
 continue|continue;
-comment|/* we have .idx.  Is it a file we can map? */
+comment|/* Don't reopen a pack we already have. */
 name|strcpy
 argument_list|(
 name|path
@@ -4778,6 +4778,7 @@ condition|(
 name|p
 condition|)
 continue|continue;
+comment|/* See if it really is a valid .idx file with corresponding 		 * .pack file that we can map. 		 */
 name|p
 operator|=
 name|add_packed_git
