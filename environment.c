56bd745aca0e2f7c2a850b55db13561f3abd4329
Linus Torvalds <torvalds@linux-foundation.org>
2007-02-13 18:16:12 -0800

    Make AutoCRLF ternary variable.
    This allows you to do:
    
    	[core]
    		AutoCRLF = input
    
    and it should do only the CRLF->LF translation (ie it simplifies CRLF only
    when reading working tree files, but when checking out files, it leaves
    the LF alone, and doesn't turn it into a CRLF).
    
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    
    Former-commit-id: d7f4633405acf3dc09798a759463c616c7c49dfd

--- environment.c
+++ environment.c
@@ -193,6 +193,11 @@ literal|0
 decl_stmt|;
 end_decl_stmt
 
+begin_comment
+DECL|variable|auto_crlf
+comment|/* 1: both ways, -1: only when adding git objects */
+end_comment
+
 begin_decl_stmt
 DECL|variable|git_dir
 specifier|static
