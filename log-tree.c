68a10cc15f3c99731e4adf5d593c57f10eabb1b8
Linus Torvalds <torvalds@linux-foundation.org>
2007-05-05 15:18:03 -0700

    Fix --boundary output
    "git log --boundary" incorrectly honoured the option only when
    "left-right" was enabled.
    
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    
    Former-commit-id: e330a406cd20cf45a6816929155181c37678bc47

--- log-tree.c
+++ log-tree.c
@@ -1060,13 +1060,6 @@ argument_list|)
 expr_stmt|;
 if|if
 condition|(
-name|opt
-operator|->
-name|left_right
-condition|)
-block|{
-if|if
-condition|(
 name|commit
 operator|->
 name|object
@@ -1081,6 +1074,13 @@ literal|'-'
 argument_list|)
 expr_stmt|;
 elseif|else
+if|if
+condition|(
+name|opt
+operator|->
+name|left_right
+condition|)
+block|{
 if|if
 condition|(
 name|commit
