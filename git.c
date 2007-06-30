427649595d1b21d88ea5d74df481c8253010cf54
Linus Torvalds <torvalds@linux-foundation.org>
2007-06-30 11:44:20 -0700

    git: Try a bit harder not to lose errno in stdio
    This switches the checks around upon the exit codepath of the
    git wrapper, so that we may recover at least non-transient errors.
    
    It's still not perfect. As I've been harping on, stdio simply isn't very
    good for error reporting. For example, if an IO error happened, you'd want
    to see EIO, wouldn't you? And yes, that's what the kernel would return.
    However, with buffered stdio (and flushing outside of our control), what
    would likely happen is that some intermediate error return _does_ return
    EIO, but then the kernel might decide to re-mount the filesystem read-only
    due to the error, and the actual *report* for us might be
    
    	"write failure on standard output: read-only filesystem"
    
    which lost the EIO.
    
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 0227f9887bcc158dcd22ac4f60e5e428b259dd2d

--- git.c
+++ git.c
@@ -1381,24 +1381,7 @@ return|;
 comment|/* Check for ENOSPC and EIO errors.. */
 if|if
 condition|(
-name|ferror
-argument_list|(
-name|stdout
-argument_list|)
-condition|)
-name|die
-argument_list|(
-literal|"write failure on standard output"
-argument_list|)
-expr_stmt|;
-if|if
-condition|(
 name|fflush
-argument_list|(
-name|stdout
-argument_list|)
-operator|||
-name|fclose
 argument_list|(
 name|stdout
 argument_list|)
@@ -1406,6 +1389,35 @@ condition|)
 name|die
 argument_list|(
 literal|"write failure on standard output: %s"
+argument_list|,
+name|strerror
+argument_list|(
+name|errno
+argument_list|)
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|ferror
+argument_list|(
+name|stdout
+argument_list|)
+condition|)
+name|die
+argument_list|(
+literal|"unknown write failure on standard output"
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|fclose
+argument_list|(
+name|stdout
+argument_list|)
+condition|)
+name|die
+argument_list|(
+literal|"close failed on standard output: %s"
 argument_list|,
 name|strerror
 argument_list|(
