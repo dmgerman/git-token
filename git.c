04af6237c4384d1841b43896eb283504ae3d4412
Carlos Rica <jasampler@gmail.com>
2007-07-20 01:42:28 +0200

    Make git tag a builtin.
    This replaces the script "git-tag.sh" with "builtin-tag.c".
    
    The existing test suite for "git tag" guarantees the compatibility
    with the features provided by the script version.
    
    There are some minor changes in the behaviour of "git tag" here:
    "git tag -v" now can get more than one tag to verify, like "git tag -d" does,
    "git tag" with no arguments prints all tags, more like "git branch" does,
    and "git tag -n" also prints all tags with annotations (without needing -l).
    Tests and documentation were also updated to reflect these changes.
    
    The program is currently calling the script "git verify-tag" for verify.
    This can be changed porting it to C and calling its functions directly
    from builtin-tag.c.
    
    Signed-off-by: Carlos Rica <jasampler@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 62e09ce998dd7f6b844deb650101c743a5c4ce50

--- git.c
+++ git.c
@@ -2211,6 +2211,14 @@ name|RUN_SETUP
 block|}
 block|,
 block|{
+literal|"tag"
+block|,
+name|cmd_tag
+block|,
+name|RUN_SETUP
+block|}
+block|,
+block|{
 literal|"tar-tree"
 block|,
 name|cmd_tar_tree
