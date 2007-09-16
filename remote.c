4ee89eba58beaf50354e44e6b6256fb93d33c816
Shawn O. Pearce <spearce@spearce.org>
2007-09-16 02:32:11 -0400

    Don't configure remote "." to fetch everything to itself
    When we are talking about a remote URI of "." we are really talking
    about *this* repository that we are fetching into or pushing out of.
    There are no matching tracking branches for this repository; we
    do not attempt to map a ref back to ourselves as this would either
    create an infinite cycle (for example "fetch = +refs/*:refs/mine/*")
    or it causes problems when we attempt to push back to ourselves.
    
    So we really cannot setup a remote like this:
    
      [remote "."]
        url = .
        fetch = +refs/*:refs/*
    
    In the case of `git push . B:T` to fast-forward branch T to B's
    current commit git-send-pack will update branch T to B, assuming that
    T is the remote tracking branch for B.  This update is performed
    immediately before git-send-pack asks git-receive-pack to perform
    the same update, and git-receive-pack then fails because T is not
    where git-send-pack told it to expect T to be at.
    
    In the case of `git fetch .` we really should do the same thing as
    `git fetch $otherrepo`, that is load .git/FETCH_HEAD with the commit
    of HEAD, so that `git pull .` will report "Already up-to-date".
    We have always behaved like this before on this insane request and
    we should at least continue to behave the same way.  With the above
    (bad) remote configuration we were instead getting fetch errors
    about funny refs, e.g. "refs/stash".
    
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: ad23603c3af77b47f65382ea22b30d1ecccaab6f

--- remote.c
+++ remote.c
@@ -2213,26 +2213,6 @@ condition|)
 return|return
 name|NULL
 return|;
-if|if
-condition|(
-operator|!
-name|strcmp
-argument_list|(
-name|name
-argument_list|,
-literal|"."
-argument_list|)
-condition|)
-block|{
-comment|// we always fetch "refs/*:refs/*", which is trivial
-name|add_fetch_refspec
-argument_list|(
-name|ret
-argument_list|,
-literal|"refs/*:refs/*"
-argument_list|)
-expr_stmt|;
-block|}
 name|ret
 operator|->
 name|fetch
