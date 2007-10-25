b8ea82d43038b05d76e768c45bc2bfb3d216d93f
Linus Torvalds <torvalds@linux-foundation.org>
2007-10-25 11:19:10 -0700

    Ref-count the filespecs used by diffcore
    Rather than copy the filespecs when introducing new versions of them
    (for rename or copy detection), use a refcount and increment the count
    when reusing the diff_filespec.
    
    This avoids unnecessary allocations, but the real reason behind this is
    a future enhancement: we will want to track shared data across the
    copy/rename detection.  In order to efficiently notice when a filespec
    is used by a rename, the rename machinery wants to keep track of a
    rename usage count which is shared across all different users of the
    filespec.
    
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 9fb88419ba85e641006c80db53620423f37f1c93

--- diffcore.h
+++ diffcore.h
@@ -123,6 +123,11 @@ name|unsigned
 name|long
 name|size
 decl_stmt|;
+DECL|member|count
+name|int
+name|count
+decl_stmt|;
+comment|/* Reference count */
 DECL|member|xfrm_flags
 name|int
 name|xfrm_flags
@@ -189,6 +194,18 @@ name|alloc_filespec
 parameter_list|(
 specifier|const
 name|char
+modifier|*
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
+specifier|extern
+name|void
+name|free_filespec
+parameter_list|(
+name|struct
+name|diff_filespec
 modifier|*
 parameter_list|)
 function_decl|;
