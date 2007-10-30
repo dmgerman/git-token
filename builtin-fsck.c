dmgerman/git-token

commit 143c167ee66a3a60f9604f0e0a2ba844c6cc249d

Make builtin-fsck.c use parse_options.
Signed-off-by: Pierre Habouzit <madcoder@debian.org>
Signed-off-by: Shawn O. Pearce <spearce@spearce.org>

Former-commit-id: 5ac0a2063e8f824f6e8ffb4d18de74c55aae7131

builtin-fsck.c
@@ -59,6 +59,12 @@ directive|include
 file|"tree-walk.h"
 end_include
 
+begin_include
+include|#
+directive|include
+file|"parse-options.h"
+end_include
+
 begin_define
 DECL|macro|REACHABLE
 define|#
@@ -3222,13 +3228,135 @@ end_function
 begin_decl_stmt
 DECL|variable|fsck_usage
 specifier|static
-specifier|const
 name|char
+specifier|const
+modifier|*
+specifier|const
 name|fsck_usage
 index|[]
 init|=
-literal|"git-fsck [--tags] [--root] [[--unreachable] [--cache] [--full] "
-literal|"[--strict] [--verbose]<head-sha1>*]"
+block|{
+literal|"git-fsck [options] [<object>...]"
+block|,
+name|NULL
+block|}
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+DECL|variable|fsck_opts
+specifier|static
+name|struct
+name|option
+name|fsck_opts
+index|[]
+init|=
+block|{
+name|OPT__VERBOSE
+argument_list|(
+operator|&
+name|verbose
+argument_list|)
+block|,
+name|OPT_BOOLEAN
+argument_list|(
+literal|0
+argument_list|,
+literal|"unreachable"
+argument_list|,
+operator|&
+name|show_unreachable
+argument_list|,
+literal|"show unreachable objects"
+argument_list|)
+block|,
+name|OPT_BOOLEAN
+argument_list|(
+literal|0
+argument_list|,
+literal|"tags"
+argument_list|,
+operator|&
+name|show_tags
+argument_list|,
+literal|"report tags"
+argument_list|)
+block|,
+name|OPT_BOOLEAN
+argument_list|(
+literal|0
+argument_list|,
+literal|"root"
+argument_list|,
+operator|&
+name|show_root
+argument_list|,
+literal|"report root nodes"
+argument_list|)
+block|,
+name|OPT_BOOLEAN
+argument_list|(
+literal|0
+argument_list|,
+literal|"cache"
+argument_list|,
+operator|&
+name|keep_cache_objects
+argument_list|,
+literal|"make index objects head nodes"
+argument_list|)
+block|,
+name|OPT_BOOLEAN
+argument_list|(
+literal|0
+argument_list|,
+literal|"reflogs"
+argument_list|,
+operator|&
+name|include_reflogs
+argument_list|,
+literal|"make reflogs head nodes (default)"
+argument_list|)
+block|,
+name|OPT_BOOLEAN
+argument_list|(
+literal|0
+argument_list|,
+literal|"full"
+argument_list|,
+operator|&
+name|check_full
+argument_list|,
+literal|"also consider alternate objects"
+argument_list|)
+block|,
+name|OPT_BOOLEAN
+argument_list|(
+literal|0
+argument_list|,
+literal|"struct"
+argument_list|,
+operator|&
+name|check_strict
+argument_list|,
+literal|"enable more strict checking"
+argument_list|)
+block|,
+name|OPT_BOOLEAN
+argument_list|(
+literal|0
+argument_list|,
+literal|"lost-found"
+argument_list|,
+operator|&
+name|write_lost_and_found
+argument_list|,
+literal|"write dangling objects in .git/lost-found"
+argument_list|)
+block|,
+name|OPT_END
+argument_list|()
+block|, }
 decl_stmt|;
 end_decl_stmt
 
@@ -3265,202 +3393,33 @@ name|errors_found
 operator|=
 literal|0
 expr_stmt|;
-for|for
-control|(
-name|i
-operator|=
-literal|1
-init|;
-name|i
-operator|<
 name|argc
-condition|;
-name|i
-operator|++
-control|)
-block|{
-specifier|const
-name|char
-modifier|*
-name|arg
-init|=
+operator|=
+name|parse_options
+argument_list|(
+name|argc
+argument_list|,
 name|argv
-index|[
-name|i
-index|]
-decl_stmt|;
-if|if
-condition|(
-operator|!
-name|strcmp
-argument_list|(
-name|arg
 argument_list|,
-literal|"--unreachable"
-argument_list|)
-condition|)
-block|{
-name|show_unreachable
-operator|=
-literal|1
-expr_stmt|;
-continue|continue;
-block|}
-if|if
-condition|(
-operator|!
-name|strcmp
-argument_list|(
-name|arg
+name|fsck_opts
 argument_list|,
-literal|"--tags"
-argument_list|)
-condition|)
-block|{
-name|show_tags
-operator|=
-literal|1
-expr_stmt|;
-continue|continue;
-block|}
-if|if
-condition|(
-operator|!
-name|strcmp
-argument_list|(
-name|arg
-argument_list|,
-literal|"--root"
-argument_list|)
-condition|)
-block|{
-name|show_root
-operator|=
-literal|1
-expr_stmt|;
-continue|continue;
-block|}
-if|if
-condition|(
-operator|!
-name|strcmp
-argument_list|(
-name|arg
-argument_list|,
-literal|"--cache"
-argument_list|)
-condition|)
-block|{
-name|keep_cache_objects
-operator|=
-literal|1
-expr_stmt|;
-continue|continue;
-block|}
-if|if
-condition|(
-operator|!
-name|strcmp
-argument_list|(
-name|arg
-argument_list|,
-literal|"--no-reflogs"
-argument_list|)
-condition|)
-block|{
-name|include_reflogs
-operator|=
-literal|0
-expr_stmt|;
-continue|continue;
-block|}
-if|if
-condition|(
-operator|!
-name|strcmp
-argument_list|(
-name|arg
-argument_list|,
-literal|"--full"
-argument_list|)
-condition|)
-block|{
-name|check_full
-operator|=
-literal|1
-expr_stmt|;
-continue|continue;
-block|}
-if|if
-condition|(
-operator|!
-name|strcmp
-argument_list|(
-name|arg
-argument_list|,
-literal|"--strict"
-argument_list|)
-condition|)
-block|{
-name|check_strict
-operator|=
-literal|1
-expr_stmt|;
-continue|continue;
-block|}
-if|if
-condition|(
-operator|!
-name|strcmp
-argument_list|(
-name|arg
-argument_list|,
-literal|"--verbose"
-argument_list|)
-condition|)
-block|{
-name|verbose
-operator|=
-literal|1
-expr_stmt|;
-continue|continue;
-block|}
-if|if
-condition|(
-operator|!
-name|strcmp
-argument_list|(
-name|arg
-argument_list|,
-literal|"--lost-found"
-argument_list|)
-condition|)
-block|{
-name|check_full
-operator|=
-literal|1
-expr_stmt|;
-name|include_reflogs
-operator|=
-literal|0
-expr_stmt|;
-name|write_lost_and_found
-operator|=
-literal|1
-expr_stmt|;
-continue|continue;
-block|}
-if|if
-condition|(
-operator|*
-name|arg
-operator|==
-literal|'-'
-condition|)
-name|usage
-argument_list|(
 name|fsck_usage
+argument_list|,
+literal|0
 argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|write_lost_and_found
+condition|)
+block|{
+name|check_full
+operator|=
+literal|1
+expr_stmt|;
+name|include_reflogs
+operator|=
+literal|0
 expr_stmt|;
 block|}
 name|fsck_head_link
@@ -3589,7 +3548,7 @@ name|next
 control|)
 block|{
 name|uint32_t
-name|i
+name|j
 decl_stmt|,
 name|num
 decl_stmt|;
@@ -3609,15 +3568,15 @@ name|num_objects
 expr_stmt|;
 for|for
 control|(
-name|i
+name|j
 operator|=
 literal|0
 init|;
-name|i
+name|j
 operator|<
 name|num
 condition|;
-name|i
+name|j
 operator|++
 control|)
 name|fsck_sha1
@@ -3626,7 +3585,7 @@ name|nth_packed_object_sha1
 argument_list|(
 name|p
 argument_list|,
-name|i
+name|j
 argument_list|)
 argument_list|)
 expr_stmt|;
@@ -3660,14 +3619,6 @@ index|[
 name|i
 index|]
 decl_stmt|;
-if|if
-condition|(
-operator|*
-name|arg
-operator|==
-literal|'-'
-condition|)
-continue|continue;
 if|if
 condition|(
 operator|!
@@ -3742,9 +3693,6 @@ condition|(
 name|keep_cache_objects
 condition|)
 block|{
-name|int
-name|i
-decl_stmt|;
 name|read_cache
 argument_list|()
 expr_stmt|;

commit 27dfdda29fad3133d43cd55d95c23dbd03b41669

Fixed a command line option type for builtin-fsck.c
The typo was introduced by 143c167ee66a3a60f9604f0e0a2ba844c6cc249d [formerly 5ac0a2063e8f824f6e8ffb4d18de74c55aae7131]
(Make builtin-fsck.c use parse_options.)

Signed-off-by: Emil Medve <Emilian.Medve@Freescale.com>
Acked-by: Pierre Habouzit <madcoder@debian.org>
Signed-off-by: Junio C Hamano <gitster@pobox.com>

Former-commit-id: dd46b9b95b853328e7ff1c11b7f5cae68751c79e

builtin-fsck.c
@@ -3334,7 +3334,7 @@ name|OPT_BOOLEAN
 argument_list|(
 literal|0
 argument_list|,
-literal|"struct"
+literal|"strict"
 argument_list|,
 operator|&
 name|check_strict