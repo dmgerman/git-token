aed3a86f21c4f05693390447e66c3e5ad1123346
Steven Grimm <koreth@midwinter.com>
2007-11-03 19:26:54 -0700

    builtin-fetch: Add "-q" as a synonym for "--quiet"
    "-q" is the very first option described in the git-fetch manpage, and it
    isn't supported.
    
    Signed-off-by: Steven Grimm <koreth@midwinter.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 4b7bbdd14c6b926fbb0dbd203ce02e150b308694

--- builtin-fetch.c
+++ builtin-fetch.c
@@ -3010,6 +3010,14 @@ name|arg
 argument_list|,
 literal|"--quiet"
 argument_list|)
+operator|||
+operator|!
+name|strcmp
+argument_list|(
+name|arg
+argument_list|,
+literal|"-q"
+argument_list|)
 condition|)
 block|{
 name|quiet
