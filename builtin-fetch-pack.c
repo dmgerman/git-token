dmgerman/git-token

commit d471c2e00ad2a04ee6edde7b7ccc38db1ce41f25

Always obtain fetch-pack arguments from struct fetch_pack_args
Copying the arguments from a fetch_pack_args into static globals
within the builtin-fetch-pack module is error-prone and may lead
rise to cases where arguments supplied via the struct from the
new fetch_pack() API may not be honored by the implementation.

Here we reorganize all of the static globals into a single static
struct fetch_pack_args instance and use memcpy() to move the data
from the caller supplied structure into the globals before we
execute our pack fetching implementation.  This strategy is more
robust to additions and deletions of properties.

As keep_pack is a single bit we have also introduced lock_pack to
mean not only download and store the packfile via index-pack but
also to lock it against repacking by creating a .keep file when
the packfile itself is stored.  The caller must remove the .keep
file when it is safe to do so.

Signed-off-by: Shawn O. Pearce <spearce@spearce.org>
Signed-off-by: Junio C Hamano <gitster@pobox.com>

Former-commit-id: fa74052922cf39e5a39ad7178d1b13c2da9b4519

builtin-fetch-pack.c
@@ -54,14 +54,6 @@ file|"fetch-pack.h"
 end_include
 
 begin_decl_stmt
-DECL|variable|keep_pack
-specifier|static
-name|int
-name|keep_pack
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
 DECL|variable|transfer_unpack_limit
 specifier|static
 name|int
@@ -94,42 +86,11 @@ decl_stmt|;
 end_decl_stmt
 
 begin_decl_stmt
-DECL|variable|quiet
+DECL|variable|args
 specifier|static
-name|int
-name|quiet
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-DECL|variable|verbose
-specifier|static
-name|int
-name|verbose
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-DECL|variable|fetch_all
-specifier|static
-name|int
-name|fetch_all
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-DECL|variable|depth
-specifier|static
-name|int
-name|depth
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-DECL|variable|no_progress
-specifier|static
-name|int
-name|no_progress
+name|struct
+name|fetch_pack_args
+name|args
 decl_stmt|;
 end_decl_stmt
 
@@ -933,6 +894,8 @@ literal|""
 operator|)
 argument_list|,
 operator|(
+name|args
+operator|.
 name|no_progress
 condition|?
 literal|" no-progress"
@@ -980,6 +943,8 @@ argument_list|)
 expr_stmt|;
 if|if
 condition|(
+name|args
+operator|.
 name|depth
 operator|>
 literal|0
@@ -993,6 +958,8 @@ index|]
 argument_list|,
 literal|"deepen %d"
 argument_list|,
+name|args
+operator|.
 name|depth
 argument_list|)
 expr_stmt|;
@@ -1014,6 +981,8 @@ literal|1
 return|;
 if|if
 condition|(
+name|args
+operator|.
 name|depth
 operator|>
 literal|0
@@ -1204,6 +1173,8 @@ argument_list|)
 expr_stmt|;
 if|if
 condition|(
+name|args
+operator|.
 name|verbose
 condition|)
 name|fprintf
@@ -1270,6 +1241,8 @@ argument_list|)
 expr_stmt|;
 if|if
 condition|(
+name|args
+operator|.
 name|verbose
 operator|&&
 name|ack
@@ -1371,6 +1344,8 @@ condition|)
 block|{
 if|if
 condition|(
+name|args
+operator|.
 name|verbose
 condition|)
 name|fprintf
@@ -1399,6 +1374,8 @@ argument_list|)
 expr_stmt|;
 if|if
 condition|(
+name|args
+operator|.
 name|verbose
 condition|)
 name|fprintf
@@ -1450,6 +1427,8 @@ condition|)
 block|{
 if|if
 condition|(
+name|args
+operator|.
 name|verbose
 condition|)
 name|fprintf
@@ -1658,6 +1637,8 @@ condition|)
 block|{
 if|if
 condition|(
+name|args
+operator|.
 name|verbose
 condition|)
 name|fprintf
@@ -1754,6 +1735,8 @@ condition|(
 name|nr_match
 operator|&&
 operator|!
+name|args
+operator|.
 name|fetch_all
 condition|)
 block|{
@@ -1857,10 +1840,14 @@ comment|/* trash */
 elseif|else
 if|if
 condition|(
+name|args
+operator|.
 name|fetch_all
 operator|&&
 operator|(
 operator|!
+name|args
+operator|.
 name|depth
 operator|||
 name|prefixcmp
@@ -1937,6 +1924,8 @@ block|}
 if|if
 condition|(
 operator|!
+name|args
+operator|.
 name|fetch_all
 condition|)
 block|{
@@ -2132,6 +2121,8 @@ block|}
 if|if
 condition|(
 operator|!
+name|args
+operator|.
 name|depth
 condition|)
 block|{
@@ -2331,6 +2322,8 @@ expr_stmt|;
 if|if
 condition|(
 operator|!
+name|args
+operator|.
 name|verbose
 condition|)
 continue|continue;
@@ -2364,6 +2357,8 @@ expr_stmt|;
 if|if
 condition|(
 operator|!
+name|args
+operator|.
 name|verbose
 condition|)
 continue|continue;
@@ -2631,6 +2626,8 @@ decl_stmt|;
 name|int
 name|do_keep
 init|=
+name|args
+operator|.
 name|keep_pack
 decl_stmt|;
 name|int
@@ -2659,6 +2656,11 @@ literal|0
 expr_stmt|;
 if|if
 condition|(
+operator|!
+name|args
+operator|.
+name|keep_pack
+operator|&&
 name|unpack_limit
 condition|)
 block|{
@@ -2770,9 +2772,13 @@ expr_stmt|;
 if|if
 condition|(
 operator|!
+name|args
+operator|.
 name|quiet
 operator|&&
 operator|!
+name|args
+operator|.
 name|no_progress
 condition|)
 operator|*
@@ -2783,6 +2789,8 @@ literal|"-v"
 expr_stmt|;
 if|if
 condition|(
+name|args
+operator|.
 name|use_thin_pack
 condition|)
 operator|*
@@ -2793,9 +2801,9 @@ literal|"--fix-thin"
 expr_stmt|;
 if|if
 condition|(
-name|keep_pack
-operator|>
-literal|1
+name|args
+operator|.
+name|lock_pack
 operator|||
 name|unpack_limit
 condition|)
@@ -2856,6 +2864,8 @@ literal|"unpack-objects"
 expr_stmt|;
 if|if
 condition|(
+name|args
+operator|.
 name|quiet
 condition|)
 operator|*
@@ -3236,6 +3246,8 @@ condition|)
 block|{
 if|if
 condition|(
+name|args
+operator|.
 name|verbose
 condition|)
 name|fprintf
@@ -3260,6 +3272,8 @@ condition|)
 block|{
 if|if
 condition|(
+name|args
+operator|.
 name|verbose
 condition|)
 name|fprintf
@@ -3285,6 +3299,8 @@ condition|)
 block|{
 if|if
 condition|(
+name|args
+operator|.
 name|verbose
 condition|)
 name|fprintf
@@ -3359,9 +3375,10 @@ literal|0
 condition|)
 if|if
 condition|(
+operator|!
+name|args
+operator|.
 name|keep_pack
-operator|!=
-literal|1
 condition|)
 comment|/* When cloning, it is not unusual to have 			 * no common commit. 			 */
 name|fprintf
@@ -3586,92 +3603,6 @@ decl_stmt|;
 end_decl_stmt
 
 begin_function
-DECL|function|setup_fetch_pack
-name|void
-name|setup_fetch_pack
-parameter_list|(
-name|struct
-name|fetch_pack_args
-modifier|*
-name|args
-parameter_list|)
-block|{
-name|uploadpack
-operator|=
-name|args
-operator|->
-name|uploadpack
-expr_stmt|;
-name|quiet
-operator|=
-name|args
-operator|->
-name|quiet
-expr_stmt|;
-name|keep_pack
-operator|=
-name|args
-operator|->
-name|keep_pack
-expr_stmt|;
-if|if
-condition|(
-name|args
-operator|->
-name|unpacklimit
-operator|>=
-literal|0
-condition|)
-name|unpack_limit
-operator|=
-name|args
-operator|->
-name|unpacklimit
-expr_stmt|;
-if|if
-condition|(
-name|args
-operator|->
-name|keep_pack
-condition|)
-name|unpack_limit
-operator|=
-literal|0
-expr_stmt|;
-name|use_thin_pack
-operator|=
-name|args
-operator|->
-name|use_thin_pack
-expr_stmt|;
-name|fetch_all
-operator|=
-name|args
-operator|->
-name|fetch_all
-expr_stmt|;
-name|verbose
-operator|=
-name|args
-operator|->
-name|verbose
-expr_stmt|;
-name|depth
-operator|=
-name|args
-operator|->
-name|depth
-expr_stmt|;
-name|no_progress
-operator|=
-name|args
-operator|->
-name|no_progress
-expr_stmt|;
-block|}
-end_function
-
-begin_function
 DECL|function|cmd_fetch_pack
 name|int
 name|cmd_fetch_pack
@@ -3790,6 +3721,8 @@ literal|"--upload-pack="
 argument_list|)
 condition|)
 block|{
+name|args
+operator|.
 name|uploadpack
 operator|=
 name|arg
@@ -3809,6 +3742,8 @@ literal|"--exec="
 argument_list|)
 condition|)
 block|{
+name|args
+operator|.
 name|uploadpack
 operator|=
 name|arg
@@ -3836,6 +3771,8 @@ name|arg
 argument_list|)
 condition|)
 block|{
+name|args
+operator|.
 name|quiet
 operator|=
 literal|1
@@ -3861,12 +3798,19 @@ name|arg
 argument_list|)
 condition|)
 block|{
-name|keep_pack
-operator|++
-expr_stmt|;
-name|unpack_limit
+name|args
+operator|.
+name|lock_pack
 operator|=
-literal|0
+name|args
+operator|.
+name|keep_pack
+expr_stmt|;
+name|args
+operator|.
+name|keep_pack
+operator|=
+literal|1
 expr_stmt|;
 continue|continue;
 block|}
@@ -3881,6 +3825,8 @@ name|arg
 argument_list|)
 condition|)
 block|{
+name|args
+operator|.
 name|use_thin_pack
 operator|=
 literal|1
@@ -3898,6 +3844,8 @@ name|arg
 argument_list|)
 condition|)
 block|{
+name|args
+operator|.
 name|fetch_all
 operator|=
 literal|1
@@ -3915,6 +3863,8 @@ name|arg
 argument_list|)
 condition|)
 block|{
+name|args
+operator|.
 name|verbose
 operator|=
 literal|1
@@ -3932,6 +3882,8 @@ literal|"--depth="
 argument_list|)
 condition|)
 block|{
+name|args
+operator|.
 name|depth
 operator|=
 name|strtol
@@ -3958,6 +3910,8 @@ name|arg
 argument_list|)
 condition|)
 block|{
+name|args
+operator|.
 name|no_progress
 operator|=
 literal|1
@@ -4017,6 +3971,9 @@ name|ref
 operator|=
 name|fetch_pack
 argument_list|(
+operator|&
+name|args
+argument_list|,
 name|dest
 argument_list|,
 name|nr_heads
@@ -4072,6 +4029,11 @@ name|ref
 modifier|*
 name|fetch_pack
 parameter_list|(
+name|struct
+name|fetch_pack_args
+modifier|*
+name|my_args
+parameter_list|,
 specifier|const
 name|char
 modifier|*
@@ -4114,8 +4076,23 @@ name|struct
 name|stat
 name|st
 decl_stmt|;
+name|memcpy
+argument_list|(
+operator|&
+name|args
+argument_list|,
+name|my_args
+argument_list|,
+sizeof|sizeof
+argument_list|(
+name|args
+argument_list|)
+argument_list|)
+expr_stmt|;
 if|if
 condition|(
+name|args
+operator|.
 name|depth
 operator|>
 literal|0
@@ -4155,6 +4132,8 @@ name|dest
 argument_list|,
 name|uploadpack
 argument_list|,
+name|args
+operator|.
 name|verbose
 condition|?
 name|CONNECT_VERBOSE
@@ -4281,6 +4260,8 @@ condition|(
 operator|!
 name|ret
 operator|&&
+name|args
+operator|.
 name|depth
 operator|>
 literal|0

fetch-pack.h
@@ -42,6 +42,11 @@ name|keep_pack
 range|:
 literal|1
 decl_stmt|,
+DECL|member|lock_pack
+name|lock_pack
+range|:
+literal|1
+decl_stmt|,
 DECL|member|use_thin_pack
 name|use_thin_pack
 range|:
@@ -67,23 +72,16 @@ struct|;
 end_struct
 
 begin_function_decl
-name|void
-name|setup_fetch_pack
-parameter_list|(
-name|struct
-name|fetch_pack_args
-modifier|*
-name|args
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
 name|struct
 name|ref
 modifier|*
 name|fetch_pack
 parameter_list|(
+name|struct
+name|fetch_pack_args
+modifier|*
+name|args
+parameter_list|,
 specifier|const
 name|char
 modifier|*

transport.c
@@ -1811,6 +1811,19 @@ decl_stmt|;
 name|int
 name|i
 decl_stmt|;
+name|memset
+argument_list|(
+operator|&
+name|args
+argument_list|,
+literal|0
+argument_list|,
+sizeof|sizeof
+argument_list|(
+name|args
+argument_list|)
+argument_list|)
+expr_stmt|;
 name|args
 operator|.
 name|uploadpack
@@ -1818,12 +1831,6 @@ operator|=
 name|data
 operator|->
 name|uploadpack
-expr_stmt|;
-name|args
-operator|.
-name|quiet
-operator|=
-literal|0
 expr_stmt|;
 name|args
 operator|.
@@ -1832,6 +1839,12 @@ operator|=
 name|data
 operator|->
 name|keep
+expr_stmt|;
+name|args
+operator|.
+name|lock_pack
+operator|=
+literal|1
 expr_stmt|;
 name|args
 operator|.
@@ -1851,12 +1864,6 @@ name|thin
 expr_stmt|;
 name|args
 operator|.
-name|fetch_all
-operator|=
-literal|0
-expr_stmt|;
-name|args
-operator|.
 name|verbose
 operator|=
 name|transport
@@ -1870,18 +1877,6 @@ operator|=
 name|data
 operator|->
 name|depth
-expr_stmt|;
-name|args
-operator|.
-name|no_progress
-operator|=
-literal|0
-expr_stmt|;
-name|setup_fetch_pack
-argument_list|(
-operator|&
-name|args
-argument_list|)
 expr_stmt|;
 for|for
 control|(
@@ -1920,6 +1915,9 @@ name|refs
 operator|=
 name|fetch_pack
 argument_list|(
+operator|&
+name|args
+argument_list|,
 name|dest
 argument_list|,
 name|nr_heads

commit 8fd9b6bbfcdcac40dc4da1c92af7f2cf2b143d75

restore fetching with thin-pack capability
Broken since commit d471c2e00ad2a04ee6edde7b7ccc38db1ce41f25 [formerly fa74052922cf39e5a39ad7178d1b13c2da9b4519].

Signed-off-by: Nicolas Pitre <nico@cam.org>
Signed-off-by: Junio C Hamano <gitster@pobox.com>

Former-commit-id: 3891f390eafa4bec95088f16f8f481553c535b78

builtin-fetch-pack.c
@@ -182,15 +182,12 @@ end_decl_stmt
 begin_decl_stmt
 DECL|variable|non_common_revs
 DECL|variable|multi_ack
-DECL|variable|use_thin_pack
 DECL|variable|use_sideband
 specifier|static
 name|int
 name|non_common_revs
 decl_stmt|,
 name|multi_ack
-decl_stmt|,
-name|use_thin_pack
 decl_stmt|,
 name|use_sideband
 decl_stmt|;
@@ -885,6 +882,8 @@ literal|""
 operator|)
 argument_list|,
 operator|(
+name|args
+operator|.
 name|use_thin_pack
 condition|?
 literal|" thin-pack"