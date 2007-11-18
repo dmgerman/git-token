ab8a430e10b486be13c0ef7989014c8fa09d9372
Junio C Hamano <gitster@pobox.com>
2007-11-18 12:21:17 -0800

    builtin-commit: run commit-msg hook with correct message file
    It should run with $GIT_DIR/COMMIT_EDITMSG, not just COMMIT_EDITMSG.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: e06ad5ffb9f8d4207d676fadd675cf1b949ce358

--- builtin-commit.c
+++ builtin-commit.c
@@ -3507,7 +3507,10 @@ name|index_file
 argument_list|,
 literal|"commit-msg"
 argument_list|,
+name|git_path
+argument_list|(
 name|commit_editmsg
+argument_list|)
 argument_list|)
 condition|)
 name|exit
