739a60bdca4ee08501f821d11eac2694a6a3ade1
Martin Koegler <mkoegler@auto.tuwien.ac.at>
2008-02-25 22:46:08 +0100

    builtin-fsck: move common object checking code to fsck.c
    Signed-off-by: Martin Koegler <mkoegler@auto.tuwien.ac.at>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: ba002f3b28ab9febea432d4c415dbe581836d9a0

--- fsck.h
+++ fsck.h
@@ -12,6 +12,22 @@ directive|define
 name|GIT_FSCK_H
 end_define
 
+begin_define
+DECL|macro|FSCK_ERROR
+define|#
+directive|define
+name|FSCK_ERROR
+value|1
+end_define
+
+begin_define
+DECL|macro|FSCK_WARN
+define|#
+directive|define
+name|FSCK_WARN
+value|2
+end_define
+
 begin_comment
 comment|/*  * callback function for fsck_walk  * type is the expected type of the object or OBJ_ANY  * the return value is:  *     0	everything OK  *<0	error signaled and abort  *>0	error signaled and do not abort  */
 end_comment
@@ -41,6 +57,37 @@ function_decl|;
 end_typedef
 
 begin_comment
+comment|/* callback for fsck_object, type is FSCK_ERROR or FSCK_WARN */
+end_comment
+
+begin_typedef
+DECL|typedef|fsck_error
+typedef|typedef
+name|int
+function_decl|(
+modifier|*
+name|fsck_error
+function_decl|)
+parameter_list|(
+name|struct
+name|object
+modifier|*
+name|obj
+parameter_list|,
+name|int
+name|type
+parameter_list|,
+specifier|const
+name|char
+modifier|*
+name|err
+parameter_list|,
+modifier|...
+parameter_list|)
+function_decl|;
+end_typedef
+
+begin_comment
 comment|/* descend in all linked child objects  * the return value is:  *    -1	error in processing the object  *<0	return value of the callback, which lead to an abort  *>0	return value of the first sigaled error>0 (in the case of no other errors)  *    0		everything OK  */
 end_comment
 
@@ -59,6 +106,24 @@ parameter_list|,
 name|void
 modifier|*
 name|data
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
+name|int
+name|fsck_object
+parameter_list|(
+name|struct
+name|object
+modifier|*
+name|obj
+parameter_list|,
+name|int
+name|strict
+parameter_list|,
+name|fsck_error
+name|error_func
 parameter_list|)
 function_decl|;
 end_function_decl
