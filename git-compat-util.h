32b875c0e95873bf45f001fd799e6e8ad270f140
Heikki Orsila <heikki.orsila@iki.fi>
2008-04-27 12:48:30 +0300

    Document functions xmemdupz(), xread() and xwrite()
    Signed-off-by: Heikki Orsila <heikki.orsila@iki.fi>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: e8729f5380788855943c69f749eb5e276c3b8310

--- git-compat-util.h
+++ git-compat-util.h
@@ -1769,6 +1769,10 @@ return|;
 block|}
 end_function
 
+begin_comment
+comment|/*  * xmemdupz() allocates (len + 1) bytes of memory, duplicates "len" bytes of  * "data" to the allocated memory, zero terminates the allocated memory,  * and returns a pointer to the allocated memory. If the allocation fails,  * the program dies.  */
+end_comment
+
 begin_function
 DECL|function|xmemdupz
 specifier|static
@@ -2188,6 +2192,10 @@ return|;
 block|}
 end_function
 
+begin_comment
+comment|/*  * xread() is the same a read(), but it automatically restarts read()  * operations with a recoverable error (EAGAIN and EINTR). xread()  * DOES NOT GUARANTEE that "len" bytes is read even if the data is available.  */
+end_comment
+
 begin_function
 DECL|function|xread
 specifier|static
@@ -2250,6 +2258,10 @@ return|;
 block|}
 block|}
 end_function
+
+begin_comment
+comment|/*  * xwrite() is the same a write(), but it automatically restarts write()  * operations with a recoverable error (EAGAIN and EINTR). xwrite() DOES NOT  * GUARANTEE that "len" bytes is written even if the operation is successful.  */
+end_comment
 
 begin_function
 DECL|function|xwrite
