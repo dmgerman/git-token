81225e9c910e616f32581837540c716d278981c0
Alex Riesen <raa.lkml@gmail.com>
2008-05-12 19:58:10 +0200

    Extend interface of add_files_to_cache to allow ignore indexing errors
    Signed-off-by: Alex Riesen <raa.lkml@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 7ae02a30e817eda16ea362c6304b6ae28c3a7644

--- builtin-commit.c
+++ builtin-commit.c
@@ -1317,8 +1317,6 @@ argument_list|)
 decl_stmt|;
 name|add_files_to_cache
 argument_list|(
-literal|0
-argument_list|,
 name|also
 condition|?
 name|prefix
@@ -1326,6 +1324,8 @@ else|:
 name|NULL
 argument_list|,
 name|pathspec
+argument_list|,
+literal|0
 argument_list|)
 expr_stmt|;
 name|refresh_cache
