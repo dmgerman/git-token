5c1119312d533e29570f1f0b30b8dd59ac6a934e
Steffen Prohaska <prohaska@zib.de>
2008-07-13 22:31:19 +0200

    help.c: Add support for htmldir relative to git_exec_path()
    If htmldir (in the Makefile) is a relative path, this path will now be
    interpreted relative to git_exec_path.  This can be used to create an
    installation that can be moved to a different directory without
    re-compiling.  The Windows installer (msysgit) is an example for such
    a setup.
    
    Note that the Makefile maps htmldir to the define GIT_HTML_PATH.
    
    Signed-off-by: Steffen Prohaska <prohaska@zib.de>
    Signed-off-by: Johannes Sixt <johannes.sixt@telecom.at>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 868da8d5e329c951f0d0cd049a8f9fecda64d388

--- help.c
+++ help.c
@@ -3527,13 +3527,27 @@ name|struct
 name|stat
 name|st
 decl_stmt|;
+specifier|const
+name|char
+modifier|*
+name|html_path
+init|=
+name|system_path
+argument_list|(
+name|GIT_HTML_PATH
+argument_list|)
+decl_stmt|;
 comment|/* Check that we have a git documentation directory. */
 if|if
 condition|(
 name|stat
 argument_list|(
-name|GIT_HTML_PATH
-literal|"/git.html"
+name|mkpath
+argument_list|(
+literal|"%s/git.html"
+argument_list|,
+name|html_path
+argument_list|)
 argument_list|,
 operator|&
 name|st
@@ -3551,7 +3565,7 @@ name|die
 argument_list|(
 literal|"'%s': not a documentation directory."
 argument_list|,
-name|GIT_HTML_PATH
+name|html_path
 argument_list|)
 expr_stmt|;
 name|strbuf_init
@@ -3565,8 +3579,9 @@ name|strbuf_addf
 argument_list|(
 name|page_path
 argument_list|,
-name|GIT_HTML_PATH
-literal|"/%s.html"
+literal|"%s/%s.html"
+argument_list|,
+name|html_path
 argument_list|,
 name|page
 argument_list|)
