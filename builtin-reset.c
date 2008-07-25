da229a7e8fe197fb22ea6f59a50ac4b910a3879c
Stephan Beyer <s-beyer@gmx.net>
2008-07-25 22:49:08 +0200

    git-reset: Let -q hush "locally modified" messages
    "git reset -q" is advertised to "only report errors", but "locally
    modified" messages are still shown.  They are not errors but diagnostics.
    
    Signed-off-by: Stephan Beyer <s-beyer@gmx.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: b0320eaf6a25fbc4adf35d611c27006e6d853aa8

--- builtin-reset.c
+++ builtin-reset.c
@@ -457,6 +457,9 @@ name|struct
 name|lock_file
 modifier|*
 name|index_lock
+parameter_list|,
+name|int
+name|flags
 parameter_list|)
 block|{
 name|int
@@ -508,7 +511,7 @@ name|result
 operator|=
 name|refresh_cache
 argument_list|(
-name|REFRESH_SAY_CHANGED
+name|flags
 argument_list|)
 condition|?
 literal|1
@@ -691,6 +694,9 @@ name|unsigned
 name|char
 modifier|*
 name|tree_sha1
+parameter_list|,
+name|int
+name|refresh_flags
 parameter_list|)
 block|{
 name|struct
@@ -833,6 +839,8 @@ argument_list|(
 name|index_fd
 argument_list|,
 name|lock
+argument_list|,
+name|refresh_flags
 argument_list|)
 return|;
 block|}
@@ -1350,6 +1358,12 @@ operator|+
 name|i
 argument_list|,
 name|sha1
+argument_list|,
+name|quiet
+condition|?
+name|REFRESH_QUIET
+else|:
+name|REFRESH_SAY_CHANGED
 argument_list|)
 return|;
 block|}
@@ -1563,6 +1577,12 @@ argument_list|(
 literal|0
 argument_list|,
 name|NULL
+argument_list|,
+name|quiet
+condition|?
+name|REFRESH_QUIET
+else|:
+name|REFRESH_SAY_CHANGED
 argument_list|)
 expr_stmt|;
 break|break;
