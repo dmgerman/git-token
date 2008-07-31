0cf8fdffaf97b637df2a69211f4ee5abc40f9c6c
Giuseppe Bilotta <giuseppe.bilotta@gmail.com>
2008-07-31 09:21:48 +0200

    diff: add ruby funcname pattern
    Provide a regexp that catches class, module and method definitions in
    Ruby scripts, since the built-in default only finds classes.
    
    Signed-off-by: Giuseppe Bilotta <giuseppe.bilotta@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: ad8c1d92600da963692d57c213979e638c357f1a

--- diff.c
+++ diff.c
@@ -8169,6 +8169,12 @@ literal|"tex"
 block|,
 literal|"^\\(\\\\\\(sub\\)*section{.*\\)$"
 block|}
+block|,
+block|{
+literal|"ruby"
+block|,
+literal|"^\\s*\\(\\(class\\|module\\|def\\)\\s.*\\)$"
+block|}
 block|, }
 struct|;
 end_struct
