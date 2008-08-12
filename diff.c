31f4f51240d3c517b2d391868af067642c36cbe1
Gustaf Hendeby <hendeby@isy.liu.se>
2008-08-12 16:24:26 +0200

    Teach git diff about BibTeX head hunk patterns
    All BibTeX entries starts with an @ followed by an entry type.  Since
    there are many entry types and own can be defined, the pattern matches
    legal entry type names instead of just the default types (which would
    be a long list).  The pattern also matches strings and comments since
    they will also be useful to position oneself in a bib-file.
    
    Signed-off-by: Gustaf Hendeby <hendeby@isy.liu.se>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 23b5beb28fdadbb1d80ebf686a35385609f7a180

--- diff.c
+++ diff.c
@@ -8175,6 +8175,12 @@ literal|"^\\(.*=[ \t]*\\(class\\|record\\).*\\)$"
 block|}
 block|,
 block|{
+literal|"bibtex"
+block|,
+literal|"\\(@[a-zA-Z]\\{1,\\}[ \t]*{\\{0,1\\}[ \t]*[^ \t\"@',\\#}{~%]*\\).*$"
+block|}
+block|,
+block|{
 literal|"tex"
 block|,
 literal|"^\\(\\\\\\(\\(sub\\)*section\\|chapter\\|part\\)\\*\\{0,1\\}{.*\\)$"
