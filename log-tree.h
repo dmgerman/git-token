8807f24020e27d9630cee22e5729f459bd870b97
René Scharfe <rene.scharfe@lsrfire.ath.cx>
2008-09-04 23:39:21 +0200

    move load_ref_decorations() to log-tree.c and export it
    log-tree.c is the ideal place for load_ref_decorations() and its
    helper functions to live in, because the variable name_decoration
    they're operating on is already located there, so move them thither.
    
    Signed-off-by: Rene Scharfe <rene.scharfe@lsrfire.ath.cx>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: cab4feb67db64d3d201145d9748b33d148f96185

--- log-tree.h
+++ log-tree.h
@@ -149,6 +149,15 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_function_decl
+name|void
+name|load_ref_decorations
+parameter_list|(
+name|void
+parameter_list|)
+function_decl|;
+end_function_decl
+
 begin_endif
 endif|#
 directive|endif
