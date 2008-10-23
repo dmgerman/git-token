ba9ec2d6fb8f1a42a8a2179df57fc46f5faa3e86
Nicolas Pitre <nico@cam.org>
2008-10-23 15:05:59 -0400

    index-pack: don't leak leaf delta result
    Another (but minor this time) fallout from commit 9441b61 (index-pack:
    rationalize delta resolution code, 2008-10-17).
    
    Signed-off-by: Nicolas Pitre <nico@cam.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 9ed19614d4afd86a8d51072ed34c2c44554c3b54

--- index-pack.c
+++ index-pack.c
@@ -3112,7 +3112,16 @@ operator|==
 operator|-
 literal|1
 condition|)
+block|{
+name|free
+argument_list|(
+name|base
+operator|->
+name|data
+argument_list|)
+expr_stmt|;
 return|return;
+block|}
 name|link_base_data
 argument_list|(
 name|prev_base
