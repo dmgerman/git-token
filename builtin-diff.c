36ed70b66aa28c896b68293d024e117fbb669e2e
Junio C Hamano <gitster@pobox.com>
2008-11-26 09:58:41 -0800

    Teach "git diff" to honour --[no-]ext-diff
    The original intention of 72909be (Add diff-option --ext-diff, 2007-06-30)
    was to optionally allow the use of external diff viewer in "git log"
    family (while keeping them disabled by default).  It exposed the "allow
    external diff" bit to the UI, but forgot to adjust the "git diff" codepath
    that was set up to always allow use of the external diff viewer.
    
    Noticed by Nazri Ramliy; tests by René Scharfe squashed in.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 61af494ca4e63cdc484ea091617ab5f3974f2f9c

--- builtin-diff.c
+++ builtin-diff.c
@@ -1526,6 +1526,17 @@ operator|!
 operator|!
 name|diff_auto_refresh_index
 expr_stmt|;
+comment|/* Default to let external be used */
+name|DIFF_OPT_SET
+argument_list|(
+operator|&
+name|rev
+operator|.
+name|diffopt
+argument_list|,
+name|ALLOW_EXTERNAL
+argument_list|)
+expr_stmt|;
 if|if
 condition|(
 name|nongit
@@ -1585,16 +1596,6 @@ literal|"diff_setup_done failed"
 argument_list|)
 expr_stmt|;
 block|}
-name|DIFF_OPT_SET
-argument_list|(
-operator|&
-name|rev
-operator|.
-name|diffopt
-argument_list|,
-name|ALLOW_EXTERNAL
-argument_list|)
-expr_stmt|;
 name|DIFF_OPT_SET
 argument_list|(
 operator|&
