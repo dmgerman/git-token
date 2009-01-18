9554cd6f0e6ddf2ce1f8258afdcc2ffdcae16025
Junio C Hamano <gitster@pobox.com>
2009-01-17 22:27:08 -0800

    bundle: allow the same ref to be given more than once
    "git bundle create x master master" used to create a bundle that lists
    the same branch (master) twice.  Cloning from such a bundle resulted in
    a needless warning "warning: Duplicated ref: refs/remotes/origin/master".
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: b2a6d1c6868b6d5e7d2b4fa9129341220a1e848a

--- object.h
+++ object.h
@@ -444,6 +444,17 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_function_decl
+name|void
+name|object_array_remove_duplicates
+parameter_list|(
+name|struct
+name|object_array
+modifier|*
+parameter_list|)
+function_decl|;
+end_function_decl
+
 begin_endif
 endif|#
 directive|endif
