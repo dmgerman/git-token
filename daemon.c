896679b403f573659e88801eed325795229fd7f7
Jeff King <peff@peff.net>
2009-03-04 03:32:29 -0500

    improve missing repository error message
    Certain remote commands, when asked to do something in a
    particular directory that was not actually a git repository,
    would say "unable to chdir or not a git archive". The
    "chdir" bit is an unnecessary detail, and the term "git
    archive" is much less common these days than "git repository".
    
    So let's switch them all to:
    
      fatal: '%s' does not appear to be a git repository
    
    Signed-off-by: Jeff King <peff@peff.net>
    Acked-by: Shawn O. Pearce <spearce@spearce.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 05ac6b34e2c2adda5cb85dd0bdacb47fe5db953e

--- daemon.c
+++ daemon.c
@@ -1011,7 +1011,7 @@ condition|)
 block|{
 name|logerror
 argument_list|(
-literal|"'%s': unable to chdir or not a git archive"
+literal|"'%s' does not appear to be a git repository"
 argument_list|,
 name|dir
 argument_list|)
