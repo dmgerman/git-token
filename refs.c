f9fcf22bd5066fec4c5b671e511ec05735f6edd8
Robin Rosenberg <robin.rosenberg@dewire.com>
2009-05-08 07:32:37 +0200

    Disallow '\' in ref names
    This is asking for trouble since '\' is a directory separator in
    Windows and thus may produce unpredictable results.
    
    Signed-off-by: Robin Rosenberg <robin.rosenberg@dewire.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: a4c2e69936df8dd0b071b85664c6cc6a4870dd84

--- refs.c
+++ refs.c
@@ -3564,7 +3564,7 @@ block|}
 end_function
 
 begin_comment
-comment|/*  * Make sure "ref" is something reasonable to have under ".git/refs/";  * We do not like it if:  *  * - any path component of it begins with ".", or  * - it has double dots "..", or  * - it has ASCII control character, "~", "^", ":" or SP, anywhere, or  * - it ends with a "/".  * - it ends with ".lock"  */
+comment|/*  * Make sure "ref" is something reasonable to have under ".git/refs/";  * We do not like it if:  *  * - any path component of it begins with ".", or  * - it has double dots "..", or  * - it has ASCII control character, "~", "^", ":" or SP, anywhere, or  * - it ends with a "/".  * - it ends with ".lock"  * - it contains a "\" (backslash)  */
 end_comment
 
 begin_function
@@ -3600,6 +3600,10 @@ operator|||
 name|ch
 operator|==
 literal|':'
+operator|||
+name|ch
+operator|==
+literal|'\\'
 condition|)
 return|return
 literal|1
