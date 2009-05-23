dmgerman/git-token

commit c33388f6f0d4bd82f048bdf2196ac46e94f66cce

grep -w: forward to next possible position after rejected match
grep -w accepts matches between non-word characters, only.  If a match
from regexec() doesn't meet this criteria, grep continues its search
after the first character of that match.

We can be a bit smarter here and skip all positions that follow a word
character first, as they can't match our criteria.  This way we can
consume characters quite cheaply and don't need to special-case the
handling of the beginning of a line.

Here's a contrived example command on msysgit (best of five runs):

	$ time git grep -w ...... v1.6.1 >/dev/null

	real    0m1.611s
	user    0m0.000s
	sys     0m0.015s

With the patch it's quite a bit faster:

	$ time git grep -w ...... v1.6.1 >/dev/null

	real    0m1.179s
	user    0m0.000s
	sys     0m0.015s

More common search patterns will gain a lot less, but it's a nice clean
up anyway.

Signed-off-by: Rene Scharfe <rene.scharfe@lsrfire.ath.cx>
Signed-off-by: Junio C Hamano <gitster@pobox.com>

Former-commit-id: fb62eb7fab97cea880ea7fe4f341a4dfad14ab48

grep.c
@@ -1628,11 +1628,6 @@ init|=
 literal|0
 decl_stmt|;
 name|int
-name|at_true_bol
-init|=
-literal|1
-decl_stmt|;
-name|int
 name|saved_ch
 init|=
 literal|0
@@ -1885,8 +1880,6 @@ operator|.
 name|rm_so
 operator|==
 literal|0
-operator|&&
-name|at_true_bol
 operator|)
 operator|||
 operator|!
@@ -1962,7 +1955,7 @@ operator|<
 name|eol
 condition|)
 block|{
-comment|/* There could be more than one match on the 			 * line, and the first match might not be 			 * strict word match.  But later ones could be! 			 */
+comment|/* There could be more than one match on the 			 * line, and the first match might not be 			 * strict word match.  But later ones could be! 			 * Forward to the next possible start, i.e. the 			 * next position following a non-word char. 			 */
 name|bol
 operator|=
 name|pmatch
@@ -1976,10 +1969,30 @@ name|bol
 operator|+
 literal|1
 expr_stmt|;
-name|at_true_bol
-operator|=
-literal|0
+while|while
+condition|(
+name|word_char
+argument_list|(
+name|bol
+index|[
+operator|-
+literal|1
+index|]
+argument_list|)
+operator|&&
+name|bol
+operator|<
+name|eol
+condition|)
+name|bol
+operator|++
 expr_stmt|;
+if|if
+condition|(
+name|bol
+operator|<
+name|eol
+condition|)
 goto|goto
 name|again
 goto|;

commit c4470919fae28b89bd1a6a0fc4a9e83115291c59

grep: fix word-regexp at the beginning of lines
After bol is forwarded, it doesn't represent the beginning of the line
any more.  This means that the beginning-of-line marker (^) mustn't match,
i.e. the regex flag REG_NOTBOL needs to be set.

This bug was introduced by c33388f6f0d4bd82f048bdf2196ac46e94f66cce [formerly fb62eb7fab97cea880ea7fe4f341a4dfad14ab48]
("grep -w: forward to next possible position after rejected match").

Signed-off-by: Rene Scharfe <rene.scharfe@lsrfire.ath.cx>
Signed-off-by: Junio C Hamano <gitster@pobox.com>

Former-commit-id: dbb6a4ada6c6c1065b62313127ff032196e9d232

grep.c
@@ -1974,6 +1974,10 @@ condition|)
 name|bol
 operator|++
 expr_stmt|;
+name|eflags
+operator||=
+name|REG_NOTBOL
+expr_stmt|;
 if|if
 condition|(
 name|bol

t/t7002-grep.sh
@@ -16,12 +16,13 @@ test_expect_success setup '
 		echo foo mmap bar_mmap
 		echo foo_mmap bar mmap baz
 	} >file &&
+	echo ww w >w &&
 	echo x x xx x >x &&
 	echo y yy >y &&
 	echo zzz > z &&
 	mkdir t &&
 	echo test >t/t &&
-	git add file x y z t/t &&
+	git add file w x y z t/t &&
 	test_tick &&
 	git commit -m initial
 '
@@ -46,6 +47,12 @@ do
 		} >expected &&
 		git grep -n -w -e mmap $H >actual &&
 		diff expected actual
+	'
+
+	test_expect_success "grep -w $L (w)" '
+		: >expected &&
+		! git grep -n -w -e "^w" >actual &&
+		test_cmp expected actual
 	'
 
 	test_expect_success "grep -w $L (x)" '