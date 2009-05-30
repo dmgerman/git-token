c26bfa5469f197203dde78e9f92936289271de07
Jeff King <peff@peff.net>
2009-05-30 17:54:18 -0400

    git-add: no need for -f when resolving a conflict in already tracked path
    When a path F that matches ignore pattern has a conflict, "git add F"
    insisted the -f option be given, which did not make sense.  It would have
    required -f when the path was originally added, but when resolving a
    conflict, it already is tracked.
    
    So this should work (and does):
    
      $ echo file >.gitignore
      $ echo content >file
      $ git add -f file ;# need -f because we are adding new path
      $ echo more content >>file
      $ git add file ;# don't need -f; it is not actually an "other" file
    
    This is handled under the hood by the COLLECT_IGNORED option to
    read_directory. When that code finds an ignored file, it checks the
    index to make sure it is not actually a tracked file. However, the test
    it uses does not take into account unmerged entries, and considers them
    to still be ignored. "git ls-files" uses a more elaborate test and gets
    the right answer and the same test should be used here.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 6e4f981ffb88c8a9d3d6128314f4dd4f54ffb8a8

--- dir.c
+++ dir.c
@@ -2138,14 +2138,13 @@ parameter_list|)
 block|{
 if|if
 condition|(
-name|cache_name_pos
+operator|!
+name|cache_name_is_other
 argument_list|(
 name|pathname
 argument_list|,
 name|len
 argument_list|)
-operator|>=
-literal|0
 condition|)
 return|return
 name|NULL
