eb50117f7f0ae1b038deb519c6d54e249a2350a9
Johannes Schindelin <johannes.schindelin@gmx.de>
2009-06-01 08:04:16 +0200

    Work around a regression in Windows 7, causing erase_in_line() to crash sometimes
    The function FillConsoleOutputCharacterA() was pretty content in XP to take a NULL
    pointer if we did not want to store the number of written columns.  In Windows 7,
    it crashes, but only when called from within Git Bash, not from within cmd.exe.
    Go figure.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Steffen Prohaska <prohaska@zib.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 492f70913e11de19bc4cb197b1fe195c645b89c9

--- compat/winansi.c
+++ compat/winansi.c
@@ -282,6 +282,10 @@ block|{
 name|CONSOLE_SCREEN_BUFFER_INFO
 name|sbi
 decl_stmt|;
+name|DWORD
+name|dummy
+decl_stmt|;
+comment|/* Needed for Windows 7 (or Vista) regression */
 if|if
 condition|(
 operator|!
@@ -318,7 +322,8 @@ name|sbi
 operator|.
 name|dwCursorPosition
 argument_list|,
-name|NULL
+operator|&
+name|dummy
 argument_list|)
 expr_stmt|;
 block|}
