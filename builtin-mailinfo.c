e3f9b426b1c7f22896a20d2f40ff9191e7e4f7c3
Andreas Ericsson <ae@op5.se>
2009-06-29 11:55:51 +0200

    mailinfo: Remove only one set of square brackets
    git-format-patch prepends patches with a [PATCH x/n] prefix, but
    mailinfo used to remove any number of square-bracket pairs and
    the content between them. This prevents one from using a commit
    subject like this:
    
      [ and ] must be allowed as input
    
    Removing the square bracket pair from this rather clumsily
    constructed subject line loses important information, so we must
    take care not to.
    
    This patch causes the subject stripping to stop after it has
    encountered one pair of square brackets.
    
    One possible downside of this patch is that the patch-handling
    programs will now fail at removing author-added square-brackets
    to be removed, such as
    
      [RFC][PATCH x/n]
    
    However, since format-patch only adds one set of square brackets,
    this behaviour is quite easily undesrstood and defended while the
    previous behaviour is not.
    
    Signed-off-by: Andreas Ericsson <ae@op5.se>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 650d30d8a120c8982309ccb9ef40432b4ea2eb74

--- builtin-mailinfo.c
+++ builtin-mailinfo.c
@@ -1175,6 +1175,11 @@ decl_stmt|;
 name|size_t
 name|remove
 decl_stmt|;
+name|int
+name|brackets_removed
+init|=
+literal|0
+decl_stmt|;
 while|while
 condition|(
 name|subject
@@ -1256,6 +1261,12 @@ continue|continue;
 case|case
 literal|'['
 case|:
+comment|/* remove only one set of square brackets */
+if|if
+condition|(
+name|brackets_removed
+condition|)
+break|break;
 if|if
 condition|(
 operator|(
@@ -1305,6 +1316,10 @@ name|remove
 operator|+
 literal|1
 argument_list|)
+expr_stmt|;
+name|brackets_removed
+operator|=
+literal|1
 expr_stmt|;
 continue|continue;
 block|}
