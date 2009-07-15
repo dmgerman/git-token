e57df12166a7f9e490720975a5ba07a1e7de5463
Junio C Hamano <gitster@pobox.com>
2009-07-15 15:10:06 -0700

    Revert "mailinfo: Remove only one set of square brackets"
    This reverts commit e3f9b426b1c7f22896a20d2f40ff9191e7e4f7c3 [formerly 650d30d8a120c8982309ccb9ef40432b4ea2eb74].
    
    Some mailing lists are configured add prefix "[listname] " to all their
    messages, and also people hand-edit subject lines, be it an output from
    format-patch or a patch generated by some other means.
    
    We cannot stop people from mucking with the subject line, and with the
    change, there always will be need for hand editing the subject when that
    happens.  People have depended on the leading [bracketed string] removal.
    
    
    Former-commit-id: 4525e8e41a8c0c4246a95097744604ea72198dad

--- builtin-mailinfo.c
+++ builtin-mailinfo.c
@@ -1167,11 +1167,6 @@ decl_stmt|;
 name|size_t
 name|remove
 decl_stmt|;
-name|int
-name|brackets_removed
-init|=
-literal|0
-decl_stmt|;
 while|while
 condition|(
 name|subject
@@ -1253,12 +1248,6 @@ continue|continue;
 case|case
 literal|'['
 case|:
-comment|/* remove only one set of square brackets */
-if|if
-condition|(
-name|brackets_removed
-condition|)
-break|break;
 if|if
 condition|(
 operator|(
@@ -1308,10 +1297,6 @@ name|remove
 operator|+
 literal|1
 argument_list|)
-expr_stmt|;
-name|brackets_removed
-operator|=
-literal|1
 expr_stmt|;
 continue|continue;
 block|}
