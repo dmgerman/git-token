b480a524a7b7da2513972ffe61a66e4f3c55f210
Pierre Habouzit <madcoder@debian.org>
2009-07-22 23:34:34 +0200

    refactor: use bitsizeof() instead of 8 * sizeof()
    Signed-off-by: Pierre Habouzit <madcoder@debian.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: f630cfda8800aee03c7eb2fcd0f840730fbe43b9

--- git-compat-util.h
+++ git-compat-util.h
@@ -169,6 +169,17 @@ parameter_list|)
 value|(sizeof(x)/sizeof(x[0]))
 end_define
 
+begin_define
+DECL|macro|bitsizeof
+define|#
+directive|define
+name|bitsizeof
+parameter_list|(
+name|x
+parameter_list|)
+value|(CHAR_BIT * sizeof(x))
+end_define
+
 begin_ifdef
 ifdef|#
 directive|ifdef
@@ -216,7 +227,7 @@ name|x
 parameter_list|,
 name|bits
 parameter_list|)
-value|((x)& TYPEOF(x)(~0ULL<< (sizeof(x) * 8 - (bits))))
+value|((x)& TYPEOF(x)(~0ULL<< (bitsizeof(x) - (bits))))
 end_define
 
 begin_define
