330c3262b2481318f5e02f231c9b290e6b3e8c77
Michael J Gruber <git@drmicha.warpmail.net>
2009-08-05 09:59:20 +0200

    git-merge-base/git-show-branch --merge-base: Documentation and test
    Currently, the documentation suggests that 'git merge-base -a' and 'git
    show-branch --merge-base' are equivalent (in fact it claims that the
    former cannot handle more than two revs).
    
    Alas, the handling of more than two revs is very different. Document
    this by tests and correct the documentation to reflect this.
    
    Signed-off-by: Michael J Gruber <git@drmicha.warpmail.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: f621a8454d19d17fe46e6951b7e3d22bebd92aba

--- builtin-show-branch.c
+++ builtin-show-branch.c
@@ -3615,7 +3615,7 @@ argument_list|,
 operator|&
 name|merge_base
 argument_list|,
-literal|"act like git merge-base -a"
+literal|"show possible merge bases"
 argument_list|)
 block|,
 name|OPT_BOOLEAN
