2b2178ae40a29cdb821f5a1cef0fd9cd67f64c29
Björn Steinbrink <B.Steinbrink@gmx.de>
2009-08-06 14:15:14 +0200

    git-describe: Die early if there are no possible descriptions
    If we find no refs that may be used for git-describe with the current
    options, then die early instead of pointlessly walking the whole
    history.
    
    In git.git with all the tags dropped, this makes "git describe" go down
    from 0.244 to 0.003 seconds for me. This is especially noticeable with
    "git submodule status" which calls describe with increasing levels of
    allowed refs to be matched. For a submodule without tags, this means
    that it walks the whole history in the submodule twice (first annotated,
    then plain tags), just to find out that it can't describe the commit
    anyway.
    
    Signed-off-by: Björn Steinbrink <B.Steinbrink@gmx.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: d68dc34cb41c30515e64766c303c2930994e5085

--- builtin-describe.c
+++ builtin-describe.c
@@ -143,6 +143,14 @@ decl_stmt|;
 end_decl_stmt
 
 begin_decl_stmt
+DECL|variable|found_names
+specifier|static
+name|int
+name|found_names
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
 DECL|variable|pattern
 specifier|static
 specifier|const
@@ -329,6 +337,10 @@ operator|=
 name|e
 expr_stmt|;
 block|}
+name|found_names
+operator|=
+literal|1
+expr_stmt|;
 block|}
 end_function
 
@@ -1183,6 +1195,21 @@ name|NULL
 argument_list|)
 expr_stmt|;
 block|}
+if|if
+condition|(
+operator|!
+name|found_names
+condition|)
+name|die
+argument_list|(
+literal|"cannot describe '%s'"
+argument_list|,
+name|sha1_to_hex
+argument_list|(
+name|sha1
+argument_list|)
+argument_list|)
+expr_stmt|;
 name|n
 operator|=
 name|cmit
