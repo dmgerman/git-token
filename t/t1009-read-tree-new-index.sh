dmgerman/git-token

commit b106452cbef552d72d30b3578db06f665bb63529

read-tree: migrate to parse-options
Cleanup the documentation to explicitly state that --exclude-directory
is only meaningful when used with -u. Also make the documentation more
consistent with the usage message printed with read-tree --help-all.

The -m, --prefix, --reset options are performing similar actions
(setting some flags, read_cache_unmerged(), checking for illegal option
combinations). Instead of performing these actions when the options are
parsed, we delay performing them until after parse-opts has finished.

The bit fields in struct unpack_trees_options have been promoted to full
unsigned ints. This is necessary to avoid "foo ? 1 : 0" constructs to
set these fields.

Signed-off-by: Stephen Boyd <bebarino@gmail.com>
Signed-off-by: Junio C Hamano <gitster@pobox.com>

Former-commit-id: 5a56da58060e50980fab0f4c38203a25440d1530

builtin-read-tree.c
@@ -51,6 +51,12 @@ directive|include
 file|"builtin.h"
 end_include
 
+begin_include
+include|#
+directive|include
+file|"parse-options.h"
+end_include
+
 begin_decl_stmt
 DECL|variable|nr_trees
 specifier|static
@@ -137,12 +143,143 @@ DECL|variable|read_tree_usage
 specifier|static
 specifier|const
 name|char
+modifier|*
+specifier|const
 name|read_tree_usage
 index|[]
 init|=
-literal|"git read-tree (<sha> | [[-m [--trivial] [--aggressive] | --reset | --prefix=<prefix>] [-u | -i]] [--exclude-per-directory=<gitignore>] [--index-output=<file>]<sha1> [<sha2> [<sha3>]])"
+block|{
+literal|"git read-tree [[-m [--trivial] [--aggressive] | --reset | --prefix=<prefix>] [-u [--exclude-per-directory=<gitignore>] | -i]]  [--index-output=<file>]<tree-ish1> [<tree-ish2> [<tree-ish3>]]"
+block|,
+name|NULL
+block|}
 decl_stmt|;
 end_decl_stmt
+
+begin_function
+DECL|function|index_output_cb
+specifier|static
+name|int
+name|index_output_cb
+parameter_list|(
+specifier|const
+name|struct
+name|option
+modifier|*
+name|opt
+parameter_list|,
+specifier|const
+name|char
+modifier|*
+name|arg
+parameter_list|,
+name|int
+name|unset
+parameter_list|)
+block|{
+name|set_alternate_index_output
+argument_list|(
+name|arg
+argument_list|)
+expr_stmt|;
+return|return
+literal|0
+return|;
+block|}
+end_function
+
+begin_function
+DECL|function|exclude_per_directory_cb
+specifier|static
+name|int
+name|exclude_per_directory_cb
+parameter_list|(
+specifier|const
+name|struct
+name|option
+modifier|*
+name|opt
+parameter_list|,
+specifier|const
+name|char
+modifier|*
+name|arg
+parameter_list|,
+name|int
+name|unset
+parameter_list|)
+block|{
+name|struct
+name|dir_struct
+modifier|*
+name|dir
+decl_stmt|;
+name|struct
+name|unpack_trees_options
+modifier|*
+name|opts
+decl_stmt|;
+name|opts
+operator|=
+operator|(
+expr|struct
+name|unpack_trees_options
+operator|*
+operator|)
+name|opt
+operator|->
+name|value
+expr_stmt|;
+if|if
+condition|(
+name|opts
+operator|->
+name|dir
+condition|)
+name|die
+argument_list|(
+literal|"more than one --exclude-per-directory given."
+argument_list|)
+expr_stmt|;
+name|dir
+operator|=
+name|xcalloc
+argument_list|(
+literal|1
+argument_list|,
+sizeof|sizeof
+argument_list|(
+operator|*
+name|opts
+operator|->
+name|dir
+argument_list|)
+argument_list|)
+expr_stmt|;
+name|dir
+operator|->
+name|flags
+operator||=
+name|DIR_SHOW_IGNORED
+expr_stmt|;
+name|dir
+operator|->
+name|exclude_per_dir
+operator|=
+name|arg
+expr_stmt|;
+name|opts
+operator|->
+name|dir
+operator|=
+name|dir
+expr_stmt|;
+comment|/* We do not need to nor want to do read-directory 	 * here; we are merely interested in reusing the 	 * per directory ignore stack mechanism. 	 */
+return|return
+literal|0
+return|;
+block|}
+end_function
 
 begin_decl_stmt
 DECL|variable|lock_file
@@ -200,6 +337,189 @@ name|struct
 name|unpack_trees_options
 name|opts
 decl_stmt|;
+name|int
+name|prefix_set
+init|=
+literal|0
+decl_stmt|;
+specifier|const
+name|struct
+name|option
+name|read_tree_options
+index|[]
+init|=
+block|{
+block|{
+name|OPTION_CALLBACK
+block|,
+literal|0
+block|,
+literal|"index-output"
+block|,
+name|NULL
+block|,
+literal|"FILE"
+block|,
+literal|"write resulting index to<FILE>"
+block|,
+name|PARSE_OPT_NONEG
+block|,
+name|index_output_cb
+block|}
+block|,
+name|OPT__VERBOSE
+argument_list|(
+operator|&
+name|opts
+operator|.
+name|verbose_update
+argument_list|)
+block|,
+name|OPT_GROUP
+argument_list|(
+literal|"Merging"
+argument_list|)
+block|,
+name|OPT_SET_INT
+argument_list|(
+literal|'m'
+argument_list|,
+name|NULL
+argument_list|,
+operator|&
+name|opts
+operator|.
+name|merge
+argument_list|,
+literal|"perform a merge in addition to a read"
+argument_list|,
+literal|1
+argument_list|)
+block|,
+name|OPT_SET_INT
+argument_list|(
+literal|0
+argument_list|,
+literal|"trivial"
+argument_list|,
+operator|&
+name|opts
+operator|.
+name|trivial_merges_only
+argument_list|,
+literal|"3-way merge if no file level merging required"
+argument_list|,
+literal|1
+argument_list|)
+block|,
+name|OPT_SET_INT
+argument_list|(
+literal|0
+argument_list|,
+literal|"aggressive"
+argument_list|,
+operator|&
+name|opts
+operator|.
+name|aggressive
+argument_list|,
+literal|"3-way merge in presence of adds and removes"
+argument_list|,
+literal|1
+argument_list|)
+block|,
+name|OPT_SET_INT
+argument_list|(
+literal|0
+argument_list|,
+literal|"reset"
+argument_list|,
+operator|&
+name|opts
+operator|.
+name|reset
+argument_list|,
+literal|"same as -m, but discard unmerged entries"
+argument_list|,
+literal|1
+argument_list|)
+block|,
+block|{
+name|OPTION_STRING
+block|,
+literal|0
+block|,
+literal|"prefix"
+block|,
+operator|&
+name|opts
+operator|.
+name|prefix
+block|,
+literal|"<subdirectory>/"
+block|,
+literal|"read the tree into the index under<subdirectory>/"
+block|,
+name|PARSE_OPT_NONEG
+operator||
+name|PARSE_OPT_LITERAL_ARGHELP
+block|}
+block|,
+name|OPT_SET_INT
+argument_list|(
+literal|'u'
+argument_list|,
+name|NULL
+argument_list|,
+operator|&
+name|opts
+operator|.
+name|update
+argument_list|,
+literal|"update working tree with merge result"
+argument_list|,
+literal|1
+argument_list|)
+block|,
+block|{
+name|OPTION_CALLBACK
+block|,
+literal|0
+block|,
+literal|"exclude-per-directory"
+block|,
+operator|&
+name|opts
+block|,
+literal|"gitignore"
+block|,
+literal|"allow explicitly ignored files to be overwritten"
+block|,
+name|PARSE_OPT_NONEG
+block|,
+name|exclude_per_directory_cb
+block|}
+block|,
+name|OPT_SET_INT
+argument_list|(
+literal|'i'
+argument_list|,
+name|NULL
+argument_list|,
+operator|&
+name|opts
+operator|.
+name|index_only
+argument_list|,
+literal|"don't check the working tree after merging"
+argument_list|,
+literal|1
+argument_list|)
+block|,
+name|OPT_END
+argument_list|()
+block|}
+decl_stmt|;
 name|memset
 argument_list|(
 operator|&
@@ -251,11 +571,95 @@ argument_list|,
 literal|1
 argument_list|)
 expr_stmt|;
+name|argc
+operator|=
+name|parse_options
+argument_list|(
+name|argc
+argument_list|,
+name|argv
+argument_list|,
+name|unused_prefix
+argument_list|,
+name|read_tree_options
+argument_list|,
+name|read_tree_usage
+argument_list|,
+literal|0
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|read_cache_unmerged
+argument_list|()
+operator|&&
+operator|(
+name|opts
+operator|.
+name|prefix
+operator|||
+name|opts
+operator|.
+name|merge
+operator|)
+condition|)
+name|die
+argument_list|(
+literal|"You need to resolve your current index first"
+argument_list|)
+expr_stmt|;
+name|prefix_set
+operator|=
+name|opts
+operator|.
+name|prefix
+condition|?
+literal|1
+else|:
+literal|0
+expr_stmt|;
+if|if
+condition|(
+literal|1
+operator|<
+name|opts
+operator|.
+name|merge
+operator|+
+name|opts
+operator|.
+name|reset
+operator|+
+name|prefix_set
+condition|)
+name|die
+argument_list|(
+literal|"Which one? -m, --reset, or --prefix?"
+argument_list|)
+expr_stmt|;
+name|stage
+operator|=
+name|opts
+operator|.
+name|merge
+operator|=
+operator|(
+name|opts
+operator|.
+name|reset
+operator|||
+name|opts
+operator|.
+name|merge
+operator|||
+name|prefix_set
+operator|)
+expr_stmt|;
 for|for
 control|(
 name|i
 operator|=
-literal|1
+literal|0
 init|;
 name|i
 operator|<
@@ -275,365 +679,6 @@ index|[
 name|i
 index|]
 decl_stmt|;
-comment|/* "-u" means "update", meaning that a merge will update 		 * the working tree. 		 */
-if|if
-condition|(
-operator|!
-name|strcmp
-argument_list|(
-name|arg
-argument_list|,
-literal|"-u"
-argument_list|)
-condition|)
-block|{
-name|opts
-operator|.
-name|update
-operator|=
-literal|1
-expr_stmt|;
-continue|continue;
-block|}
-if|if
-condition|(
-operator|!
-name|strcmp
-argument_list|(
-name|arg
-argument_list|,
-literal|"-v"
-argument_list|)
-condition|)
-block|{
-name|opts
-operator|.
-name|verbose_update
-operator|=
-literal|1
-expr_stmt|;
-continue|continue;
-block|}
-comment|/* "-i" means "index only", meaning that a merge will 		 * not even look at the working tree. 		 */
-if|if
-condition|(
-operator|!
-name|strcmp
-argument_list|(
-name|arg
-argument_list|,
-literal|"-i"
-argument_list|)
-condition|)
-block|{
-name|opts
-operator|.
-name|index_only
-operator|=
-literal|1
-expr_stmt|;
-continue|continue;
-block|}
-if|if
-condition|(
-operator|!
-name|prefixcmp
-argument_list|(
-name|arg
-argument_list|,
-literal|"--index-output="
-argument_list|)
-condition|)
-block|{
-name|set_alternate_index_output
-argument_list|(
-name|arg
-operator|+
-literal|15
-argument_list|)
-expr_stmt|;
-continue|continue;
-block|}
-comment|/* "--prefix=<subdirectory>/" means keep the current index 		 *  entries and put the entries from the tree under the 		 * given subdirectory. 		 */
-if|if
-condition|(
-operator|!
-name|prefixcmp
-argument_list|(
-name|arg
-argument_list|,
-literal|"--prefix="
-argument_list|)
-condition|)
-block|{
-if|if
-condition|(
-name|stage
-operator|||
-name|opts
-operator|.
-name|merge
-operator|||
-name|opts
-operator|.
-name|prefix
-condition|)
-name|usage
-argument_list|(
-name|read_tree_usage
-argument_list|)
-expr_stmt|;
-name|opts
-operator|.
-name|prefix
-operator|=
-name|arg
-operator|+
-literal|9
-expr_stmt|;
-name|opts
-operator|.
-name|merge
-operator|=
-literal|1
-expr_stmt|;
-name|stage
-operator|=
-literal|1
-expr_stmt|;
-if|if
-condition|(
-name|read_cache_unmerged
-argument_list|()
-condition|)
-name|die
-argument_list|(
-literal|"you need to resolve your current index first"
-argument_list|)
-expr_stmt|;
-continue|continue;
-block|}
-comment|/* This differs from "-m" in that we'll silently ignore 		 * unmerged entries and overwrite working tree files that 		 * correspond to them. 		 */
-if|if
-condition|(
-operator|!
-name|strcmp
-argument_list|(
-name|arg
-argument_list|,
-literal|"--reset"
-argument_list|)
-condition|)
-block|{
-if|if
-condition|(
-name|stage
-operator|||
-name|opts
-operator|.
-name|merge
-operator|||
-name|opts
-operator|.
-name|prefix
-condition|)
-name|usage
-argument_list|(
-name|read_tree_usage
-argument_list|)
-expr_stmt|;
-name|opts
-operator|.
-name|reset
-operator|=
-literal|1
-expr_stmt|;
-name|opts
-operator|.
-name|merge
-operator|=
-literal|1
-expr_stmt|;
-name|stage
-operator|=
-literal|1
-expr_stmt|;
-name|read_cache_unmerged
-argument_list|()
-expr_stmt|;
-continue|continue;
-block|}
-if|if
-condition|(
-operator|!
-name|strcmp
-argument_list|(
-name|arg
-argument_list|,
-literal|"--trivial"
-argument_list|)
-condition|)
-block|{
-name|opts
-operator|.
-name|trivial_merges_only
-operator|=
-literal|1
-expr_stmt|;
-continue|continue;
-block|}
-if|if
-condition|(
-operator|!
-name|strcmp
-argument_list|(
-name|arg
-argument_list|,
-literal|"--aggressive"
-argument_list|)
-condition|)
-block|{
-name|opts
-operator|.
-name|aggressive
-operator|=
-literal|1
-expr_stmt|;
-continue|continue;
-block|}
-comment|/* "-m" stands for "merge", meaning we start in stage 1 */
-if|if
-condition|(
-operator|!
-name|strcmp
-argument_list|(
-name|arg
-argument_list|,
-literal|"-m"
-argument_list|)
-condition|)
-block|{
-if|if
-condition|(
-name|stage
-operator|||
-name|opts
-operator|.
-name|merge
-operator|||
-name|opts
-operator|.
-name|prefix
-condition|)
-name|usage
-argument_list|(
-name|read_tree_usage
-argument_list|)
-expr_stmt|;
-if|if
-condition|(
-name|read_cache_unmerged
-argument_list|()
-condition|)
-name|die
-argument_list|(
-literal|"you need to resolve your current index first"
-argument_list|)
-expr_stmt|;
-name|stage
-operator|=
-literal|1
-expr_stmt|;
-name|opts
-operator|.
-name|merge
-operator|=
-literal|1
-expr_stmt|;
-continue|continue;
-block|}
-if|if
-condition|(
-operator|!
-name|prefixcmp
-argument_list|(
-name|arg
-argument_list|,
-literal|"--exclude-per-directory="
-argument_list|)
-condition|)
-block|{
-name|struct
-name|dir_struct
-modifier|*
-name|dir
-decl_stmt|;
-if|if
-condition|(
-name|opts
-operator|.
-name|dir
-condition|)
-name|die
-argument_list|(
-literal|"more than one --exclude-per-directory are given."
-argument_list|)
-expr_stmt|;
-name|dir
-operator|=
-name|xcalloc
-argument_list|(
-literal|1
-argument_list|,
-sizeof|sizeof
-argument_list|(
-operator|*
-name|opts
-operator|.
-name|dir
-argument_list|)
-argument_list|)
-expr_stmt|;
-name|dir
-operator|->
-name|flags
-operator||=
-name|DIR_SHOW_IGNORED
-expr_stmt|;
-name|dir
-operator|->
-name|exclude_per_dir
-operator|=
-name|arg
-operator|+
-literal|24
-expr_stmt|;
-name|opts
-operator|.
-name|dir
-operator|=
-name|dir
-expr_stmt|;
-comment|/* We do not need to nor want to do read-directory 			 * here; we are merely interested in reusing the 			 * per directory ignore stack mechanism. 			 */
-continue|continue;
-block|}
-if|if
-condition|(
-literal|1
-operator|<
-name|opts
-operator|.
-name|index_only
-operator|+
-name|opts
-operator|.
-name|update
-condition|)
-name|die
-argument_list|(
-literal|"-u and -i at the same time makes no sense"
-argument_list|)
-expr_stmt|;
 if|if
 condition|(
 name|get_sha1
@@ -670,6 +715,23 @@ name|stage
 operator|++
 expr_stmt|;
 block|}
+if|if
+condition|(
+literal|1
+operator|<
+name|opts
+operator|.
+name|index_only
+operator|+
+name|opts
+operator|.
+name|update
+condition|)
+name|die
+argument_list|(
+literal|"-u and -i at the same time makes no sense"
+argument_list|)
+expr_stmt|;
 if|if
 condition|(
 operator|(

unpack-trees.h
@@ -97,63 +97,39 @@ DECL|member|reset
 name|unsigned
 name|int
 name|reset
-range|:
-literal|1
 decl_stmt|,
 DECL|member|merge
 name|merge
-range|:
-literal|1
 decl_stmt|,
 DECL|member|update
 name|update
-range|:
-literal|1
 decl_stmt|,
 DECL|member|index_only
 name|index_only
-range|:
-literal|1
 decl_stmt|,
 DECL|member|nontrivial_merge
 name|nontrivial_merge
-range|:
-literal|1
 decl_stmt|,
 DECL|member|trivial_merges_only
 name|trivial_merges_only
-range|:
-literal|1
 decl_stmt|,
 DECL|member|verbose_update
 name|verbose_update
-range|:
-literal|1
 decl_stmt|,
 DECL|member|aggressive
 name|aggressive
-range|:
-literal|1
 decl_stmt|,
 DECL|member|skip_unmerged
 name|skip_unmerged
-range|:
-literal|1
 decl_stmt|,
 DECL|member|initial_checkout
 name|initial_checkout
-range|:
-literal|1
 decl_stmt|,
 DECL|member|diff_index_cached
 name|diff_index_cached
-range|:
-literal|1
 decl_stmt|,
 DECL|member|gently
 name|gently
-range|:
-literal|1
 decl_stmt|;
 DECL|member|prefix
 specifier|const

commit 672166189219023f4857e6ffa277c8310f472ed5

read-tree: Fix regression with creation of a new index file.
Reading the index into an empty file has been broken by
b106452cbef552d72d30b3578db06f665bb63529 [formerly 5a56da58060e50980fab0f4c38203a25440d1530], since it causes the existing
index to always be loaded first, and dies if it's an empty file:

$ GIT_INDEX_FILE=`mktemp` git read-tree master
fatal: index file smaller than expected

It breaks for instance committing from git.el. This patch reverts to the
previous behavior of only loading the index when merging it.

Signed-off-by: Alexandre Julliard <julliard@winehq.org>
Signed-off-by: Junio C Hamano <gitster@pobox.com>

Former-commit-id: db137fe91ea4afda7a2073364c610fe61dc769b8

builtin-read-tree.c
@@ -588,26 +588,6 @@ argument_list|,
 literal|0
 argument_list|)
 expr_stmt|;
-if|if
-condition|(
-name|read_cache_unmerged
-argument_list|()
-operator|&&
-operator|(
-name|opts
-operator|.
-name|prefix
-operator|||
-name|opts
-operator|.
-name|merge
-operator|)
-condition|)
-name|die
-argument_list|(
-literal|"You need to resolve your current index first"
-argument_list|)
-expr_stmt|;
 name|prefix_set
 operator|=
 name|opts
@@ -637,13 +617,8 @@ argument_list|(
 literal|"Which one? -m, --reset, or --prefix?"
 argument_list|)
 expr_stmt|;
-name|stage
-operator|=
-name|opts
-operator|.
-name|merge
-operator|=
-operator|(
+if|if
+condition|(
 name|opts
 operator|.
 name|reset
@@ -652,9 +627,40 @@ name|opts
 operator|.
 name|merge
 operator|||
-name|prefix_set
+name|opts
+operator|.
+name|prefix
+condition|)
+block|{
+if|if
+condition|(
+name|read_cache_unmerged
+argument_list|()
+operator|&&
+operator|(
+name|opts
+operator|.
+name|prefix
+operator|||
+name|opts
+operator|.
+name|merge
 operator|)
+condition|)
+name|die
+argument_list|(
+literal|"You need to resolve your current index first"
+argument_list|)
 expr_stmt|;
+name|stage
+operator|=
+name|opts
+operator|.
+name|merge
+operator|=
+literal|1
+expr_stmt|;
+block|}
 for|for
 control|(
 name|i

t/t1009-read-tree-new-index.sh
@@ -1,0 +1,25 @@
+#!/bin/sh
+
+test_description='test read-tree into a fresh index file'
+
+. ./test-lib.sh
+
+test_expect_success setup '
+	echo one >a &&
+	git add a &&
+	git commit -m initial
+'
+
+test_expect_success 'non-existent index file' '
+	rm -f new-index &&
+	GIT_INDEX_FILE=new-index git read-tree master
+'
+
+test_expect_success 'empty index file' '
+	rm -f new-index &&
+	> new-index &&
+	GIT_INDEX_FILE=new-index git read-tree master
+'
+
+test_done
+