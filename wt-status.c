4ce682b151178a8ccdaaf0c7d62f75e956f70d9d
Jeff King <peff@peff.net>
2009-09-09 07:43:03 -0400

    status: make "how to stage" messages optional
    These messages are nice for new users, but experienced git
    users know how to manipulate the index, and these messages
    waste a lot of screen real estate.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: edf563fbaa2ab50734db4a61e9092f25fbb5a417

--- wt-status.c
+++ wt-status.c
@@ -305,6 +305,12 @@ expr_stmt|;
 if|if
 condition|(
 operator|!
+name|advice_status_hints
+condition|)
+return|return;
+if|if
+condition|(
+operator|!
 name|s
 operator|->
 name|is_initial
@@ -399,6 +405,12 @@ expr_stmt|;
 if|if
 condition|(
 operator|!
+name|advice_status_hints
+condition|)
+return|return;
+if|if
+condition|(
+operator|!
 name|s
 operator|->
 name|is_initial
@@ -489,6 +501,12 @@ expr_stmt|;
 if|if
 condition|(
 operator|!
+name|advice_status_hints
+condition|)
+return|return;
+if|if
+condition|(
+operator|!
 name|has_deleted
 condition|)
 name|color_fprintf_ln
@@ -574,6 +592,12 @@ argument_list|,
 literal|"# Untracked files:"
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
+operator|!
+name|advice_status_hints
+condition|)
+return|return;
 name|color_fprintf_ln
 argument_list|(
 name|s
