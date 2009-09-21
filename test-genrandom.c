2e3613f5938d69330d0ac7436b3ae99668a776a4
Johannes Sixt <j6t@kdbg.org>
2009-09-21 09:34:58 +0200

    test-genrandom: ensure stdout is set to _O_BINARY on Windows
    Commit a6ca8c62 (Set _O_BINARY as default fmode for both MinGW and MSVC)
    removed the definition of _CRT_fmode from mingw.c. Before this commit,
    since test-genrandom is linked against libgit.a, the MinGW process
    initialization code would pick up that definition of _CRT_fmode, which was
    initialized to _O_BINARY. After this commit, however, text mode is used
    for std(in|out|err) because it is the default in absence of _CRT_fmode.
    In order to fix that, we must use git-compat-util.h, which overrides
    main() to set the mode to binary.
    
    Signed-off-by: Johannes Sixt <j6t@kdbg.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: a16753dc2e6f885fd30e570610c3e8cf355ecf04

--- test-genrandom.c
+++ test-genrandom.c
@@ -6,13 +6,7 @@ end_comment
 begin_include
 include|#
 directive|include
-file|<stdio.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<stdlib.h>
+file|"git-compat-util.h"
 end_include
 
 begin_function
