32a4d8265c73204fb0d293784e88dcf887ba3a7c
Jonathan Nieder <jrnieder@gmail.com>
2009-10-30 20:41:27 -0500

    Teach git var about GIT_PAGER
    Expose the command found by setup_pager() for scripts to use.
    Scripts can use this to avoid repeating the logic to look for a
    proper pager in each command.
    
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 6361824589bc2d32989a9a33f985d09a368436a3

--- cache.h
+++ cache.h
@@ -5336,6 +5336,18 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_function_decl
+specifier|extern
+specifier|const
+name|char
+modifier|*
+name|git_pager
+parameter_list|(
+name|void
+parameter_list|)
+function_decl|;
+end_function_decl
+
 begin_struct
 DECL|struct|checkout
 struct|struct
