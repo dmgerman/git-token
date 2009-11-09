95ab978e22a76ff090b6280325686beca722a7f2
Jonathan Nieder <jrnieder@gmail.com>
2009-11-09 09:05:00 -0600

    http-fetch: add missing initialization of argv0_path
    According to c6dfb39 (remote-curl: add missing initialization of
    argv0_path, 2009-10-13), programs with "main" must call this to
    work correctly on MinGW.
    
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: f01d74960363736caaf4d98d39555e99bdc72c25

--- http-fetch.c
+++ http-fetch.c
@@ -8,6 +8,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|"exec_cmd.h"
+end_include
+
+begin_include
+include|#
+directive|include
 file|"walker.h"
 end_include
 
@@ -103,6 +109,14 @@ name|get_recover
 init|=
 literal|0
 decl_stmt|;
+name|git_extract_argv0_path
+argument_list|(
+name|argv
+index|[
+literal|0
+index|]
+argument_list|)
+expr_stmt|;
 name|prefix
 operator|=
 name|setup_git_directory
