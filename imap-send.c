8cd02298707ecea2450f8263513804e3c9b55f37
Jonathan Nieder <jrnieder@gmail.com>
2009-11-09 09:04:51 -0600

    Show usage string for 'git imap-send -h'
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 9a2861e32a147d89f22961bd7d3257b8776de4d2

--- imap-send.c
+++ imap-send.c
@@ -356,6 +356,18 @@ block|}
 struct|;
 end_struct
 
+begin_decl_stmt
+DECL|variable|imap_send_usage
+specifier|static
+specifier|const
+name|char
+name|imap_send_usage
+index|[]
+init|=
+literal|"git imap-send<<mbox>"
+decl_stmt|;
+end_decl_stmt
+
 begin_define
 DECL|macro|DRV_OK
 define|#
@@ -8654,6 +8666,17 @@ name|argv
 index|[
 literal|0
 index|]
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|argc
+operator|!=
+literal|1
+condition|)
+name|usage
+argument_list|(
+name|imap_send_usage
 argument_list|)
 expr_stmt|;
 comment|/* init the random number generator */
