c668548d2cce8a4ed03b2ec818640b12d7ac760c
Tay Ray Chuan <rctay89@gmail.com>
2009-12-04 07:31:44 +0800

    builtin-push: don't access freed transport->url
    Move the failed push message to before transport_disconnect() so that
    it doesn't access transport->url after transport has been free()'d (in
    transport_disconnect()).
    
    Additionally, make the failed push message more accurate by moving it
    before transport_disconnect(), so that it doesn't report errors due
    to a failed disconnect.
    
    Signed-off-by: Tay Ray Chuan <rctay89@gmail.com>
    Acked-by: Daniel Barkalow <barkalow@iabervon.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 53970b92d9dc883669f3a9b79b5e39e73931b331

--- builtin-push.c
+++ builtin-push.c
@@ -514,6 +514,21 @@ operator|&
 name|nonfastforward
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
+name|err
+operator|!=
+literal|0
+condition|)
+name|error
+argument_list|(
+literal|"failed to push some refs to '%s'"
+argument_list|,
+name|transport
+operator|->
+name|url
+argument_list|)
+expr_stmt|;
 name|err
 operator||=
 name|transport_disconnect
@@ -529,15 +544,6 @@ condition|)
 return|return
 literal|0
 return|;
-name|error
-argument_list|(
-literal|"failed to push some refs to '%s'"
-argument_list|,
-name|transport
-operator|->
-name|url
-argument_list|)
-expr_stmt|;
 if|if
 condition|(
 name|nonfastforward
