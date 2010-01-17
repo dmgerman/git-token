3d03a859e87fe3ae5249b600c85146249527ae24
Jens Lehmann <Jens.Lehmann@web.de>
2010-01-17 20:42:31 +0100

    git status: Show uncommitted submodule changes too when enabled
    When the configuration variable status.submodulesummary is not 0 or
    false, "git status" shows the submodule summary of the staged submodule
    commits. But it did not show the summary of those commits not yet
    staged in the supermodule, making it hard to see what will not be
    committed.
    
    The output of "submodule summary --for-status" has been changed from
    "# Modified submodules:" to "# Submodule changes to be committed:" for
    the already staged changes. "# Submodules changed but not updated:" has
    been added for changes that will not be committed. This is much clearer
    and consistent with the output for regular files.
    
    Signed-off-by: Jens Lehmann <Jens.Lehmann@web.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: f17a5d34948363087db94a1cb2c3c715c1ada2d8

--- wt-status.c
+++ wt-status.c
@@ -2645,6 +2645,9 @@ name|struct
 name|wt_status
 modifier|*
 name|s
+parameter_list|,
+name|int
+name|uncommitted
 parameter_list|)
 block|{
 name|struct
@@ -2686,6 +2689,10 @@ literal|"submodule"
 block|,
 literal|"summary"
 block|,
+name|uncommitted
+operator|?
+literal|"--files"
+operator|:
 literal|"--cached"
 block|,
 literal|"--for-status"
@@ -2694,6 +2701,11 @@ literal|"--summary-limit"
 block|,
 name|summary_limit
 block|,
+name|uncommitted
+operator|?
+name|NULL
+operator|:
+operator|(
 name|s
 operator|->
 name|amend
@@ -2701,6 +2713,7 @@ operator|?
 literal|"HEAD^"
 operator|:
 literal|"HEAD"
+operator|)
 block|,
 name|NULL
 block|}
@@ -3416,11 +3429,24 @@ name|s
 operator|->
 name|submodule_summary
 condition|)
+block|{
 name|wt_status_print_submodule_summary
 argument_list|(
 name|s
+argument_list|,
+literal|0
 argument_list|)
 expr_stmt|;
+comment|/* staged */
+name|wt_status_print_submodule_summary
+argument_list|(
+name|s
+argument_list|,
+literal|1
+argument_list|)
+expr_stmt|;
+comment|/* unstaged */
+block|}
 if|if
 condition|(
 name|s
