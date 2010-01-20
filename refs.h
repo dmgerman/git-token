92f4efb1486101a31a87d8028b2cc060edcca167
Ilari Liusvaara <ilari.liusvaara@elisanet.fi>
2010-01-20 11:48:25 +0200

    rev-parse --glob
    Add --glob=<glob-pattern> option to rev-parse and everything that
    accepts its options. This option matches all refs that match given
    shell glob pattern (complete with some DWIM logic).
    
    Example:
    
    'git log --branches --not --glob=remotes/origin'
    
    To show what you have that origin doesn't.
    
    Signed-off-by: Ilari Liusvaara <ilari.liusvaara@elisanet.fi>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: d08bae7e221727e26baab984b792854b842130d7

--- refs.h
+++ refs.h
@@ -195,6 +195,24 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_function_decl
+specifier|extern
+name|int
+name|for_each_glob_ref
+parameter_list|(
+name|each_ref_fn
+parameter_list|,
+specifier|const
+name|char
+modifier|*
+name|pattern
+parameter_list|,
+name|void
+modifier|*
+parameter_list|)
+function_decl|;
+end_function_decl
+
 begin_comment
 comment|/* can be used to learn about broken ref and symref */
 end_comment
