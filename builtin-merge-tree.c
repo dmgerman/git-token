1e1e0b1871157a050a143a258d8d96c83b5f2c99
Johannes Sixt <j6t@kdbg.org>
2010-01-22 12:47:59 +0100

    merge-tree: remove unnecessary call of git_extract_argv0_path
    This call should have been removed when the utility was made a builtin by
    907a7cb.
    
    Signed-off-by: Johannes Sixt <j6t@kdbg.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 07c073206798483c9916d492dfc6faf7400a50ad

--- builtin-merge-tree.c
+++ builtin-merge-tree.c
@@ -1864,14 +1864,6 @@ argument_list|(
 name|merge_tree_usage
 argument_list|)
 expr_stmt|;
-name|git_extract_argv0_path
-argument_list|(
-name|argv
-index|[
-literal|0
-index|]
-argument_list|)
-expr_stmt|;
 name|buf1
 operator|=
 name|get_tree_descriptor
