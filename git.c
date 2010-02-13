2dd577d6a46939a97cdb5d8a0926fc818416aa65
Johan Herland <johan@herland.net>
2010-02-13 22:28:20 +0100

    Builtin-ify git-notes
    The builtin-ification includes some minor behavioural changes to the
    command-line interface: It is no longer allowed to mix the -m and -F
    arguments, and it is not allowed to use multiple -F options.
    
    As part of the builtin-ification, we add the commit_notes() function
    to the builtin API. This function (together with the notes.h API) can
    be easily used from other builtins to manipulate the notes tree.
    
    Also includes needed changes to t3301.
    
    This patch has been improved by the following contributions:
    - Stephen Boyd: Use die() instead of fprintf(stderr, ...) followed by exit(1)
    
    Cc: Stephen Boyd <bebarino@gmail.com>
    Signed-off-by: Johan Herland <johan@herland.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: cd067d3bf4ea3f89969cd143be3e281e1c5ac58a

--- git.c
+++ git.c
@@ -2035,6 +2035,14 @@ name|RUN_SETUP
 block|}
 block|,
 block|{
+literal|"notes"
+block|,
+name|cmd_notes
+block|,
+name|RUN_SETUP
+block|}
+block|,
+block|{
 literal|"pack-objects"
 block|,
 name|cmd_pack_objects
