a04d62d34d77fb28bf845a47efc4b55e6fbf18ab
Junio C Hamano <gitster@pobox.com>
2010-02-17 12:30:41 -0800

    status: preload index to optimize lstat(2) calls
    Noticed by James Pickens
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 149794dd1db18b22f4df94ef13cf61520d7be1d8

--- builtin-commit.c
+++ builtin-commit.c
@@ -5539,8 +5539,12 @@ argument_list|,
 name|argv
 argument_list|)
 expr_stmt|;
-name|read_cache
-argument_list|()
+name|read_cache_preload
+argument_list|(
+name|s
+operator|.
+name|pathspec
+argument_list|)
 expr_stmt|;
 name|refresh_index
 argument_list|(
