12389752c05d3e7e9a80e95f9b9adb8f765b4455
Junio C Hamano <gitster@pobox.com>
2010-03-08 22:58:09 -0800

    revision: introduce setup_revision_opt
    So far the last parameter to setup_revisions() was to specify the default
    ref when the command line did not give any (typically "HEAD").  This changes
    it to take a pointer to a structure so that we can add other information without
    touching too many codepaths in later patches.
    
    There is no functionality change.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 32962c9bd5149005b163dff230670872eb99286a

--- revision.c
+++ revision.c
@@ -7687,10 +7687,10 @@ name|rev_info
 modifier|*
 name|revs
 parameter_list|,
-specifier|const
-name|char
+name|struct
+name|setup_revision_opt
 modifier|*
-name|def
+name|opt
 parameter_list|)
 block|{
 name|int
@@ -8401,7 +8401,13 @@ name|revs
 operator|->
 name|def
 operator|=
+name|opt
+condition|?
+name|opt
+operator|->
 name|def
+else|:
+name|NULL
 expr_stmt|;
 if|if
 condition|(
