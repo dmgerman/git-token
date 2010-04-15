d041c53510fa3368cdd19eeae61c660c475b7956
Thomas Rast <trast@student.ethz.ch>
2010-04-15 14:59:37 +0200

    combined diff: correctly handle truncated file
    Consider an evil merge of two commits A and B, both of which have a
    file 'foo', but the merge result does not have that file.
    
    The combined-diff code learned in 4462731 (combine-diff: do not punt
    on removed or added files., 2006-02-06) to concisely show only the
    removal, since that is the evil part and the previous contents are
    presumably uninteresting.
    
    However, to diagnose an empty merge result, it overloaded the variable
    that holds the file's length.  This means that the check also triggers
    for truncated files.  Consequently, such files were not shown in the
    diff at all despite the merge being clearly evil.
    
    Fix this by adding a new variable that distinguishes whether the file
    was deleted (which is the case 4462731 handled) or truncated.  In the
    truncated case, we show the full combined diff again, which is rather
    spammy but at least does not hide the evilness.
    
    Reported-by: David Martínez Martí <desarrollo@gestiweb.com>
    Signed-off-by: Thomas Rast <trast@student.ethz.ch>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 21798708031ed808cb77232e771e20d3146cf9c8

--- combine-diff.c
+++ combine-diff.c
@@ -1329,6 +1329,9 @@ name|n
 parameter_list|,
 name|int
 name|num_parent
+parameter_list|,
+name|int
+name|result_deleted
 parameter_list|)
 block|{
 name|unsigned
@@ -1369,8 +1372,7 @@ name|sz
 decl_stmt|;
 if|if
 condition|(
-operator|!
-name|cnt
+name|result_deleted
 condition|)
 return|return;
 comment|/* result deleted */
@@ -2749,6 +2751,9 @@ name|num_parent
 parameter_list|,
 name|int
 name|use_color
+parameter_list|,
+name|int
+name|result_deleted
 parameter_list|)
 block|{
 name|unsigned
@@ -2854,8 +2859,7 @@ argument_list|)
 decl_stmt|;
 if|if
 condition|(
-operator|!
-name|cnt
+name|result_deleted
 condition|)
 return|return;
 comment|/* result deleted */
@@ -3718,6 +3722,11 @@ name|cnt
 decl_stmt|,
 name|lno
 decl_stmt|;
+name|int
+name|result_deleted
+init|=
+literal|0
+decl_stmt|;
 name|char
 modifier|*
 name|result
@@ -4220,6 +4229,10 @@ else|else
 block|{
 name|deleted_file
 label|:
+name|result_deleted
+operator|=
+literal|1
+expr_stmt|;
 name|result_size
 operator|=
 literal|0
@@ -4642,6 +4655,8 @@ argument_list|,
 name|i
 argument_list|,
 name|num_parent
+argument_list|,
+name|result_deleted
 argument_list|)
 expr_stmt|;
 if|if
@@ -5063,6 +5078,8 @@ name|opt
 argument_list|,
 name|COLOR_DIFF
 argument_list|)
+argument_list|,
+name|result_deleted
 argument_list|)
 expr_stmt|;
 block|}
