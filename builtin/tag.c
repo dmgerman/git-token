dfa7ba9cb4cf269dce90fbe35a873534b7529316
Jonathan Nieder <jrnieder@gmail.com>
2010-04-15 04:36:25 -0500

    tag -v: use RUN_GIT_CMD to run verify-tag
    This is the preferred way to run a git command.
    
    The only obvious observable effects I can think of are that the exec
    is properly reported in GIT_TRACE output and that verifying signed
    tags will still work if the git-verify-tag hard link in gitexecdir
    goes missing.
    
    Helped-by: Johannes Sixt <j6t@kdbg.org>
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: a6ccbbdb66be4e4b90bd54fc644d6366491996b5

--- builtin/tag.c
+++ builtin/tag.c
@@ -745,7 +745,7 @@ name|argv_verify_tag
 index|[]
 init|=
 block|{
-literal|"git-verify-tag"
+literal|"verify-tag"
 block|,
 literal|"-v"
 block|,
@@ -770,7 +770,7 @@ name|run_command_v_opt
 argument_list|(
 name|argv_verify_tag
 argument_list|,
-literal|0
+name|RUN_GIT_CMD
 argument_list|)
 condition|)
 return|return
