dc7aa267e4a335cb00d520a1274af15c751323a4
Bo Yang <struggleyb.nku@gmail.com>
2010-05-06 21:52:27 -0700

    Add a macro DIFF_QUEUE_CLEAR.
    Refactor the diff_queue_struct code, this macro help
    to reset the structure.
    
    Signed-off-by: Bo Yang <struggleyb.nku@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 9ca5df90615aa3c6b60e1bc8f03db6cae98e816c

--- diffcore.h
+++ diffcore.h
@@ -462,6 +462,18 @@ block|}
 struct|;
 end_struct
 
+begin_define
+DECL|macro|DIFF_QUEUE_CLEAR
+define|#
+directive|define
+name|DIFF_QUEUE_CLEAR
+parameter_list|(
+name|q
+parameter_list|)
+define|\
+value|do { \ 		(q)->queue = NULL; \ 		(q)->nr = (q)->alloc = 0; \ 	} while(0);
+end_define
+
 begin_decl_stmt
 specifier|extern
 name|struct
