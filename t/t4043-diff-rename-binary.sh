dmgerman/git-token

commit 719db850880f7d9d3cee1c783540e66b153e48ff

No diff -b/-w output for all-whitespace changes
Change git-diff's whitespace-ignoring modes to generate
output only if a non-empty patch results, which git-apply
rejects.

Update the tests to look for the new behavior.

Signed-off-by: Greg Bacon <gbacon@dbresearch.net>
Signed-off-by: Junio C Hamano <gitster@pobox.com>

Former-commit-id: 3e97c7c6af2901cec63bf35fcd43ae3472e24af8

diff.c
@@ -1117,6 +1117,12 @@ name|FILE
 modifier|*
 name|file
 decl_stmt|;
+DECL|member|header
+name|struct
+name|strbuf
+modifier|*
+name|header
+decl_stmt|;
 block|}
 struct|;
 end_struct
@@ -4696,6 +4702,42 @@ argument_list|,
 name|DIFF_RESET
 argument_list|)
 decl_stmt|;
+if|if
+condition|(
+name|ecbdata
+operator|->
+name|header
+condition|)
+block|{
+name|fprintf
+argument_list|(
+name|ecbdata
+operator|->
+name|file
+argument_list|,
+literal|"%s"
+argument_list|,
+name|ecbdata
+operator|->
+name|header
+operator|->
+name|buf
+argument_list|)
+expr_stmt|;
+name|strbuf_reset
+argument_list|(
+name|ecbdata
+operator|->
+name|header
+argument_list|)
+expr_stmt|;
+name|ecbdata
+operator|->
+name|header
+operator|=
+name|NULL
+expr_stmt|;
+block|}
 operator|*
 operator|(
 name|ecbdata
@@ -9455,6 +9497,12 @@ name|textconv_two
 init|=
 name|NULL
 decl_stmt|;
+name|struct
+name|strbuf
+name|header
+init|=
+name|STRBUF_INIT
+decl_stmt|;
 if|if
 condition|(
 name|DIFF_OPT_TST
@@ -9705,11 +9753,10 @@ name|b_two
 else|:
 literal|"/dev/null"
 expr_stmt|;
-name|fprintf
+name|strbuf_addf
 argument_list|(
-name|o
-operator|->
-name|file
+operator|&
+name|header
 argument_list|,
 literal|"%sdiff --git %s %s%s\n"
 argument_list|,
@@ -9736,11 +9783,10 @@ literal|'/'
 condition|)
 block|{
 comment|/* /dev/null */
-name|fprintf
+name|strbuf_addf
 argument_list|(
-name|o
-operator|->
-name|file
+operator|&
+name|header
 argument_list|,
 literal|"%snew file mode %06o%s\n"
 argument_list|,
@@ -9762,11 +9808,10 @@ index|[
 literal|0
 index|]
 condition|)
-name|fprintf
+name|strbuf_addf
 argument_list|(
-name|o
-operator|->
-name|file
+operator|&
+name|header
 argument_list|,
 literal|"%s%s%s\n"
 argument_list|,
@@ -9792,11 +9837,10 @@ operator|==
 literal|'/'
 condition|)
 block|{
-name|fprintf
+name|strbuf_addf
 argument_list|(
-name|o
-operator|->
-name|file
+operator|&
+name|header
 argument_list|,
 literal|"%sdeleted file mode %06o%s\n"
 argument_list|,
@@ -9818,11 +9862,10 @@ index|[
 literal|0
 index|]
 condition|)
-name|fprintf
+name|strbuf_addf
 argument_list|(
-name|o
-operator|->
-name|file
+operator|&
+name|header
 argument_list|,
 literal|"%s%s%s\n"
 argument_list|,
@@ -9847,11 +9890,10 @@ operator|->
 name|mode
 condition|)
 block|{
-name|fprintf
+name|strbuf_addf
 argument_list|(
-name|o
-operator|->
-name|file
+operator|&
+name|header
 argument_list|,
 literal|"%sold mode %06o%s\n"
 argument_list|,
@@ -9864,11 +9906,10 @@ argument_list|,
 name|reset
 argument_list|)
 expr_stmt|;
-name|fprintf
+name|strbuf_addf
 argument_list|(
-name|o
-operator|->
-name|file
+operator|&
+name|header
 argument_list|,
 literal|"%snew mode %06o%s\n"
 argument_list|,
@@ -9891,11 +9932,10 @@ index|[
 literal|0
 index|]
 condition|)
-name|fprintf
+name|strbuf_addf
 argument_list|(
-name|o
-operator|->
-name|file
+operator|&
+name|header
 argument_list|,
 literal|"%s%s%s\n"
 argument_list|,
@@ -9949,6 +9989,25 @@ argument_list|)
 operator|)
 condition|)
 block|{
+name|fprintf
+argument_list|(
+name|o
+operator|->
+name|file
+argument_list|,
+literal|"%s"
+argument_list|,
+name|header
+operator|.
+name|buf
+argument_list|)
+expr_stmt|;
+name|strbuf_reset
+argument_list|(
+operator|&
+name|header
+argument_list|)
+expr_stmt|;
 name|emit_rewrite_diff
 argument_list|(
 name|name_a
@@ -10067,6 +10126,25 @@ condition|)
 goto|goto
 name|free_ab_and_return
 goto|;
+name|fprintf
+argument_list|(
+name|o
+operator|->
+name|file
+argument_list|,
+literal|"%s"
+argument_list|,
+name|header
+operator|.
+name|buf
+argument_list|)
+expr_stmt|;
+name|strbuf_reset
+argument_list|(
+operator|&
+name|header
+argument_list|)
+expr_stmt|;
 if|if
 condition|(
 name|DIFF_OPT_TST
@@ -10148,6 +10226,37 @@ name|userdiff_funcname
 modifier|*
 name|pe
 decl_stmt|;
+if|if
+condition|(
+operator|!
+name|DIFF_XDL_TST
+argument_list|(
+name|o
+argument_list|,
+name|WHITESPACE_FLAGS
+argument_list|)
+condition|)
+block|{
+name|fprintf
+argument_list|(
+name|o
+operator|->
+name|file
+argument_list|,
+literal|"%s"
+argument_list|,
+name|header
+operator|.
+name|buf
+argument_list|)
+expr_stmt|;
+name|strbuf_reset
+argument_list|(
+operator|&
+name|header
+argument_list|)
+expr_stmt|;
+block|}
 if|if
 condition|(
 name|textconv_one
@@ -10354,6 +10463,19 @@ operator|=
 name|o
 operator|->
 name|file
+expr_stmt|;
+name|ecbdata
+operator|.
+name|header
+operator|=
+name|header
+operator|.
+name|len
+condition|?
+operator|&
+name|header
+else|:
+name|NULL
 expr_stmt|;
 name|xpp
 operator|.
@@ -10667,6 +10789,12 @@ expr_stmt|;
 block|}
 name|free_ab_and_return
 label|:
+name|strbuf_release
+argument_list|(
+operator|&
+name|header
+argument_list|)
+expr_stmt|;
 name|diff_free_filespec_data
 argument_list|(
 name|one

t/t4015-diff-whitespace.sh
@@ -93,8 +93,6 @@ git diff > out
 test_expect_success 'another test, without options' 'test_cmp expect out'
 
 cat << EOF > expect
-diff --git a/x b/x
-index d99af23..8b32fb5 100644
 EOF
 git diff -w > out
 test_expect_success 'another test, with -w' 'test_cmp expect out'
@@ -384,6 +382,18 @@ test_expect_success 'checkdiff allows new blank lines' '
 		cat y
 	) >x &&
 	git diff --check
+'
+
+cat <<EOF >expect
+EOF
+test_expect_success 'whitespace-only changes not reported' '
+	git reset --hard &&
+	echo >x "hello world" &&
+	git add x &&
+	git commit -m "hello 1" &&
+	echo >x "hello  world" &&
+	git diff -b >actual &&
+	test_cmp expect actual
 '
 
 test_expect_success 'combined diff with autocrlf conversion' '

commit a713bf46e740f0a30839518c970960fb4bad4eb0

diff: fix "git show -C -C" output when renaming a binary file
A bug was introduced in 719db850880f7d9d3cee1c783540e66b153e48ff [formerly 3e97c7c6af2901cec63bf35fcd43ae3472e24af8]
(No diff -b/-w output for all-whitespace changes, Nov 19 2009)
that made the lines:

  diff --git a/bar b/sub/bar
  similarity index 100%
  rename from bar
  rename to sub/bar

disappear from "git show -C -C" output when file bar is a binary
file.

Signed-off-by: Christian Couder <chriscool@tuxfamily.org>
Signed-off-by: Junio C Hamano <gitster@pobox.com>

Former-commit-id: 296c6bb21a6980f6e5b42f0790d6365c1e3f696f

diff.c
@@ -9704,6 +9704,9 @@ name|char
 modifier|*
 name|xfrm_msg
 parameter_list|,
+name|int
+name|must_show_header
+parameter_list|,
 name|struct
 name|diff_options
 modifier|*
@@ -10106,6 +10109,10 @@ argument_list|,
 name|reset
 argument_list|)
 expr_stmt|;
+name|must_show_header
+operator|=
+literal|1
+expr_stmt|;
 block|}
 elseif|else
 if|if
@@ -10160,6 +10167,10 @@ argument_list|,
 name|reset
 argument_list|)
 expr_stmt|;
+name|must_show_header
+operator|=
+literal|1
+expr_stmt|;
 block|}
 else|else
 block|{
@@ -10205,6 +10216,10 @@ name|mode
 argument_list|,
 name|reset
 argument_list|)
+expr_stmt|;
+name|must_show_header
+operator|=
+literal|1
 expr_stmt|;
 block|}
 if|if
@@ -10407,9 +10422,28 @@ operator|.
 name|size
 argument_list|)
 condition|)
+block|{
+if|if
+condition|(
+name|must_show_header
+condition|)
+name|fprintf
+argument_list|(
+name|o
+operator|->
+name|file
+argument_list|,
+literal|"%s"
+argument_list|,
+name|header
+operator|.
+name|buf
+argument_list|)
+expr_stmt|;
 goto|goto
 name|free_ab_and_return
 goto|;
+block|}
 name|fprintf
 argument_list|(
 name|o
@@ -10516,6 +10550,8 @@ name|o
 argument_list|,
 name|WHITESPACE_FLAGS
 argument_list|)
+operator|||
+name|must_show_header
 condition|)
 block|{
 name|fprintf
@@ -13728,8 +13764,17 @@ name|struct
 name|diff_filepair
 modifier|*
 name|p
+parameter_list|,
+name|int
+modifier|*
+name|must_show_header
 parameter_list|)
 block|{
+operator|*
+name|must_show_header
+operator|=
+literal|1
+expr_stmt|;
 name|strbuf_init
 argument_list|(
 name|msg
@@ -13893,7 +13938,11 @@ block|}
 comment|/* fallthru */
 default|default:
 comment|/* nothing */
-empty_stmt|;
+operator|*
+name|must_show_header
+operator|=
+literal|0
+expr_stmt|;
 block|}
 if|if
 condition|(
@@ -14127,6 +14176,11 @@ name|p
 operator|->
 name|score
 decl_stmt|;
+name|int
+name|must_show_header
+init|=
+literal|0
+decl_stmt|;
 if|if
 condition|(
 name|msg
@@ -14147,6 +14201,9 @@ argument_list|,
 name|o
 argument_list|,
 name|p
+argument_list|,
+operator|&
+name|must_show_header
 argument_list|)
 expr_stmt|;
 name|xfrm_msg
@@ -14248,6 +14305,8 @@ argument_list|,
 name|two
 argument_list|,
 name|xfrm_msg
+argument_list|,
+name|must_show_header
 argument_list|,
 name|o
 argument_list|,

t/t4015-diff-whitespace.sh
@@ -396,6 +396,43 @@ test_expect_success 'whitespace-only changes not reported' '
 	test_cmp expect actual
 '
 
+cat <<EOF >expect
+diff --git a/x b/z
+similarity index NUM%
+rename from x
+rename to z
+index 380c32a..a97b785 100644
+EOF
+test_expect_success 'whitespace-only changes reported across renames' '
+	git reset --hard &&
+	for i in 1 2 3 4 5 6 7 8 9; do echo "$i$i$i$i$i$i"; done >x &&
+	git add x &&
+	git commit -m "base" &&
+	sed -e "5s/^/ /" x >z &&
+	git rm x &&
+	git add z &&
+	git diff -w -M --cached |
+	sed -e "/^similarity index /s/[0-9][0-9]*/NUM/" >actual &&
+	test_cmp expect actual
+'
+
+cat >expected <<\EOF
+diff --git a/empty b/void
+similarity index 100%
+rename from empty
+rename to void
+EOF
+
+test_expect_success 'rename empty' '
+	git reset --hard &&
+	>empty &&
+	git add empty &&
+	git commit -m empty &&
+	git mv empty void &&
+	git diff -w --cached -M >current &&
+	test_cmp expected current
+'
+
 test_expect_success 'combined diff with autocrlf conversion' '
 
 	git reset --hard &&

t/t4043-diff-rename-binary.sh
@@ -1,0 +1,45 @@
+#!/bin/sh
+#
+# Copyright (c) 2010 Jakub Narebski, Christian Couder
+#
+
+test_description='Move a binary file'
+
+. ./test-lib.sh
+
+
+test_expect_success 'prepare repository' '
+	git init &&
+	echo foo > foo &&
+	echo "barQ" | q_to_nul > bar &&
+	git add . &&
+	git commit -m "Initial commit"
+'
+
+test_expect_success 'move the files into a "sub" directory' '
+	mkdir sub &&
+	git mv bar foo sub/ &&
+	git commit -m "Moved to sub/"
+'
+
+cat > expected <<\EOF
+ bar => sub/bar |  Bin 5 -> 5 bytes
+ foo => sub/foo |    0
+ 2 files changed, 0 insertions(+), 0 deletions(-)
+
+diff --git a/bar b/sub/bar
+similarity index 100%
+rename from bar
+rename to sub/bar
+diff --git a/foo b/sub/foo
+similarity index 100%
+rename from foo
+rename to sub/foo
+EOF
+
+test_expect_success 'git show -C -C report renames' '
+	git show -C -C --raw --binary --stat | tail -n 12 > current &&
+	test_cmp expected current
+'
+
+test_done