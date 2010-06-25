e3a385ae6ee155aa2e66658803a2f0769a319f44
Jens Lehmann <Jens.Lehmann@web.de>
2010-06-25 16:56:47 +0200

    Add the option "--ignore-submodules" to "git status"
    In some use cases it is not desirable that "git status" considers
    submodules that only contain untracked content as dirty. This may happen
    e.g. when the submodule is not under the developers control and not all
    build generated files have been added to .gitignore by the upstream
    developers. Using the "untracked" parameter for the "--ignore-submodules"
    option disables checking for untracked content and lets git diff report
    them as changed only when they have new commits or modified content.
    
    Sometimes it is not wanted to have submodules show up as changed when they
    just contain changes to their work tree (this was the behavior before
    1.7.0). An example for that are scripts which just want to check for
    submodule commits while ignoring any changes to the work tree. Also users
    having large submodules known not to change might want to use this option,
    as the - sometimes substantial - time it takes to scan the submodule work
    tree(s) is saved when using the "dirty" parameter.
    
    And if you want to ignore any changes to submodules, you can now do that
    by using this option without parameters or with "all" (when the config
    option status.submodulesummary is set, using "all" will also suppress the
    output of the submodule summary).
    
    A new function handle_ignore_submodules_arg() is introduced to parse this
    option new to "git status" in a single location, as "git diff" already
    knew it.
    
    Signed-off-by: Jens Lehmann <Jens.Lehmann@web.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 46a958b3daa1da336683ec82d7f321d0f51b39c8

--- builtin/commit.c
+++ builtin/commit.c
@@ -375,6 +375,7 @@ end_decl_stmt
 begin_decl_stmt
 DECL|variable|untracked_files_arg
 DECL|variable|force_date
+DECL|variable|ignore_submodule_arg
 specifier|static
 name|char
 modifier|*
@@ -382,6 +383,9 @@ name|untracked_files_arg
 decl_stmt|,
 modifier|*
 name|force_date
+decl_stmt|,
+modifier|*
+name|ignore_submodule_arg
 decl_stmt|;
 end_decl_stmt
 
@@ -5479,6 +5483,30 @@ operator|)
 literal|"all"
 block|}
 block|,
+block|{
+name|OPTION_STRING
+block|,
+literal|0
+block|,
+literal|"ignore-submodules"
+block|,
+operator|&
+name|ignore_submodule_arg
+block|,
+literal|"when"
+block|,
+literal|"ignore changes to submodules, optional when: all, dirty, untracked. (Default: all)"
+block|,
+name|PARSE_OPT_OPTARG
+block|,
+name|NULL
+block|,
+operator|(
+name|intptr_t
+operator|)
+literal|"all"
+block|}
+block|,
 name|OPT_END
 argument_list|()
 block|, 	}
@@ -5606,6 +5634,12 @@ name|in_merge
 operator|=
 name|in_merge
 expr_stmt|;
+name|s
+operator|.
+name|ignore_submodule_arg
+operator|=
+name|ignore_submodule_arg
+expr_stmt|;
 name|wt_status_collect
 argument_list|(
 operator|&
@@ -5687,6 +5721,12 @@ operator|.
 name|verbose
 operator|=
 name|verbose
+expr_stmt|;
+name|s
+operator|.
+name|ignore_submodule_arg
+operator|=
+name|ignore_submodule_arg
 expr_stmt|;
 name|wt_status_print
 argument_list|(
