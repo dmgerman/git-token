18527736b87071e778edf990aaff5d648b7dce0a
Brandon Casey <drafnel@gmail.com>
2010-09-09 13:56:36 -0500

    builtin/fetch.c: comment that branch->remote_name is usable when has_merge
    Save future readers the trouble of tracing code to determine that the two
    uses of branch->remote_name are safe when has_merge is set, by adding a
    comment explaining that it is so.
    
    Signed-off-by: Brandon Casey <casey@nrlssc.navy.mil>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: f31dbdc7dad3511e1ee73cb04a032773d11231cd

--- builtin/fetch.c
+++ builtin/fetch.c
@@ -844,6 +844,7 @@ name|remote
 operator|->
 name|fetch_refspec_nr
 operator|||
+comment|/* Note: has_merge implies non-NULL branch->remote_name */
 operator|(
 name|has_merge
 operator|&&
@@ -951,7 +952,7 @@ operator|=
 literal|1
 expr_stmt|;
 block|}
-comment|/* 			 * if the remote we're fetching from is the same 			 * as given in branch.<name>.remote, we add the 			 * ref given in branch.<name>.merge, too. 			 */
+comment|/* 			 * if the remote we're fetching from is the same 			 * as given in branch.<name>.remote, we add the 			 * ref given in branch.<name>.merge, too. 			 * 			 * Note: has_merge implies non-NULL branch->remote_name 			 */
 if|if
 condition|(
 name|has_merge
