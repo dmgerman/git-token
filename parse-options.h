8eb5d2ffb05a11f6023a1f746cea7865e580f29e
René Scharfe <rene.scharfe@lsrfire.ath.cx>
2010-11-08 18:58:51 +0100

    add description parameter to OPT__DRY_RUN
    Allows better help text to be defined than "dry run".  Also make use
    of the macro in places that already had a different description.  No
    object code changes intended.
    
    Signed-off-by: Rene Scharfe <rene.scharfe@lsrfire.ath.cx>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: e21adb8c100cd2859cff1d74f7d0e622ac640085

--- parse-options.h
+++ parse-options.h
@@ -918,8 +918,10 @@ directive|define
 name|OPT__DRY_RUN
 parameter_list|(
 name|var
+parameter_list|,
+name|h
 parameter_list|)
-value|OPT_BOOLEAN('n', "dry-run", (var), "dry run")
+value|OPT_BOOLEAN('n', "dry-run", (var), (h))
 end_define
 
 begin_define
