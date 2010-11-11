e8d64ba44307366cfef4ea0a0d72891779e9c39d
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2010-11-11 20:03:22 +0700

    add: do not rely on dtype being NULL behavior
    Commit c84de70 (excluded_1(): support exclude files in index -
    2009-08-20) added support for excluded() where dtype can be NULL. It
    was designed specifically for index matching because there was no
    other way to extract dtype information from index. It did not support
    wildcard matching (for example, "a*/" pattern would fail to match).
    
    The code was probably misread when commit 108da0d (git add: Add the
    "--ignore-missing" option for the dry run - 2010-07-10) was made
    because DT_UNKNOWN happens to be zero (NULL) too.
    
    Do not pass DT_UNKNOWN/NULL to excluded(), instead pass a pointer to a
    variable that contains DT_UNKNOWN. The real dtype will be extracted
    from worktree by excluded(), as expected.
    
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 0188f6b3c191eb5277e9ebab3b9079f923dbd283

--- builtin/add.c
+++ builtin/add.c
@@ -2532,6 +2532,11 @@ condition|(
 name|ignore_missing
 condition|)
 block|{
+name|int
+name|dtype
+init|=
+name|DT_UNKNOWN
+decl_stmt|;
 if|if
 condition|(
 name|excluded
@@ -2544,7 +2549,8 @@ index|[
 name|i
 index|]
 argument_list|,
-name|DT_UNKNOWN
+operator|&
+name|dtype
 argument_list|)
 condition|)
 name|dir_add_ignored
