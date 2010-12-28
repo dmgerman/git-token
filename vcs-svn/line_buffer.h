2d88e77e07ce7c10b40bb284ea2a3fb82431c99a
Jonathan Nieder <jrnieder@gmail.com>
2010-12-28 04:26:17 -0600

    vcs-svn: make buffer_copy_bytes return length read
    Currently buffer_copy_bytes does not report to its caller whether
    it encountered an early end of file.
    
    Add a return value representing the number of bytes read (but not
    the number of bytes copied).  This way all three unusual conditions
    can be distinguished: input error with buffer_ferror, output error
    with ferror(outfile), early end of input by checking the return
    value.
    
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>
    Signed-off-by: David Barr <david.barr@cordelta.com>
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>
    
    Former-commit-id: 26557fc1b37480d184a32de025b060aa1aa231db

--- vcs-svn/line_buffer.h
+++ vcs-svn/line_buffer.h
@@ -230,8 +230,12 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_comment
+comment|/* Returns number of bytes read (not necessarily written). */
+end_comment
+
 begin_function_decl
-name|void
+name|off_t
 name|buffer_copy_bytes
 parameter_list|(
 name|struct
