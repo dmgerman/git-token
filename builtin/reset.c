4f97e3f463b12216cd83b1bebf526b01ad6cd9a7
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2011-02-22 23:42:06 +0000

    i18n: git-reset basic messages
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: b50a64e81875944985f84df9324c7624776d6cf8

--- builtin/reset.c
+++ builtin/reset.c
@@ -479,7 +479,10 @@ condition|)
 return|return
 name|error
 argument_list|(
+name|_
+argument_list|(
 literal|"You do not have a valid HEAD."
+argument_list|)
 argument_list|)
 return|;
 if|if
@@ -495,7 +498,10 @@ condition|)
 return|return
 name|error
 argument_list|(
+name|_
+argument_list|(
 literal|"Failed to find tree of HEAD."
+argument_list|)
 argument_list|)
 return|;
 name|nr
@@ -525,7 +531,10 @@ condition|)
 return|return
 name|error
 argument_list|(
+name|_
+argument_list|(
 literal|"Failed to find tree of %s."
+argument_list|)
 argument_list|,
 name|sha1_to_hex
 argument_list|(
@@ -568,7 +577,10 @@ condition|)
 return|return
 name|error
 argument_list|(
+name|_
+argument_list|(
 literal|"Could not write new index file."
+argument_list|)
 argument_list|)
 return|;
 return|return
@@ -612,7 +624,10 @@ argument_list|)
 expr_stmt|;
 name|printf
 argument_list|(
+name|_
+argument_list|(
 literal|"HEAD is now at %s"
+argument_list|)
 argument_list|,
 name|hex
 argument_list|)
@@ -749,7 +764,10 @@ condition|)
 return|return
 name|error
 argument_list|(
+name|_
+argument_list|(
 literal|"Could not read index"
+argument_list|)
 argument_list|)
 return|;
 name|result
@@ -914,7 +932,10 @@ name|ce
 condition|)
 name|die
 argument_list|(
+name|_
+argument_list|(
 literal|"make_cache_entry failed for path '%s'"
+argument_list|)
 argument_list|,
 name|one
 operator|->
@@ -1242,7 +1263,10 @@ name|size
 condition|)
 name|warning
 argument_list|(
+name|_
+argument_list|(
 literal|"Reflog action message too long: %.*s..."
+argument_list|)
 argument_list|,
 literal|50
 argument_list|,
@@ -1643,7 +1667,10 @@ argument_list|)
 condition|)
 name|die
 argument_list|(
+name|_
+argument_list|(
 literal|"Failed to resolve '%s' as a valid ref."
+argument_list|)
 argument_list|,
 name|rev
 argument_list|)
@@ -1662,7 +1689,10 @@ name|commit
 condition|)
 name|die
 argument_list|(
+name|_
+argument_list|(
 literal|"Could not parse object '%s'."
+argument_list|)
 argument_list|,
 name|rev
 argument_list|)
@@ -1691,7 +1721,10 @@ name|NONE
 condition|)
 name|die
 argument_list|(
+name|_
+argument_list|(
 literal|"--patch is incompatible with --{hard,mixed,soft}"
+argument_list|)
 argument_list|)
 expr_stmt|;
 return|return
@@ -1723,7 +1756,10 @@ name|MIXED
 condition|)
 name|warning
 argument_list|(
+name|_
+argument_list|(
 literal|"--mixed with paths is deprecated; use 'git reset --<paths>' instead."
+argument_list|)
 argument_list|)
 expr_stmt|;
 elseif|else
@@ -1869,7 +1905,10 @@ name|err
 condition|)
 name|die
 argument_list|(
+name|_
+argument_list|(
 literal|"Could not reset index file to revision '%s'."
+argument_list|)
 argument_list|,
 name|rev
 argument_list|)
