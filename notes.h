8f3df37d7739a9e78c176e8676ec6f91ddffa801
Jeff King <peff@peff.net>
2011-03-29 16:55:32 -0400

    notes: make expand_notes_ref globally accessible
    This function is useful for other commands besides "git
    notes" which want to let users refer to notes by their
    shorthand name.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 03bb5789cd97de989897e1c9de71a2831ada0544

--- notes.h
+++ notes.h
@@ -670,6 +670,22 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_comment
+comment|/* Expand inplace a note ref like "foo" or "notes/foo" into "refs/notes/foo" */
+end_comment
+
+begin_function_decl
+name|void
+name|expand_notes_ref
+parameter_list|(
+name|struct
+name|strbuf
+modifier|*
+name|sb
+parameter_list|)
+function_decl|;
+end_function_decl
+
 begin_endif
 endif|#
 directive|endif
