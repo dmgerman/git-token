6645147c82467b12c5fc4c5f032bf1ae3a2fbc91
Johan Herland <johan@herland.net>
2011-04-29 11:36:21 +0200

    New --dirstat=lines mode, doing dirstat analysis based on diffstat
    This patch adds an alternative implementation of show_dirstat(), called
    show_dirstat_by_line(), which uses the more expensive diffstat analysis
    (as opposed to show_dirstat()'s own (relatively inexpensive) analysis)
    to derive the numbers from which the --dirstat output is computed.
    
    The alternative implementation is controlled by the new "lines" parameter
    to the --dirstat option (or the diff.dirstat config variable).
    
    For binary files, the diffstat analysis counts bytes instead of lines,
    so to prevent binary files from dominating the dirstat results, the
    byte counts for binary files are divided by 64 before being compared to
    their textual/line-based counterparts. This is a stupid and ugly - but
    very cheap - heuristic.
    
    In linux-2.6.git, running the three different --dirstat modes:
    
      time git diff v2.6.20..v2.6.30 --dirstat=changes > /dev/null
    vs.
      time git diff v2.6.20..v2.6.30 --dirstat=lines > /dev/null
    vs.
      time git diff v2.6.20..v2.6.30 --dirstat=files > /dev/null
    
    yields the following average runtimes on my machine:
    
     - "changes" (default): ~6.0 s
     - "lines":             ~9.6 s
     - "files":             ~0.1 s
    
    So, as expected, there's a considerable performance hit (~60%) by going
    through the full diffstat analysis as compared to the default "changes"
    analysis (obviously, "files" is much faster than both). As such, the
    "lines" mode is probably only useful if you really need the --dirstat
    numbers to be consistent with the numbers returned from the other
    --*stat options.
    
    The patch also includes documentation and tests for the new dirstat mode.
    
    Improved-by: Junio C Hamano <gitster@pobox.com>
    Signed-off-by: Johan Herland <johan@herland.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 1c57a627bf269f3c83c48ad724cd8b14292502ef

--- diff.h
+++ diff.h
@@ -515,6 +515,14 @@ value|(1<< 27)
 end_define
 
 begin_define
+DECL|macro|DIFF_OPT_DIRSTAT_BY_LINE
+define|#
+directive|define
+name|DIFF_OPT_DIRSTAT_BY_LINE
+value|(1<< 28)
+end_define
+
+begin_define
 DECL|macro|DIFF_OPT_TST
 define|#
 directive|define
