2c9876dc46749a47f4e22369f51e65375a9bea90
Jeff King <peff@peff.net>
2011-05-09 09:34:42 -0400

    blame: add --line-porcelain output format
    This is just like --porcelain, except that we always output
    the commit information for each line, not just the first
    time it is referenced. This can make quick and dirty scripts
    much easier to write; see the example added to the blame
    documentation.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: ed747dd5216ba1fe6fa59da9585dcd6cced202bb

--- builtin/blame.c
+++ builtin/blame.c
@@ -8159,6 +8159,14 @@ name|OUTPUT_SHOW_EMAIL
 value|0400
 end_define
 
+begin_define
+DECL|macro|OUTPUT_LINE_PORCELAIN
+define|#
+directive|define
+name|OUTPUT_LINE_PORCELAIN
+value|01000
+end_define
+
 begin_function
 DECL|function|emit_porcelain_details
 specifier|static
@@ -8225,6 +8233,13 @@ name|int
 name|opt
 parameter_list|)
 block|{
+name|int
+name|repeat
+init|=
+name|opt
+operator|&
+name|OUTPUT_LINE_PORCELAIN
+decl_stmt|;
 name|int
 name|cnt
 decl_stmt|;
@@ -8300,7 +8315,7 @@ name|emit_porcelain_details
 argument_list|(
 name|suspect
 argument_list|,
-literal|0
+name|repeat
 argument_list|)
 expr_stmt|;
 name|cp
@@ -8337,6 +8352,7 @@ if|if
 condition|(
 name|cnt
 condition|)
+block|{
 name|printf
 argument_list|(
 literal|"%s %d %d\n"
@@ -8360,6 +8376,18 @@ operator|+
 name|cnt
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
+name|repeat
+condition|)
+name|emit_porcelain_details
+argument_list|(
+name|suspect
+argument_list|,
+literal|1
+argument_list|)
+expr_stmt|;
+block|}
 name|putchar
 argument_list|(
 literal|'\t'
@@ -11971,6 +11999,22 @@ argument_list|,
 literal|"Show in a format designed for machine consumption"
 argument_list|,
 name|OUTPUT_PORCELAIN
+argument_list|)
+block|,
+name|OPT_BIT
+argument_list|(
+literal|0
+argument_list|,
+literal|"line-porcelain"
+argument_list|,
+operator|&
+name|output_option
+argument_list|,
+literal|"Show porcelain format with per-line commit information"
+argument_list|,
+name|OUTPUT_PORCELAIN
+operator||
+name|OUTPUT_LINE_PORCELAIN
 argument_list|)
 block|,
 name|OPT_BIT
