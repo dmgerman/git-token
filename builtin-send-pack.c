c89d60250f8345146a062850660488f232964de6
Junio C Hamano <gitster@pobox.com>
2011-05-16 16:19:49 -0700

    sideband_demux(): fix decl-after-stmt
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 61432146ad707e969664c0151364e2d8da9fb157

--- builtin-send-pack.c
+++ builtin-send-pack.c
@@ -1946,6 +1946,8 @@ modifier|*
 name|fd
 init|=
 name|data
+decl_stmt|,
+name|ret
 decl_stmt|;
 ifndef|#
 directive|ifndef
@@ -1960,9 +1962,8 @@ argument_list|)
 expr_stmt|;
 endif|#
 directive|endif
-name|int
 name|ret
-init|=
+operator|=
 name|recv_sideband
 argument_list|(
 literal|"send-pack"
@@ -1974,7 +1975,7 @@ index|]
 argument_list|,
 name|out
 argument_list|)
-decl_stmt|;
+expr_stmt|;
 name|close
 argument_list|(
 name|out
