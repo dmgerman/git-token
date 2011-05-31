23d9d23e1589277a3ce478f120d9aeb5ec0625ca
Junio C Hamano <gitster@pobox.com>
2011-05-31 09:14:17 -0700

    diff: futureproof "stop feeding the backend early" logic
    Refactor the "do not stop feeding the backend early" logic into a small
    helper function and use it in both run_diff_files() and diff_tree() that
    has the stop-early optimization. We may later add other types of diffcore
    transformation that require to look at the whole result like diff-filter
    does, and having the logic in a single place is essential for longer term
    maintainability.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 28b9264dd6cbadcef8b3e48c24ffcb2893b668b3

--- diff.h
+++ diff.h
@@ -1069,6 +1069,18 @@ end_function_decl
 
 begin_function_decl
 specifier|extern
+name|int
+name|diff_can_quit_early
+parameter_list|(
+name|struct
+name|diff_options
+modifier|*
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
+specifier|extern
 name|void
 name|diff_addremove
 parameter_list|(
