23f3fdc246b9b3d5f749f19578f56c8a3f93807d
René Scharfe <rene.scharfe@lsrfire.ath.cx>
2011-08-01 19:20:53 +0200

    grep: add option to show whole function as context
    Add a new option, -W, to show the whole surrounding function of a match.
    
    It uses the same regular expressions as -p and diff to find the beginning
    of sections.
    
    Currently it will not display comments in front of a function, but those
    that are following one.  Despite this shortcoming it is already useful,
    e.g. to simply see a more complete applicable context or to extract whole
    functions.
    
    Signed-off-by: Rene Scharfe <rene.scharfe@lsrfire.ath.cx>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: ba8ea7496f2b95c49fccb72ed6b332afbd865e7b

--- builtin/grep.c
+++ builtin/grep.c
@@ -4705,6 +4705,20 @@ argument_list|,
 literal|"show a line with the function name before matches"
 argument_list|)
 block|,
+name|OPT_BOOLEAN
+argument_list|(
+literal|'W'
+argument_list|,
+name|NULL
+argument_list|,
+operator|&
+name|opt
+operator|.
+name|funcbody
+argument_list|,
+literal|"show the surrounding function"
+argument_list|)
+block|,
 name|OPT_GROUP
 argument_list|(
 literal|""
@@ -5455,6 +5469,10 @@ operator|||
 name|opt
 operator|.
 name|file_break
+operator|||
+name|opt
+operator|.
+name|funcbody
 condition|)
 name|skip_first_line
 operator|=
