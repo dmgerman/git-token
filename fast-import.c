e21b103c2f5ffca28d6a33dfeb655e4f39457b3b
Dmitry Ivankov <divanorama@gmail.com>
2011-09-23 01:47:05 +0600

    fast-import: don't allow to note on empty branch
    'reset' command makes fast-import start a branch from scratch. It's name
    is kept in lookup table but it's sha1 is null_sha1 (special value).
    'notemodify' command can be used to add a note on branch head given it's
    name. lookup_branch() is used it that case and it doesn't check for
    null_sha1. So fast-import writes a note for null_sha1 object instead of
    giving a error.
    
    Add a check to deny adding a note on empty branch and add a corresponding
    test.
    
    Signed-off-by: Dmitry Ivankov <divanorama@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 0bc69881a611c582f3b89a475882161d83c56494

--- fast-import.c
+++ fast-import.c
@@ -13483,6 +13483,20 @@ condition|(
 name|s
 condition|)
 block|{
+if|if
+condition|(
+name|is_null_sha1
+argument_list|(
+name|s
+operator|->
+name|sha1
+argument_list|)
+condition|)
+name|die
+argument_list|(
+literal|"Can't add a note on empty branch."
+argument_list|)
+expr_stmt|;
 name|hashcpy
 argument_list|(
 name|commit_sha1
