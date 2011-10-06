449f73921bb1aebdb2cdf59dbfdb56528e661460
René Scharfe <rene.scharfe@lsrfire.ath.cx>
2011-10-06 18:23:11 +0200

    pickaxe: plug regex/kws leak
    With -S... --pickaxe-all, free the regex or the kws before returning
    even if we found a match.  Also get rid of the variable has_changes,
    as we can simply break out of the loop.
    
    Signed-off-by: Rene Scharfe <rene.scharfe@lsrfire.ath.cx>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 8e854b00d86070dd463a91bdbc401fb25dc9ddb5

--- diffcore-pickaxe.c
+++ diffcore-pickaxe.c
@@ -1177,13 +1177,8 @@ for|for
 control|(
 name|i
 operator|=
-name|has_changes
-operator|=
 literal|0
 init|;
-operator|!
-name|has_changes
-operator|&&
 name|i
 operator|<
 name|q
@@ -1326,13 +1321,15 @@ condition|)
 name|has_changes
 operator|++
 expr_stmt|;
-block|}
 if|if
 condition|(
 name|has_changes
 condition|)
-return|return;
-comment|/* not munge the queue */
+goto|goto
+name|out
+goto|;
+comment|/* do not munge the queue */
+block|}
 comment|/* otherwise we will clear the whole queue 		 * by copying the empty outq at the end of this 		 * function, but first clear the current entries 		 * in the queue. 		 */
 for|for
 control|(
@@ -1537,6 +1534,20 @@ name|p
 argument_list|)
 expr_stmt|;
 block|}
+name|free
+argument_list|(
+name|q
+operator|->
+name|queue
+argument_list|)
+expr_stmt|;
+operator|*
+name|q
+operator|=
+name|outq
+expr_stmt|;
+name|out
+label|:
 if|if
 condition|(
 name|opts
@@ -1554,18 +1565,6 @@ name|kwsfree
 argument_list|(
 name|kws
 argument_list|)
-expr_stmt|;
-name|free
-argument_list|(
-name|q
-operator|->
-name|queue
-argument_list|)
-expr_stmt|;
-operator|*
-name|q
-operator|=
-name|outq
 expr_stmt|;
 return|return;
 block|}
