aa5a9b12d2bb7036259b3f2557c8b87effac6c99
Thomas Rast <trast@student.ethz.ch>
2011-12-06 18:43:38 +0100

    commit: write cache-tree data when writing index anyway
    In prepare_index(), we refresh the index, and then write it to disk if
    this changed the index data.  After running hooks we re-read the index
    and compute the root tree sha1 with the cache-tree machinery.
    
    This gives us a mostly free opportunity to write up-to-date cache-tree
    data: we can compute it in prepare_index() immediately before writing
    the index to disk.
    
    If we do this, we were going to write the index anyway, and the later
    cache-tree update has no further work to do.  If we don't do it, we
    don't do any extra work, though we still don't have have cache-tree
    data after the commit.
    
    The only case that suffers badly is when the pre-commit hook changes
    many trees in the index.  I'm writing this off as highly unusual.
    
    Signed-off-by: Thomas Rast <trast@student.ethz.ch>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 11c8a74a64a49f236d475514f723123a8928463d

--- builtin/commit.c
+++ builtin/commit.c
@@ -2043,6 +2043,11 @@ argument_list|(
 name|refresh_flags
 argument_list|)
 expr_stmt|;
+name|update_main_cache_tree
+argument_list|(
+literal|1
+argument_list|)
+expr_stmt|;
 if|if
 condition|(
 name|write_cache
@@ -2109,6 +2114,11 @@ condition|(
 name|active_cache_changed
 condition|)
 block|{
+name|update_main_cache_tree
+argument_list|(
+literal|1
+argument_list|)
+expr_stmt|;
 if|if
 condition|(
 name|write_cache
