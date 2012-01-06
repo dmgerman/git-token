6000f685ec9e33b51c1beabeec711d4e45f0e914
René Scharfe <rene.scharfe@lsrfire.ath.cx>
2012-01-06 18:13:00 +0100

    xdiff: print post-image for common records instead of pre-image
    Normally it doesn't matter if we show the pre-image or th post-image
    for the common parts of a diff because they are the same.  If
    white-space changes are ignored they can differ, though.  The
    new text after applying the diff is more interesting in that case,
    so show that instead of the old contents.
    
    Note: GNU diff shows the pre-image.
    
    Suggested-by: Junio C Hamano <gitster@pobox.com>
    Signed-off-by: Rene Scharfe <rene.scharfe@lsrfire.ath.cx>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: baf5aaa33383af656a34b7ba9039e9eb3c9e678c

--- xdiff/xemit.c
+++ xdiff/xemit.c
@@ -409,7 +409,7 @@ init|=
 operator|&
 name|xe
 operator|->
-name|xdf1
+name|xdf2
 decl_stmt|;
 specifier|const
 name|char
@@ -825,13 +825,13 @@ comment|/* 		 * Emit pre-context. 		 */
 for|for
 control|(
 init|;
-name|s1
+name|s2
 operator|<
 name|xch
 operator|->
-name|i1
+name|i2
 condition|;
-name|s1
+name|s2
 operator|++
 control|)
 if|if
@@ -841,9 +841,9 @@ argument_list|(
 operator|&
 name|xe
 operator|->
-name|xdf1
+name|xdf2
 argument_list|,
-name|s1
+name|s2
 argument_list|,
 literal|" "
 argument_list|,
@@ -907,9 +907,9 @@ argument_list|(
 operator|&
 name|xe
 operator|->
-name|xdf1
+name|xdf2
 argument_list|,
-name|s1
+name|s2
 argument_list|,
 literal|" "
 argument_list|,
@@ -1041,21 +1041,21 @@ block|}
 comment|/* 		 * Emit post-context. 		 */
 for|for
 control|(
-name|s1
+name|s2
 operator|=
 name|xche
 operator|->
-name|i1
+name|i2
 operator|+
 name|xche
 operator|->
-name|chg1
+name|chg2
 init|;
-name|s1
+name|s2
 operator|<
-name|e1
+name|e2
 condition|;
-name|s1
+name|s2
 operator|++
 control|)
 if|if
@@ -1065,9 +1065,9 @@ argument_list|(
 operator|&
 name|xe
 operator|->
-name|xdf1
+name|xdf2
 argument_list|,
-name|s1
+name|s2
 argument_list|,
 literal|" "
 argument_list|,
