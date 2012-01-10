634f308e6d3f49aea80ac1a328102a8f143b7994
Jeff King <peff@peff.net>
2012-01-09 23:57:33 -0500

    credential-cache: report more daemon connection errors
    Originally, this code remained relatively silent when we
    failed to connect to the cache. The idea was that it was
    simply a cache, and we didn't want to bother the user with
    temporary failures (the worst case is that we would simply
    ask their password again).
    
    However, if you have a configuration failure or other
    problem, it is helpful for the daemon to report those
    problems. Git will happily ignore the failed error code, but
    the extra information to stderr can help the user diagnose
    the problem.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 8ec6c8d79567a71ca3c6f1ec73eb453d371b1ade

--- credential-cache.c
+++ credential-cache.c
@@ -433,12 +433,24 @@ name|buf
 argument_list|)
 operator|<
 literal|0
-operator|&&
-operator|(
+condition|)
+block|{
+if|if
+condition|(
+name|errno
+operator|!=
+name|ENOENT
+condition|)
+name|die_errno
+argument_list|(
+literal|"unable to connect to cache daemon"
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
 name|flags
 operator|&
 name|FLAG_SPAWN
-operator|)
 condition|)
 block|{
 name|spawn_daemon
@@ -463,6 +475,7 @@ argument_list|(
 literal|"unable to connect to cache daemon"
 argument_list|)
 expr_stmt|;
+block|}
 block|}
 name|strbuf_release
 argument_list|(
