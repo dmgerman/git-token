dmgerman/git-token

commit 93057a1a8b1c75fbd77de71ec2b4a05841a69da4

A loose object is not corrupt if it cannot be read due to EMFILE
"git fsck" bails out with a claim that a loose object that cannot be
read but exists on the filesystem to be corrupt, which is wrong when
read_object() failed due to e.g. EMFILE.

Signed-off-by: Junio C Hamano <gitster@pobox.com>

Former-commit-id: 3ba7a065527a27f45659398e882feaa1165cbb4c

sha1_file.c
@@ -10919,15 +10919,6 @@ decl_stmt|;
 name|void
 modifier|*
 name|data
-init|=
-name|read_object
-argument_list|(
-name|repl
-argument_list|,
-name|type
-argument_list|,
-name|size
-argument_list|)
 decl_stmt|;
 name|char
 modifier|*
@@ -10939,6 +10930,21 @@ name|packed_git
 modifier|*
 name|p
 decl_stmt|;
+name|errno
+operator|=
+literal|0
+expr_stmt|;
+name|data
+operator|=
+name|read_object
+argument_list|(
+name|repl
+argument_list|,
+name|type
+argument_list|,
+name|size
+argument_list|)
+expr_stmt|;
 if|if
 condition|(
 name|data
@@ -10957,6 +10963,22 @@ return|return
 name|data
 return|;
 block|}
+if|if
+condition|(
+name|errno
+operator|!=
+name|ENOENT
+condition|)
+name|die_errno
+argument_list|(
+literal|"failed to read object %s"
+argument_list|,
+name|sha1_to_hex
+argument_list|(
+name|sha1
+argument_list|)
+argument_list|)
+expr_stmt|;
 comment|/* die if we replaced an object with one that does not exist */
 if|if
 condition|(

commit 9c63a2cb981a4ec669af19d70e37132ed830eca5

fsck: give accurate error message on empty loose object files
Since 93057a1a8b1c [formerly 3ba7a065527a] (A loose object is not corrupt if it
cannot be read due to EMFILE), "git fsck" on a repository with an empty
loose object file complains with the error message

  fatal: failed to read object <sha1>: Invalid argument

This comes from a failure of mmap on this empty file, which sets errno to
EINVAL. Instead of calling xmmap on empty file, we display a clean error
message ourselves, and return a NULL pointer. The new message is

  error: object file .git/objects/09/<rest-of-sha1> is empty
  fatal: loose object <sha1> (stored in .git/objects/09/<rest-of-sha1>) is corrupt

The second line was already there before the regression in 93057a1a8b1c [formerly 3ba7a065527a],
and the first is an additional message, that should help diagnosing the
problem for the user.

Signed-off-by: Matthieu Moy <Matthieu.Moy@imag.fr>
Signed-off-by: Junio C Hamano <gitster@pobox.com>

Former-commit-id: 33e42de0d21a8365496e904235bd32fd4659300d

sha1_file.c
@@ -5614,6 +5614,28 @@ operator|.
 name|st_size
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
+operator|!
+operator|*
+name|size
+condition|)
+block|{
+comment|/* mmap() is forbidden on empty files */
+name|error
+argument_list|(
+literal|"object file %s is empty"
+argument_list|,
+name|sha1_file_name
+argument_list|(
+name|sha1
+argument_list|)
+argument_list|)
+expr_stmt|;
+return|return
+name|NULL
+return|;
+block|}
 name|map
 operator|=
 name|xmmap