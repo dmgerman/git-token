ee07b608bf80d77ba46ca6ce6a78a32381109987
Junio C Hamano <gitster@pobox.com>
2012-02-19 15:36:55 -0800

    xdiff: PATIENCE/HISTOGRAM are not independent option bits
    Because the default Myers, patience and histogram algorithms cannot be in
    effect at the same time, XDL_PATIENCE_DIFF and XDL_HISTOGRAM_DIFF are not
    independent bits.  Instead of wasting one bit per algorithm, define a few
    macros to access the few bits they occupy and update the code that access
    them.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 307ab20b333d9b4c818b1ff912e86944d1a3fdc1

--- diff.c
+++ diff.c
@@ -20071,7 +20071,11 @@ argument_list|,
 literal|"--patience"
 argument_list|)
 condition|)
-name|DIFF_XDL_SET
+name|options
+operator|->
+name|xdl_opts
+operator|=
+name|DIFF_WITH_ALG
 argument_list|(
 name|options
 argument_list|,
@@ -20089,7 +20093,11 @@ argument_list|,
 literal|"--histogram"
 argument_list|)
 condition|)
-name|DIFF_XDL_SET
+name|options
+operator|->
+name|xdl_opts
+operator|=
+name|DIFF_WITH_ALG
 argument_list|(
 name|options
 argument_list|,
