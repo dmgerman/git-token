96bacb110b9a1aa91c742851dcc0030a509b0e4f
Junio C Hamano <gitster@pobox.com>
2012-02-28 14:55:39 -0800

    fsck: --no-dangling omits "dangling object" information
    The default output from "fsck" is often overwhelmed by informational
    message on dangling objects, especially if you do not repack often, and a
    real error can easily be buried.
    
    Add "--no-dangling" option to omit them, and update the user manual to
    demonstrate its use.
    
    Based on a patch by Clemens Buchacher, but reverted the part to change
    the default to --no-dangling, which is unsuitable for the first patch.
    The usual three-step procedure to break the backward compatibility over
    time needs to happen on top of this, if we were to go in that direction.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: c6a13b2c86b71cb25011094ff2dee3d7769991a2

--- builtin/fsck.c
+++ builtin/fsck.c
@@ -216,6 +216,16 @@ literal|1
 decl_stmt|;
 end_decl_stmt
 
+begin_decl_stmt
+DECL|variable|show_dangling
+specifier|static
+name|int
+name|show_dangling
+init|=
+literal|1
+decl_stmt|;
+end_decl_stmt
+
 begin_define
 DECL|macro|ERROR_OBJECT
 define|#
@@ -1116,6 +1126,10 @@ operator|->
 name|used
 condition|)
 block|{
+if|if
+condition|(
+name|show_dangling
+condition|)
 name|printf
 argument_list|(
 literal|"dangling %s %s\n"
@@ -3141,6 +3155,18 @@ operator|&
 name|show_unreachable
 argument_list|,
 literal|"show unreachable objects"
+argument_list|)
+block|,
+name|OPT_BOOL
+argument_list|(
+literal|0
+argument_list|,
+literal|"dangling"
+argument_list|,
+operator|&
+name|show_dangling
+argument_list|,
+literal|"show dangling objects"
 argument_list|)
 block|,
 name|OPT_BOOLEAN
