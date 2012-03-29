c3620c6c671832794c993f1ae50325e69f0f44a5
Heiko Voigt <hvoigt@hvoigt.net>
2012-03-29 09:21:24 +0200

    push: teach --recurse-submodules the on-demand option
    When using this option git will search for all submodules that
    have changed in the revisions to be send. It will then try to
    push the currently checked out branch of each submodule.
    
    This helps when a user has finished working on a change which
    involves submodules and just wants to push everything in one go.
    
    Signed-off-by: Fredrik Gustafsson <iveqy@iveqy.com>
    Mentored-by: Jens Lehmann <Jens.Lehmann@web.de>
    Mentored-by: Heiko Voigt <hvoigt@hvoigt.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: eb21c732d6b642a8f33abd69071a95de01d5061b

--- transport.h
+++ transport.h
@@ -345,6 +345,14 @@ value|64
 end_define
 
 begin_define
+DECL|macro|TRANSPORT_RECURSE_SUBMODULES_ON_DEMAND
+define|#
+directive|define
+name|TRANSPORT_RECURSE_SUBMODULES_ON_DEMAND
+value|256
+end_define
+
+begin_define
 DECL|macro|TRANSPORT_SUMMARY_WIDTH
 define|#
 directive|define
