59b0fa60b66c9a3184b44849f00968108e9f7e9d
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2012-04-14 09:54:32 +0200

    Provide branch name in error message when using @{u}
    When using @{u} or @{upstream} it is common to omit the branch name,
    implying current branch. If the upstream is not configured, the error
    message was "No upstream branch found for ''".
    
    When resolving '@{u}', branch_get() is called, which almost always
    returns a description of a branch. This allows us to use a branch name
    in the error message, even if the user said something like '@{u}'.
    
    The only case when branch_get() returns NULL is when HEAD points to so
    something which is not a branch. Of course this also means that no
    upstream is configured, but it is better to directly say that HEAD
    does not point to a branch.
    
    Signed-off-by: Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 9884e67f9d9b511592b981572cfbb30cc50aec61

--- sha1_name.c
+++ sha1_name.c
@@ -4677,11 +4677,20 @@ else|:
 name|NULL
 argument_list|)
 expr_stmt|;
+comment|/* 	 * Upstream can be NULL only if cp refers to HEAD and HEAD 	 * points to something different than a branch. 	 */
 if|if
 condition|(
 operator|!
 name|upstream
-operator|||
+condition|)
+return|return
+name|error
+argument_list|(
+literal|"HEAD does not point to a branch"
+argument_list|)
+return|;
+if|if
+condition|(
 operator|!
 name|upstream
 operator|->
@@ -4702,7 +4711,9 @@ name|error
 argument_list|(
 literal|"No upstream branch found for '%s'"
 argument_list|,
-name|cp
+name|upstream
+operator|->
+name|name
 argument_list|)
 return|;
 name|free
