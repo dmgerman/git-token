a55c0b14febf9b6220381d0ee20ac1b6e47271c7
Neil Horman <nhorman@tuxdriver.com>
2012-04-20 10:36:15 -0400

    git-cherry-pick: Add keep-redundant-commits option
    The git-cherry-pick --allow-empty command by default only preserves empty
    commits that were originally empty, i.e only those commits for which
    <commit>^{tree} and <commit>^^{tree} are equal.  By default commits which are
    non-empty, but were made empty by the inclusion of a prior commit on the current
    history are filtered out.  This option allows us to override that behavior and
    include redundant commits as empty commits in the change history.
    
    Note that this patch changes the default behavior of git cherry-pick slightly.
    Prior to this patch all commits in a cherry-pick sequence were applied and git
    commit was run.  The implication here was that, if a commit was redundant, and
    the commit did not trigger the fast forward logic, the git commit operation, and
    therefore the git cherry-pick operation would fail, displaying the cherry pick
    advice (i.e. run git commit --allow-empty).  With this patch however, such
    redundant commits are automatically skipped without stopping, unless
    --keep-redundant-commits is specified, in which case, they are automatically
    applied as empty commits.
    
    Signed-off-by: Neil Horman <nhorman@tuxdriver.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: b27cfb0d8d4cbb6d079c70ffeadac9c0dcfff250

--- builtin/revert.c
+++ builtin/revert.c
@@ -652,6 +652,9 @@ argument_list|()
 block|,
 name|OPT_END
 argument_list|()
+block|,
+name|OPT_END
+argument_list|()
 block|, 	}
 decl_stmt|;
 if|if
@@ -708,7 +711,21 @@ name|opts
 operator|->
 name|allow_empty
 argument_list|,
-literal|"preserve empty commits"
+literal|"preserve initially empty commits"
+argument_list|)
+block|,
+name|OPT_BOOLEAN
+argument_list|(
+literal|0
+argument_list|,
+literal|"keep-redundant-commits"
+argument_list|,
+operator|&
+name|opts
+operator|->
+name|keep_redundant_commits
+argument_list|,
+literal|"keep redundant, empty commits"
 argument_list|)
 block|,
 name|OPT_END
@@ -776,6 +793,19 @@ name|rollback
 argument_list|,
 name|NULL
 argument_list|)
+expr_stmt|;
+comment|/* implies allow_empty */
+if|if
+condition|(
+name|opts
+operator|->
+name|keep_redundant_commits
+condition|)
+name|opts
+operator|->
+name|allow_empty
+operator|=
+literal|1
 expr_stmt|;
 comment|/* Set the subcommand */
 if|if
