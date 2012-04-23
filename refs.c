e82ffa856e9192210926371828e6066720c8416e
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2012-04-23 19:30:25 +0700

    i18n: make warn_dangling_symref() automatically append \n
    This helps remove \n from translatable strings
    
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 18986d5377f19e069afffa6689d507b22f1d6c71

--- refs.c
+++ refs.c
@@ -2040,6 +2040,15 @@ argument_list|,
 name|refname
 argument_list|)
 expr_stmt|;
+name|fputc
+argument_list|(
+literal|'\n'
+argument_list|,
+name|d
+operator|->
+name|fp
+argument_list|)
+expr_stmt|;
 return|return
 literal|0
 return|;
