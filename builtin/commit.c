0c925357ba636314eacdab348c6fbf288fea73b9
Chris Webb <chris@arachsys.com>
2012-07-09 19:53:26 +0100

    Allow edit of empty message with commit --amend
    "git commit --amend" used on a commit with an empty message fails
    unless -m is given, whether or not --allow-empty-message is
    specified.
    
    Allow it to proceed to the editor with an empty commit message.
    Unless --allow-empty-message is in force, it will still abort later
    if an empty message is saved from the editor (this check was
    already necessary to prevent a non-empty commit message being edited
    to an empty one).
    
    Add a test for --amend --edit of an empty commit message which fails
    without this fix, as it's a rare case that won't get frequently
    tested otherwise.
    
    Signed-off-by: Chris Webb <chris@arachsys.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: d9a93575722d9ea918f090892b06618ddf0d1d65

--- builtin/commit.c
+++ builtin/commit.c
@@ -3635,6 +3635,10 @@ expr_stmt|;
 if|if
 condition|(
 operator|!
+name|use_editor
+operator|&&
+operator|(
+operator|!
 name|buffer
 operator|||
 name|buffer
@@ -3643,6 +3647,7 @@ literal|2
 index|]
 operator|==
 literal|'\0'
+operator|)
 condition|)
 name|die
 argument_list|(
