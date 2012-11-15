b5231e0d8b4e75d30eb88552dd17a54adc0d5ea2
Carlos Martín Nieto <cmn@elego.de>
2012-11-15 10:10:01 -0800

    config: don't segfault when given --path with a missing value
    When given a variable without a value, such as '[section] var' and
    asking git-config to treat it as a path, git_config_pathname returns
    an error and doesn't modify its output parameter. show_config assumes
    that the call is always successful and sets a variable to indicate
    that vptr should be freed. In case of an error however, trying to do
    this will cause the program to be killed, as it's pointing to memory
    in the stack.
    
    Detect the error and return immediately to avoid freeing or accessing
    the uninitialed memory in the stack.
    
    Signed-off-by: Carlos Martín Nieto <cmn@elego.de>
    Acked-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 962c38eedd931a608b5e675f12a21569d9f4d39f

--- builtin/config.c
+++ builtin/config.c
@@ -1047,6 +1047,8 @@ operator|==
 name|TYPE_PATH
 condition|)
 block|{
+if|if
+condition|(
 name|git_config_pathname
 argument_list|(
 operator|&
@@ -1056,7 +1058,13 @@ name|key_
 argument_list|,
 name|value_
 argument_list|)
-expr_stmt|;
+operator|<
+literal|0
+condition|)
+return|return
+operator|-
+literal|1
+return|;
 name|must_free_vptr
 operator|=
 literal|1
