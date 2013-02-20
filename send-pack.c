47329f46c175e7b759d057420bd49a5ebe23cfb8
Jeff King <peff@peff.net>
2013-02-20 15:00:43 -0500

    send-pack: prefer prefixcmp over memcmp in receive_status
    This code predates prefixcmp, so it used memcmp along with
    static sizes. Replacing these memcmps with prefixcmp makes
    the code much more readable, and the lack of static sizes
    will make refactoring it in future patches simpler.
    
    Note that we used to be unnecessarily liberal in parsing the
    "unpack" status line, and would accept "unpack ok\njunk". No
    version of git has ever produced that, and it violates the
    BNF in Documentation/technical/pack-protocol.txt. Let's take
    this opportunity to tighten the check by converting the
    prefix comparison into a strcmp.
    
    While we're in the area, let's also fix a vague error
    message that does not follow our usual conventions (it
    writes directly to stderr and does not use the "error:"
    prefix).
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 8f9e3e498c113e987c2b8662485db5461cad1c36

--- send-pack.c
+++ send-pack.c
@@ -588,17 +588,11 @@ argument_list|)
 decl_stmt|;
 if|if
 condition|(
-name|len
-operator|<
-literal|10
-operator|||
-name|memcmp
+name|prefixcmp
 argument_list|(
 name|line
 argument_list|,
 literal|"unpack "
-argument_list|,
-literal|7
 argument_list|)
 condition|)
 return|return
@@ -609,13 +603,11 @@ argument_list|)
 return|;
 if|if
 condition|(
-name|memcmp
+name|strcmp
 argument_list|(
 name|line
 argument_list|,
 literal|"unpack ok\n"
-argument_list|,
-literal|10
 argument_list|)
 condition|)
 block|{
@@ -698,36 +690,24 @@ condition|)
 break|break;
 if|if
 condition|(
-name|len
-operator|<
-literal|3
-operator|||
-operator|(
-name|memcmp
+name|prefixcmp
 argument_list|(
 name|line
 argument_list|,
 literal|"ok "
-argument_list|,
-literal|3
 argument_list|)
 operator|&&
-name|memcmp
+name|prefixcmp
 argument_list|(
 name|line
 argument_list|,
 literal|"ng "
-argument_list|,
-literal|3
 argument_list|)
-operator|)
 condition|)
 block|{
-name|fprintf
+name|error
 argument_list|(
-name|stderr
-argument_list|,
-literal|"protocol error: %s\n"
+literal|"invalid ref status from remote: %s"
 argument_list|,
 name|line
 argument_list|)
