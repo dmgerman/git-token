94054b8e14277241849076891d9f81da32f7dc7f
Jeff King <peff@peff.net>
2013-03-25 16:23:59 -0400

    clone: die on errors from unpack_trees
    When clone is populating the working tree, it ignores the
    return status from unpack_trees; this means we may report a
    successful clone, even when the checkout fails.
    
    When checkout fails, we may want to leave the $GIT_DIR in
    place, as it might be possible to recover the data through
    further use of "git checkout" (e.g., if the checkout failed
    due to a transient error, disk full, etc). However, we
    already die on a number of other checkout-related errors, so
    this patch follows that pattern.
    
    In addition to marking a now-passing test, we need to adjust
    t5710, which blindly assumed it could make bogus clones of
    very deep alternates hierarchies. By using "--bare", we can
    avoid it actually touching any objects.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 0aac7bb287645dd72ad8ad6b805128b8ff7f111f

--- builtin/clone.c
+++ builtin/clone.c
@@ -3374,6 +3374,8 @@ operator|->
 name|size
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
 name|unpack_trees
 argument_list|(
 literal|1
@@ -3383,6 +3385,16 @@ name|t
 argument_list|,
 operator|&
 name|opts
+argument_list|)
+operator|<
+literal|0
+condition|)
+name|die
+argument_list|(
+name|_
+argument_list|(
+literal|"unable to checkout working tree"
+argument_list|)
 argument_list|)
 expr_stmt|;
 if|if
