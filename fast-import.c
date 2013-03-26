4d599ef845766314585e200feda044c3a6c62f8d
Ramsay Jones <ramsay@ramsay1.demon.co.uk>
2013-03-26 19:09:44 +0000

    fast-import: Fix an gcc -Wuninitialized warning
    Commit cbfd5e1c ("drop some obsolete "x = x" compiler warning hacks",
    21-03-2013) removed a gcc hack that suppressed an "might be used
    uninitialized" warning issued by older versions of gcc.
    
    However, commit 3aa99df8 ('fast-import: clarify "inline" logic in
    file_change_m', 21-03-2013) addresses an (almost) identical issue
    (with very similar code), but includes additional code in it's
    resolution. The solution used by this commit, unlike that used by
    commit cbfd5e1c, also suppresses the -Wuninitialized warning on
    older versions of gcc.
    
    In order to suppress the warning (against the 'oe' symbol) in the
    note_change_n() function, we adopt the same solution used by commit
    3aa99df8.
    
    Signed-off-by: Ramsay Jones <ramsay@ramsay1.demon.co.uk>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 0a34594c8340f5cf246592ec08d330901ea8951e

--- fast-import.c
+++ fast-import.c
@@ -13601,6 +13601,11 @@ name|inline_data
 operator|=
 literal|1
 expr_stmt|;
+name|oe
+operator|=
+name|NULL
+expr_stmt|;
+comment|/* not used with inline_data, but makes gcc happy */
 name|p
 operator|+=
 name|strlen
