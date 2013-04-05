af096f25af5dba26b0230560946cdf3a01081c3f
John Keeping <john@keeping.me.uk>
2013-04-05 13:19:14 +0100

    diff: allow unstuck arguments with --diff-algorithm
    The argument to --diff-algorithm is mandatory, so there is no reason to
    require the argument to be stuck to the option with '='.  Change this
    for consistency with other Git commands.
    
    Note that this does not change the handling of diff-algorithm in
    merge-recursive.c since the primary interface to that is via the -X
    option to 'git merge' where the unstuck form does not make sense.
    
    Signed-off-by: John Keeping <john@keeping.me.uk>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 0895c6d4c09045a3faeb7b652a844fc81c774631

--- diff.c
+++ diff.c
@@ -21105,13 +21105,19 @@ expr_stmt|;
 elseif|else
 if|if
 condition|(
-operator|!
-name|prefixcmp
+operator|(
+name|argcount
+operator|=
+name|parse_long_opt
 argument_list|(
-name|arg
+literal|"diff-algorithm"
 argument_list|,
-literal|"--diff-algorithm="
+name|av
+argument_list|,
+operator|&
+name|optarg
 argument_list|)
+operator|)
 condition|)
 block|{
 name|long
@@ -21119,9 +21125,7 @@ name|value
 init|=
 name|parse_algorithm_value
 argument_list|(
-name|arg
-operator|+
-literal|17
+name|optarg
 argument_list|)
 decl_stmt|;
 if|if
@@ -21158,6 +21162,9 @@ name|xdl_opts
 operator||=
 name|value
 expr_stmt|;
+return|return
+name|argcount
+return|;
 block|}
 comment|/* flags options */
 elseif|else
