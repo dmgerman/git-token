0f1a46277aa4528f213dee525a96dad1ce31582d
Junio C Hamano <gitster@pobox.com>
2013-05-10 17:10:11 +0200

    diff_opt: track whether flags have been set explicitly
    The diff_opt infrastructure sets flags based on defaults and command
    line options.  It is impossible to tell whether a flag has been set
    as a default or on explicit request.  Update the structure so that
    this detection is possible:
    
     * Add an extra "opt->touched_flags" that keeps track of all the
       fields that have been touched by DIFF_OPT_SET and DIFF_OPT_CLR.
    
     * You may continue setting the default values to the flags, like
       commands in the "log" family do in cmd_log_init_defaults(), but
       after you finished setting the defaults, you clear the
       touched_flags field;
    
     * And then you let the usual callchain call diff_opt_parse(),
       allowing the opt->flags be set or unset, while keeping track of
       which bits the user touched;
    
     * There is an optional callback "opt->set_default" that is called
       at the very beginning to let you inspect touched_flags and update
       opt->flags appropriately, before the remainder of the diffcore
       machinery is set up, taking the opt->flags value into account.
    
    Signed-off-by: Michael J Gruber <git@drmicha.warpmail.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 6c374008b1a4e3b66469168aace47003e9771e2d

--- diff.h
+++ diff.h
@@ -573,6 +573,19 @@ value|((opts)->flags& DIFF_OPT_##flag)
 end_define
 
 begin_define
+DECL|macro|DIFF_OPT_TOUCHED
+define|#
+directive|define
+name|DIFF_OPT_TOUCHED
+parameter_list|(
+name|opts
+parameter_list|,
+name|flag
+parameter_list|)
+value|((opts)->touched_flags& DIFF_OPT_##flag)
+end_define
+
+begin_define
 DECL|macro|DIFF_OPT_SET
 define|#
 directive|define
@@ -582,7 +595,7 @@ name|opts
 parameter_list|,
 name|flag
 parameter_list|)
-value|((opts)->flags |= DIFF_OPT_##flag)
+value|(((opts)->flags |= DIFF_OPT_##flag),((opts)->touched_flags |= DIFF_OPT_##flag))
 end_define
 
 begin_define
@@ -595,7 +608,7 @@ name|opts
 parameter_list|,
 name|flag
 parameter_list|)
-value|((opts)->flags&= ~DIFF_OPT_##flag)
+value|(((opts)->flags&= ~DIFF_OPT_##flag),((opts)->touched_flags |= DIFF_OPT_##flag))
 end_define
 
 begin_define
@@ -714,6 +727,10 @@ decl_stmt|;
 DECL|member|flags
 name|unsigned
 name|flags
+decl_stmt|;
+DECL|member|touched_flags
+name|unsigned
+name|touched_flags
 decl_stmt|;
 DECL|member|use_color
 name|int
@@ -844,6 +861,18 @@ DECL|member|found_follow
 name|int
 name|found_follow
 decl_stmt|;
+DECL|member|set_default
+name|void
+function_decl|(
+modifier|*
+name|set_default
+function_decl|)
+parameter_list|(
+name|struct
+name|diff_options
+modifier|*
+parameter_list|)
+function_decl|;
 DECL|member|file
 name|FILE
 modifier|*
