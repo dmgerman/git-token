26e53afc2d4288d780adb66bda47fb516cf03789
Michael Haggerty <mhagger@alum.mit.edu>
2013-05-25 11:08:15 +0200

    do_fetch(): reduce scope of peer_item
    Signed-off-by: Michael Haggerty <mhagger@alum.mit.edu>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 6f64a16faff0189d54cf30854483493574476c6e

--- builtin/fetch.c
+++ builtin/fetch.c
@@ -3957,13 +3957,6 @@ init|=
 name|STRING_LIST_INIT_DUP
 decl_stmt|;
 name|struct
-name|string_list_item
-modifier|*
-name|peer_item
-init|=
-name|NULL
-decl_stmt|;
-name|struct
 name|ref
 modifier|*
 name|ref_map
@@ -4127,8 +4120,11 @@ operator|->
 name|peer_ref
 condition|)
 block|{
+name|struct
+name|string_list_item
+modifier|*
 name|peer_item
-operator|=
+init|=
 name|string_list_lookup
 argument_list|(
 operator|&
@@ -4140,7 +4136,7 @@ name|peer_ref
 operator|->
 name|name
 argument_list|)
-expr_stmt|;
+decl_stmt|;
 if|if
 condition|(
 name|peer_item
