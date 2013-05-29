7e368283c4010bc9970eef0fb8511527b03b6422
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2013-05-29 19:12:42 +0700

    get_sha1: warn about full or short object names that look like refs
    When we get 40 hex digits, we immediately assume it's an SHA-1. This
    is the right thing to do because we have no way else to specify an
    object. If there is a ref with the same object name, it will be
    ignored. Warn the user about this case because the ref with full
    object name is likely a mistake, for example
    
        git checkout -b $empty_var $(git rev-parse something)
    
    advice.object_name_warning is not documented because frankly people
    should not be aware about it until they encounter this situation.
    
    While at there, warn about ambiguation with abbreviated SHA-1 too.
    
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 798c35fcd8a71a094ca68ac05d81e08c5ac8166d

--- advice.c
+++ advice.c
@@ -113,6 +113,15 @@ literal|1
 decl_stmt|;
 end_decl_stmt
 
+begin_decl_stmt
+DECL|variable|advice_object_name_warning
+name|int
+name|advice_object_name_warning
+init|=
+literal|1
+decl_stmt|;
+end_decl_stmt
+
 begin_struct
 specifier|static
 struct|struct
@@ -216,6 +225,13 @@ literal|"detachedhead"
 block|,
 operator|&
 name|advice_detached_head
+block|}
+block|,
+block|{
+literal|"object_name_warning"
+block|,
+operator|&
+name|advice_object_name_warning
 block|}
 block|,
 comment|/* make this an alias for backward compatibility */
