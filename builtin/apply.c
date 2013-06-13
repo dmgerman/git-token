5e7ae9de3239fdca3fb51e22b42880345210ce55
René Scharfe <rene.scharfe@lsrfire.ath.cx>
2013-06-13 20:19:44 +0200

    use logical OR (||) instead of binary OR (|) in logical context
    The compiler can short-circuit the evaluation of conditions strung
    together with logical OR operators instead of computing the resulting
    bitmask with binary ORs.  More importantly, this patch makes the
    intent of the changed code clearer, because the logical context (as
    opposed to binary context) becomes immediately obvious.
    
    While we're at it, simplify the check for patch->is_rename in
    builtin/apply.c a bit; it can only be 0 or 1, so we don't need a
    comparison operator.
    
    Signed-off-by: René Scharfe <rene.scharfe@lsrfire.ath.cx>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 0b437a18bdd188794cec8855e2a413921d08b6e8

--- builtin/apply.c
+++ builtin/apply.c
@@ -17054,15 +17054,11 @@ name|patch
 operator|->
 name|is_new
 operator|)
-operator||
-operator|(
-literal|0
-operator|<
+operator|||
 name|patch
 operator|->
 name|is_rename
-operator|)
-operator||
+operator|||
 name|patch
 operator|->
 name|is_copy
