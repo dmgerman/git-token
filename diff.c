81f284f974e4d506b91190e3d8a58a5968db7614
Junio C Hamano <gitster@pobox.com>
2013-06-24 14:35:04 -0700

    diff: demote core.safecrlf=true to core.safecrlf=warn
    Otherwise the user will not be able to start to guess where in the
    contents in the working tree the offending unsafe CR lies.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 5430bb283b478991a979437a79e10dcbb6f20e28

--- diff.c
+++ diff.c
@@ -15303,6 +15303,21 @@ name|err
 init|=
 literal|0
 decl_stmt|;
+comment|/* 	 * demote FAIL to WARN to allow inspecting the situation 	 * instead of refusing. 	 */
+name|enum
+name|safe_crlf
+name|crlf_warn
+init|=
+operator|(
+name|safe_crlf
+operator|==
+name|SAFE_CRLF_FAIL
+condition|?
+name|SAFE_CRLF_WARN
+else|:
+name|safe_crlf
+operator|)
+decl_stmt|;
 if|if
 condition|(
 operator|!
@@ -15619,7 +15634,7 @@ argument_list|,
 operator|&
 name|buf
 argument_list|,
-name|safe_crlf
+name|crlf_warn
 argument_list|)
 condition|)
 block|{
