d23a564f63807858be32aaa5f8fb34cd8409d4d5
Kevin Bracey <kevin@bracey.fi>
2013-06-26 23:06:41 +0300

    am: replace uses of --resolved with --continue
    git am was previously modified to provide --continue for consistency
    with rebase, merge etc, and the documentation changed to showing
    --continue as the primary form.
    
    Complete the work by replacing remaining uses of --resolved by
    --continue, most notably in suggested command reminders.
    
    Signed-off-by: Kevin Bracey <kevin@bracey.fi>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 8ceb6fbd63e2b3b82bbca0139a1696955aa753c6

--- wt-status.c
+++ wt-status.c
@@ -4943,7 +4943,7 @@ name|color
 argument_list|,
 name|_
 argument_list|(
-literal|"  (fix conflicts and then run \"git am --resolved\")"
+literal|"  (fix conflicts and then run \"git am --continue\")"
 argument_list|)
 argument_list|)
 expr_stmt|;
