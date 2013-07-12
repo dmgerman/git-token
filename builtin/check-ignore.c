8602e8f51963c35de97d76ee622f57e2d397b6a8
Junio C Hamano <gitster@pobox.com>
2013-07-11 23:05:48 -0700

    check-ignore: the name of the character is NUL, not NULL
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 800531a8668710b5fa02b81360116f508888b58c

--- builtin/check-ignore.c
+++ builtin/check-ignore.c
@@ -70,10 +70,10 @@ decl_stmt|;
 end_decl_stmt
 
 begin_decl_stmt
-DECL|variable|null_term_line
+DECL|variable|nul_term_line
 specifier|static
 name|int
-name|null_term_line
+name|nul_term_line
 decl_stmt|;
 end_decl_stmt
 
@@ -136,11 +136,11 @@ argument_list|,
 name|NULL
 argument_list|,
 operator|&
-name|null_term_line
+name|nul_term_line
 argument_list|,
 name|N_
 argument_list|(
-literal|"input paths are terminated by a null character"
+literal|"input paths are terminated by a NUL character"
 argument_list|)
 argument_list|)
 block|,
@@ -198,7 +198,7 @@ decl_stmt|;
 if|if
 condition|(
 operator|!
-name|null_term_line
+name|nul_term_line
 condition|)
 block|{
 if|if
@@ -629,7 +629,7 @@ decl_stmt|;
 name|int
 name|line_termination
 init|=
-name|null_term_line
+name|nul_term_line
 condition|?
 literal|0
 else|:
@@ -902,7 +902,7 @@ else|else
 block|{
 if|if
 condition|(
-name|null_term_line
+name|nul_term_line
 condition|)
 name|die
 argument_list|(
