0896c7880401a9e76de1cd5ca98fb295f240f72a
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2013-07-14 15:36:05 +0700

    kill limit_pathspec_to_literal() as it's only used by parse_pathspec()
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 341003e715c9a7c0332d3f8f08c2f3696a057565

--- pathspec.c
+++ pathspec.c
@@ -250,6 +250,13 @@ modifier|*
 name|elt
 parameter_list|)
 block|{
+specifier|static
+name|int
+name|literal_global
+init|=
+operator|-
+literal|1
+decl_stmt|;
 name|unsigned
 name|magic
 init|=
@@ -283,6 +290,21 @@ init|=
 operator|-
 literal|1
 decl_stmt|;
+if|if
+condition|(
+name|literal_global
+operator|<
+literal|0
+condition|)
+name|literal_global
+operator|=
+name|git_env_bool
+argument_list|(
+name|GIT_LITERAL_PATHSPECS_ENVIRONMENT
+argument_list|,
+literal|0
+argument_list|)
+expr_stmt|;
 if|if
 condition|(
 name|elt
@@ -783,8 +805,7 @@ condition|(
 name|prefixlen
 operator|&&
 operator|!
-name|limit_pathspec_to_literal
-argument_list|()
+name|literal_global
 condition|)
 block|{
 comment|/* Preserve the actual prefix length of each pattern */
@@ -1098,8 +1119,7 @@ expr_stmt|;
 block|}
 if|if
 condition|(
-name|limit_pathspec_to_literal
-argument_list|()
+name|literal_global
 condition|)
 name|item
 operator|->
