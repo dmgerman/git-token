89b850beea3d50fc586385a653afc418d97888f5
Thomas Rast <trast@inf.ethz.ch>
2013-07-16 11:27:36 +0200

    daemon/shell: refactor redirection of 0/1/2 from /dev/null
    Both daemon.c and shell.c contain logic to open FDs 0/1/2 from
    /dev/null if they are not already open.  Move the function in daemon.c
    to setup.c and use it in shell.c, too.
    
    While there, remove a 'not' that inverted the meaning of the comment.
    The point is indeed to *avoid* messing up.
    
    Signed-off-by: Thomas Rast <trast@inf.ethz.ch>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 1d999ddd1daa6da2779d21b293ea9b275780bff8

--- cache.h
+++ cache.h
@@ -2397,6 +2397,16 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_function_decl
+specifier|extern
+name|void
+name|sanitize_stdfds
+parameter_list|(
+name|void
+parameter_list|)
+function_decl|;
+end_function_decl
+
 begin_define
 DECL|macro|alloc_nr
 define|#
