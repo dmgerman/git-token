5db12a5456b0b85ec163fafa13bfbe32b5383a03
Thomas Rast <trast@inf.ethz.ch>
2013-07-18 14:26:55 +0200

    apply, entry: speak of submodules instead of subprojects
    There are only four (with some generous rounding) instances in the
    current source code where we speak of "subproject" instead of
    "submodule".  They are as follows:
    
    * one error message in git-apply and two in entry.c
    
    * the patch format for submodule changes
    
    The latter was introduced in 0478675 (Expose subprojects as special
    files to "git diff" machinery, 2007-04-15), apparently before the
    terminology was settled.  We can of course not change the patch
    format.
    
    Let's at least change the error messages to consistently call them
    "submodule".
    
    Signed-off-by: Thomas Rast <trast@inf.ethz.ch>
    Reviewed-by: Jonathan Nieder <jrnieder@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 42063f95a0f285382458f357bd256df252000ef1

--- builtin/apply.c
+++ builtin/apply.c
@@ -18828,7 +18828,7 @@ name|die
 argument_list|(
 name|_
 argument_list|(
-literal|"corrupt patch for subproject %s"
+literal|"corrupt patch for submodule %s"
 argument_list|)
 argument_list|,
 name|path
