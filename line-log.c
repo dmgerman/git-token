e7307b3ab828bd429c2f9d58cf91824f8102b7f4
Eric Sunshine <sunshine@sunshineco.com>
2013-07-23 10:28:08 -0400

    line-log: fix "log -LN" crash when N is last line of file
    range-set invariants are: ranges must be (1) non-empty, (2) disjoint,
    (3) sorted in ascending order.
    
    line_log_data_insert() breaks the non-empty invariant under the
    following conditions: the incoming range is empty and the pathname
    attached to the range has not yet been encountered. In this case,
    line_log_data_insert() assigns the empty range to a new line_log_data
    record without taking any action to ensure that the empty range is
    eventually folded out.  Subsequent range-set functions crash or throw an
    assertion failure upon encountering such an anomaly.  Fix this bug.
    
    Signed-off-by: Eric Sunshine <sunshine@sunshineco.com>
    Acked-by: Thomas Rast <trast@inf.ethz.ch>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: df6308eb82bc9d2074b35387583548b8b153433d

--- line-log.c
+++ line-log.c
@@ -1822,6 +1822,14 @@ argument_list|,
 name|end
 argument_list|)
 expr_stmt|;
+name|sort_and_merge_range_set
+argument_list|(
+operator|&
+name|p
+operator|->
+name|ranges
+argument_list|)
+expr_stmt|;
 if|if
 condition|(
 name|ip
