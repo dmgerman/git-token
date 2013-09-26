a93e22da0281c8187950017f90ee10f5e7ac2fd1
John Keeping <john@keeping.me.uk>
2013-09-26 21:02:48 +0100

    merge-recursive: fix parsing of "diff-algorithm" option
    The "diff-algorithm" option to the recursive merge strategy takes the
    name of the algorithm as an option, but it uses strcmp on the option
    string to check if it starts with "diff-algorithm=", meaning that this
    options cannot actually be used.
    
    Fix this by switching to prefixcmp.  At the same time, clarify the
    following line by using strlen instead of a hard-coded length, which
    also makes it consistent with nearby code.
    
    Reported-by: Luke Noel-Storr <luke.noel-storr@integrate.co.uk>
    Signed-off-by: John Keeping <john@keeping.me.uk>
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>
    
    Former-commit-id: 6562928ae9ea46a3f2a7bd5a3dd500a7c149cc64

--- merge-recursive.c
+++ merge-recursive.c
@@ -12055,7 +12055,7 @@ elseif|else
 if|if
 condition|(
 operator|!
-name|strcmp
+name|prefixcmp
 argument_list|(
 name|s
 argument_list|,
@@ -12070,7 +12070,10 @@ name|parse_algorithm_value
 argument_list|(
 name|s
 operator|+
-literal|15
+name|strlen
+argument_list|(
+literal|"diff-algorithm="
+argument_list|)
 argument_list|)
 decl_stmt|;
 if|if
