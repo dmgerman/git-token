764edd1146951d98047c99da9f6ad0adfc02d312
Jeff King <peff@peff.net>
2013-10-24 04:53:01 -0400

    assume parse_commit checks commit->object.parsed
    The parse_commit function will check the "parsed" flag of
    the object and do nothing if it is set. There is no need
    for callers to check the flag themselves, and doing so only
    clutters the code.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 0064053bd76ba385e1b5d51b6175bc17bc507804

--- builtin/blame.c
+++ builtin/blame.c
@@ -7770,15 +7770,6 @@ name|suspect
 operator|->
 name|commit
 expr_stmt|;
-if|if
-condition|(
-operator|!
-name|commit
-operator|->
-name|object
-operator|.
-name|parsed
-condition|)
 name|parse_commit
 argument_list|(
 name|commit
