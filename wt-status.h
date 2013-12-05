091ffdb32d7f2acbf4c04cc9437c373a85ccbbb9
Jens Lehmann <Jens.Lehmann@web.de>
2013-12-05 20:44:14 +0100

    commit -v: strip diffs and submodule shortlogs from the commit message
    When using the '-v' option of "git commit" the diff added to the commit
    message temporarily for editing is stripped off after the user exited the
    editor by searching for "\ndiff --git " and truncating the commmit message
    there if it is found.
    
    But this approach has two problems:
    
    - when the commit message itself contains a line starting with
      "diff --git" it will be truncated there prematurely; and
    
    - when the "diff.submodule" setting is set to "log", the diff may
      start with "Submodule <hash1>..<hash2>", which will be left in
      the commit message while it shouldn't.
    
    Fix that by introducing a special scissor separator line starting with the
    comment character ('#' or the core.commentChar config if set) followed by
    two lines describing what it is for. The scissor line - which will not be
    translated - is used to reliably detect the start of the diff so it can be
    chopped off from the commit message, no matter what the user enters there.
    
    Turn a known test failure fixed by this change into a successful test;
    also add one for a diff starting with a submodule log and another one for
    proper handling of the comment char.
    
    Reported-by: Ari Pollak <ari@debian.org>
    Signed-off-by: Jens Lehmann <Jens.Lehmann@web.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 1a72cfd7fa88e5a6c2b94568ac2fe69dfbd87f87

--- wt-status.h
+++ wt-status.h
@@ -379,6 +379,17 @@ end_struct
 
 begin_function_decl
 name|void
+name|wt_status_truncate_message_at_cut_line
+parameter_list|(
+name|struct
+name|strbuf
+modifier|*
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
+name|void
 name|wt_status_prepare
 parameter_list|(
 name|struct
