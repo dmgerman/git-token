8ada0bca3367de673fe1f21d20bb10d3a76da292
Jeff King <peff@peff.net>
2014-01-15 06:17:48 -0500

    do not discard revindex when re-preparing packfiles
    When an object lookup fails, we re-read the objects/pack
    directory to pick up any new packfiles that may have been
    created since our last read. We also discard any pack
    revindex structs we've allocated.
    
    The discarding is a problem for the pack-bitmap code, which keeps
    a pointer to the revindex for the bitmapped pack. After the
    discard, the pointer is invalid, and we may read free()d
    memory.
    
    Other revindex users do not keep a bare pointer to the
    revindex; instead, they always access it through
    revindex_for_pack(), which lazily builds the revindex. So
    one solution is to teach the pack-bitmap code a similar
    trick. It would be slightly less efficient, but probably not
    all that noticeable.
    
    However, it turns out this discarding is not actually
    necessary. When we call reprepare_packed_git, we do not
    throw away our old pack list. We keep the existing entries,
    and only add in new ones. So there is no safety problem; we
    will still have the pack struct that matches each revindex.
    The packfile itself may go away, of course, but we are
    already prepared to handle that, and it may happen outside
    of reprepare_packed_git anyway.
    
    Throwing away the revindex may save some RAM if the pack
    never gets reused (about 12 bytes per object). But it also
    wastes some CPU time (to regenerate the index) if the pack
    does get reused. It's hard to say which is more valuable,
    but in either case, it happens very rarely (only when we
    race with a simultaneous repack). Just leaving the revindex
    in place is simple and safe both for current and future
    code.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 1a6d8b91489ad4a7b7d267b46a3e838b004157f1

--- pack-revindex.h
+++ pack-revindex.h
@@ -97,15 +97,6 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
-begin_function_decl
-name|void
-name|discard_revindex
-parameter_list|(
-name|void
-parameter_list|)
-function_decl|;
-end_function_decl
-
 begin_endif
 endif|#
 directive|endif
