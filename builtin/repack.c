63229be5c5692c66ca310fbfab3bef8287753fd3
Torsten Bögershausen <tboegi@web.de>
2014-02-02 16:09:56 +0100

    repack.c: rename and unlink pack file if it exists
    When a repo was fully repacked, and is repacked again, we may run
    into the situation that "new" packfiles have the same name as
    already existing ones (traditionally packfiles have been named after
    the list of names of objects in them, so repacking all the objects
    in a single pack would have produced a packfile with the same name).
    
    The logic is to rename the existing ones into filename like
    "old-XXX", create the new ones and then remove the "old-" ones.
    When something went wrong in the middle, this sequence is rolled
    back by renaming the "old-" files back.
    
    The renaming into "old-" did not work as intended, because
    file_exists() was done on "XXX", not "pack-XXX".  Also when rolling
    back the change, the code tried to rename "old-pack-XXX" but the
    saved ones are named "old-XXX", so this couldn't have worked.
    
    Signed-off-by: Torsten Bögershausen <tboegi@web.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 9d7fbfd20422cd23fdb346ac8d609b45b4d37001

--- builtin/repack.c
+++ builtin/repack.c
@@ -1459,7 +1459,7 @@ name|fname
 operator|=
 name|mkpathdup
 argument_list|(
-literal|"%s/%s%s"
+literal|"%s/pack-%s%s"
 argument_list|,
 name|packdir
 argument_list|,
@@ -1878,7 +1878,7 @@ name|fname
 operator|=
 name|mkpath
 argument_list|(
-literal|"%s/old-pack-%s%s"
+literal|"%s/old-%s%s"
 argument_list|,
 name|packdir
 argument_list|,
