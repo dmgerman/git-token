55016240508c765a7869c3ba8ca1e20f226a32eb
Ronnie Sahlberg <sahlberg@google.com>
2014-04-16 15:27:45 -0700

    refs.c: update ref_transaction_delete to check for error and return status
    Change ref_transaction_delete() to do basic error checking and return
    non-zero on error. Update all callers to check the return for
    ref_transaction_delete(). There are currently no conditions in _delete that
    will return error but there will be in the future. Add an err argument that
    will be updated on failure.
    
    Signed-off-by: Ronnie Sahlberg <sahlberg@google.com>
    Reviewed-by: Michael Haggerty <mhagger@alum.mit.edu>
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 8c8bdc0d3582e42c13815a191344b1aa3e06c792

--- builtin/update-ref.c
+++ builtin/update-ref.c
@@ -1104,6 +1104,8 @@ argument_list|,
 name|next
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
 name|ref_transaction_delete
 argument_list|(
 name|transaction
@@ -1115,6 +1117,18 @@ argument_list|,
 name|update_flags
 argument_list|,
 name|have_old
+argument_list|,
+operator|&
+name|err
+argument_list|)
+condition|)
+name|die
+argument_list|(
+literal|"%s"
+argument_list|,
+name|err
+operator|.
+name|buf
 argument_list|)
 expr_stmt|;
 name|update_flags
