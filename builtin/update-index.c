7637616831c6d9d5f04f965265e565b5ab4713f0
Jeff King <peff@peff.net>
2014-06-04 03:11:11 -0400

    update-index: fix segfault with missing --cacheinfo argument
    Running "git update-index --cacheinfo" without any further
    arguments results in a segfault rather than an error
    message. Commit ec160ae (update-index: teach --cacheinfo a
    new syntax "mode,sha1,path", 2014-03-23) added code to
    examine the format of the argument, but forgot to handle the
    NULL case.
    
    Returning an error from the parser is enough, since we then
    treat it as an old-style "--cacheinfo <mode> <sha1> <path>",
    and complain that we have less than 3 arguments to read.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: c8e1ee4f2c8e28b7eef824248f09ba37599593d7

--- builtin/update-index.c
+++ builtin/update-index.c
@@ -3022,6 +3022,15 @@ name|char
 modifier|*
 name|endp
 decl_stmt|;
+if|if
+condition|(
+operator|!
+name|arg
+condition|)
+return|return
+operator|-
+literal|1
+return|;
 name|errno
 operator|=
 literal|0
