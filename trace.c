d2c3c72802d7c39ef09c7f1ec95ba88cc8a2ff5c
Karsten Blees <karsten.blees@gmail.com>
2014-06-11 09:57:59 +0200

    trace: remove redundant printf format attribute
    trace_printf_key() is the only non-static function that duplicates the
    printf format attribute in the .c file, remove it for consistency.
    
    Signed-off-by: Karsten Blees <blees@dcon.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 0d0424272f85afb5262613829cf1f48f994cebc7

--- trace.c
+++ trace.c
@@ -294,17 +294,6 @@ expr_stmt|;
 block|}
 end_function
 
-begin_macro
-name|__attribute__
-argument_list|(
-argument|(format (printf,
-literal|2
-argument|,
-literal|3
-argument|))
-argument_list|)
-end_macro
-
 begin_function
 DECL|function|trace_printf_key
 name|void
