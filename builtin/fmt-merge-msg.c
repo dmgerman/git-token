23c6fbdcc85e24cdf6b8ece8a168585ba6edadf3
Jeff King <peff@peff.net>
2014-06-18 15:48:29 -0400

    use skip_prefix to avoid repeating strings
    It's a common idiom to match a prefix and then skip past it
    with strlen, like:
    
      if (starts_with(foo, "bar"))
    	  foo += strlen("bar");
    
    This avoids magic numbers, but means we have to repeat the
    string (and there is no compiler check that we didn't make a
    typo in one of the strings).
    
    We can use skip_prefix to handle this case without repeating
    ourselves.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 95b567c7c3cf6b85d74b79424cdfbd40a7dee7c9

--- builtin/fmt-merge-msg.c
+++ builtin/fmt-merge-msg.c
@@ -625,7 +625,9 @@ decl_stmt|;
 name|char
 modifier|*
 name|src
-decl_stmt|,
+decl_stmt|;
+specifier|const
+name|char
 modifier|*
 name|origin
 decl_stmt|;
@@ -959,23 +961,17 @@ block|}
 elseif|else
 if|if
 condition|(
-name|starts_with
+name|skip_prefix
 argument_list|(
 name|line
 argument_list|,
 literal|"remote-tracking branch "
+argument_list|,
+operator|&
+name|origin
 argument_list|)
 condition|)
 block|{
-name|origin
-operator|=
-name|line
-operator|+
-name|strlen
-argument_list|(
-literal|"remote-tracking branch "
-argument_list|)
-expr_stmt|;
 name|string_list_append
 argument_list|(
 operator|&
