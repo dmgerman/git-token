141047c9583a1317ef3ee9158f1e7762a8f410a8
René Scharfe <l.s.r@web.de>
2014-07-20 10:00:41 +0200

    unix-socket: remove stale socket before calling chdir()
    unix_stream_listen() is given a path.  It calls unix_sockaddr_init(),
    which in turn can call chdir().  After that a relative path doesn't
    mean the same as before.  Any use of the original path should thus
    happen before that call.  For that reason, unlink the given path
    (to get rid of a possibly existing stale socket) right at the
    beginning of the function.
    
    Noticed-by: Karsten Blees <karsten.blees@gmail.com>
    Signed-off-by: Rene Scharfe <l.s.r@web.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 2869b3e5dabdd78b336455ad493951badd35279e

--- unix-socket.c
+++ unix-socket.c
@@ -491,6 +491,11 @@ name|struct
 name|unix_sockaddr_context
 name|ctx
 decl_stmt|;
+name|unlink
+argument_list|(
+name|path
+argument_list|)
+expr_stmt|;
 if|if
 condition|(
 name|unix_sockaddr_init
@@ -514,11 +519,6 @@ name|fd
 operator|=
 name|unix_stream_socket
 argument_list|()
-expr_stmt|;
-name|unlink
-argument_list|(
-name|path
-argument_list|)
 expr_stmt|;
 if|if
 condition|(
