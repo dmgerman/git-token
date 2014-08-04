f7590622c3f9f1b7ac73f42ce7dd93fdf033655f
Tanay Abhra <tanayabh@gmail.com>
2014-08-04 07:41:15 -0700

    pretty.c: make git_pretty_formats_config return -1 on git_config_string failure
    `git_pretty_formats_config()` continues without checking git_config_string's
    return value which can lead to a SEGFAULT. Instead return -1 when
    git_config_string fails signalling `git_config()` to die printing the location
    of the erroneous variable.
    
    Signed-off-by: Tanay Abhra <tanayabh@gmail.com>
    Reviewed-by: Matthieu Moy <Matthieu.Moy@imag.fr>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: a26bc613a64ac2c7ee69a50675e61b004a26382d

--- pretty.c
+++ pretty.c
@@ -402,6 +402,8 @@ name|format
 operator|=
 name|CMIT_FMT_USERFORMAT
 expr_stmt|;
+if|if
+condition|(
 name|git_config_string
 argument_list|(
 operator|&
@@ -411,7 +413,11 @@ name|var
 argument_list|,
 name|value
 argument_list|)
-expr_stmt|;
+condition|)
+return|return
+operator|-
+literal|1
+return|;
 if|if
 condition|(
 name|starts_with
