956c1843bdd98ca039a2a38b89c9336c8cd553f0
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2014-08-10 09:29:35 +0700

    mv: combine two if(s)
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: dcadc8b80609f6857ab35163a8a6e08cdaf35b8f

--- builtin/mv.c
+++ builtin/mv.c
@@ -1890,10 +1890,7 @@ expr_stmt|;
 if|if
 condition|(
 name|active_cache_changed
-condition|)
-block|{
-if|if
-condition|(
+operator|&&
 name|write_locked_index
 argument_list|(
 operator|&
@@ -1913,7 +1910,6 @@ literal|"Unable to write new index file"
 argument_list|)
 argument_list|)
 expr_stmt|;
-block|}
 return|return
 literal|0
 return|;
