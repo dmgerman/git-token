db5ae91c3b04309827b0c62f2bc5c95dc291e4a0
René Scharfe <l.s.r@web.de>
2014-08-21 20:30:24 +0200

    walker: avoid quadratic list insertion in mark_complete
    Similar to 16445242 (fetch-pack: avoid quadratic list insertion in
    mark_complete), sort only after all refs are collected instead of while
    inserting.  The result is the same, but it's more efficient that way.
    The difference will only be measurable in repositories with a large
    number of refs.
    
    Signed-off-by: Rene Scharfe <l.s.r@web.de>
    Acked-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 3bc7a05b1a78b850da94ca85267ca279489ce70f

--- walker.c
+++ walker.c
@@ -1172,7 +1172,7 @@ name|flags
 operator||=
 name|COMPLETE
 expr_stmt|;
-name|commit_list_insert_by_date
+name|commit_list_insert
 argument_list|(
 name|commit
 argument_list|,
@@ -1586,6 +1586,7 @@ name|walker
 operator|->
 name|get_recover
 condition|)
+block|{
 name|for_each_ref
 argument_list|(
 name|mark_complete
@@ -1593,6 +1594,13 @@ argument_list|,
 name|NULL
 argument_list|)
 expr_stmt|;
+name|commit_list_sort_by_date
+argument_list|(
+operator|&
+name|complete
+argument_list|)
+expr_stmt|;
+block|}
 for|for
 control|(
 name|i
