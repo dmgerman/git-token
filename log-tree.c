569b4f960822b15f5d46330e87cb654dfa50dc35
Jonathan Nieder <jrnieder@gmail.com>
2014-10-07 15:16:57 -0400

    pass config slots as pointers instead of offsets
    Many config-parsing helpers, like parse_branch_color_slot,
    take the name of a config variable and an offset to the
    "slot" name (e.g., "color.branch.plain" is passed along with
    "13" to effectively pass "plain"). This is leftover from the
    time that these functions would die() on error, and would
    want the full variable name for error reporting.
    
    These days they do not use the full variable name at all.
    Passing a single pointer to the slot name is more natural,
    and lets us more easily adjust the callers to use skip_prefix
    to avoid manually writing offset numbers.
    
    This is effectively a continuation of 9e1a5eb, which did the
    same for parse_diff_color_slot. This patch covers all of the
    remaining similar constructs.
    
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 8852117a603c5ed5131233a80453db37c0958871

--- log-tree.c
+++ log-tree.c
@@ -286,8 +286,9 @@ modifier|*
 name|var
 parameter_list|,
 specifier|const
-name|int
-name|ofs
+name|char
+modifier|*
+name|slot_name
 parameter_list|,
 specifier|const
 name|char
@@ -300,9 +301,7 @@ name|slot
 init|=
 name|parse_decorate_color_slot
 argument_list|(
-name|var
-operator|+
-name|ofs
+name|slot_name
 argument_list|)
 decl_stmt|;
 if|if
