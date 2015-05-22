c74f2da2b9c77c99224053a634bccbedf0a6dd6c
Michael Haggerty <mhagger@alum.mit.edu>
2015-05-23 01:34:56 +0200

    verify_lock(): do not capitalize error messages
    Our convention is for error messages to start with a lower-case
    letter.
    
    Signed-off-by: Michael Haggerty <mhagger@alum.mit.edu>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 000f0da57aca00d6ff960f0763dee73c34cd7677

--- refs.c
+++ refs.c
@@ -9592,7 +9592,7 @@ name|strbuf_addf
 argument_list|(
 name|err
 argument_list|,
-literal|"Can't verify ref %s"
+literal|"can't verify ref %s"
 argument_list|,
 name|lock
 operator|->
@@ -9624,7 +9624,7 @@ name|strbuf_addf
 argument_list|(
 name|err
 argument_list|,
-literal|"Ref %s is at %s but expected %s"
+literal|"ref %s is at %s but expected %s"
 argument_list|,
 name|lock
 operator|->
