67c349048817abea1cf2cf8305a46f50d81e020e
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2015-06-29 19:51:18 +0700

    worktree: new place for "git prune --worktrees"
    Commit 23af91d (prune: strategies for linked checkouts - 2014-11-30)
    adds "--worktrees" to "git prune" without realizing that "git prune" is
    for object database only. This patch moves the same functionality to a
    new command "git worktree".
    
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    
    Former-commit-id: df0b6cfbda88144714541664fb501146d6465a82

--- git.c
+++ git.c
@@ -3066,6 +3066,14 @@ name|RUN_SETUP
 block|}
 block|,
 block|{
+literal|"worktree"
+block|,
+name|cmd_worktree
+block|,
+name|RUN_SETUP
+block|}
+block|,
+block|{
 literal|"write-tree"
 block|,
 name|cmd_write_tree
