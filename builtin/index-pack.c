b0ed88778d1fb03ea8013d9b4bcc413875296d49
Junio C Hamano <gitster@pobox.com>
2015-07-03 09:51:57 -0700

    index-pack: fix allocation of sorted_by_pos array
    When c6458e60 (index-pack: kill union delta_base to save memory,
    2015-04-18) attempted to reduce the memory footprint of index-pack,
    one of the key thing it did was to keep track of ref-deltas and
    ofs-deltas separately.
    
    In fix_unresolved_deltas(), however it forgot that it now wants to
    look only at ref deltas in one place.  The code allocated an array
    for nr_unresolved, which is sum of number of ref- and ofs-deltas
    minus nr_resolved, which may be larger or smaller than the number
    ref-deltas.  Depending on nr_resolved, this was either under or over
    allocating.
    
    Also, the old code before this change had to use 'i' and 'n' because
    some of the things we see in the (old) deltas[] array we scanned
    with 'i' would not make it into the sorted_by_pos[] array in the old
    world order, but now because you have only ref delta in a separate
    ref_deltas[] array, they increment lock&step.  We no longer need
    separate variables.  And most importantly, we shouldn't pass the
    nr_unresolved parameter, as this number does not play a role in the
    working of this helper function.
    
    Helped-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 781d93067d3ab8b448a4c8989a6e3cac17009061

--- builtin/index-pack.c
+++ builtin/index-pack.c
@@ -6710,9 +6710,6 @@ name|struct
 name|sha1file
 modifier|*
 name|f
-parameter_list|,
-name|int
-name|nr_unresolved
 parameter_list|)
 function_decl|;
 end_function_decl
@@ -6856,8 +6853,6 @@ expr_stmt|;
 name|fix_unresolved_deltas
 argument_list|(
 name|f
-argument_list|,
-name|nr_unresolved
 argument_list|)
 expr_stmt|;
 name|strbuf_addf
@@ -7438,9 +7433,6 @@ name|struct
 name|sha1file
 modifier|*
 name|f
-parameter_list|,
-name|int
-name|nr_unresolved
 parameter_list|)
 block|{
 name|struct
@@ -7451,17 +7443,13 @@ name|sorted_by_pos
 decl_stmt|;
 name|int
 name|i
-decl_stmt|,
-name|n
-init|=
-literal|0
 decl_stmt|;
 comment|/* 	 * Since many unresolved deltas may well be themselves base objects 	 * for more unresolved deltas, we really want to include the 	 * smallest number of base objects that would cover as much delta 	 * as possible by picking the 	 * trunc deltas first, allowing for other deltas to resolve without 	 * additional base objects.  Since most base objects are to be found 	 * before deltas depending on them, a good heuristic is to start 	 * resolving deltas in the same order as their position in the pack. 	 */
 name|sorted_by_pos
 operator|=
 name|xmalloc
 argument_list|(
-name|nr_unresolved
+name|nr_ref_deltas
 operator|*
 sizeof|sizeof
 argument_list|(
@@ -7485,8 +7473,7 @@ operator|++
 control|)
 name|sorted_by_pos
 index|[
-name|n
-operator|++
+name|i
 index|]
 operator|=
 operator|&
@@ -7499,7 +7486,7 @@ name|qsort
 argument_list|(
 name|sorted_by_pos
 argument_list|,
-name|n
+name|nr_ref_deltas
 argument_list|,
 sizeof|sizeof
 argument_list|(
@@ -7518,7 +7505,7 @@ literal|0
 init|;
 name|i
 operator|<
-name|n
+name|nr_ref_deltas
 condition|;
 name|i
 operator|++
