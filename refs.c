776a955bc93f4b7e2406bf6f69222f1af2a3106a
David Turner <dturner@twopensource.com>
2015-07-15 18:05:28 -0400

    pseudoref: check return values from read_ref()
    These codepaths attempt to compare the "expected" current value with
    the actual current value, but did not check if we successfully read
    the current value before comparison.
    
    Signed-off-by: David Turner <dturner@twopensource.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 2c3aed1381f22494bc06fd66dec8292a296db10f

--- refs.c
+++ refs.c
@@ -12538,11 +12538,20 @@ index|[
 literal|20
 index|]
 decl_stmt|;
+if|if
+condition|(
 name|read_ref
 argument_list|(
 name|pseudoref
 argument_list|,
 name|actual_old_sha1
+argument_list|)
+condition|)
+name|die
+argument_list|(
+literal|"could not read ref '%s'"
+argument_list|,
+name|pseudoref
 argument_list|)
 expr_stmt|;
 if|if
@@ -12724,11 +12733,20 @@ argument_list|,
 name|filename
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
 name|read_ref
 argument_list|(
 name|pseudoref
 argument_list|,
 name|actual_old_sha1
+argument_list|)
+condition|)
+name|die
+argument_list|(
+literal|"could not read ref '%s'"
+argument_list|,
+name|pseudoref
 argument_list|)
 expr_stmt|;
 if|if
