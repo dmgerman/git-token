855b84e76f442032257347ed97cfdc2da703eff1
Jeff King <peff@peff.net>
2015-10-26 17:39:39 -0400

    merge: detect delete/modechange conflict
    If one side deletes a file and the other changes its
    content, we notice and report a conflict. However, if
    instead of changing the content, we change only the mode,
    the merge does not notice (and the mode change is silently
    dropped).
    
    The trivial index merge notices the problem and correctly
    leaves the conflict in the index, but both merge-recursive
    and merge-one-file will silently resolve this in favor of
    the deletion.  In many cases that is a sane resolution, but
    we should be punting to the user whenever there is any
    question. So let's detect and treat this as a conflict (in
    both strategies).
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 72fac66bca302dbecbe42dfa0ddc7e42db2fe567

--- merge-recursive.c
+++ merge-recursive.c
@@ -8765,11 +8765,17 @@ name|char
 modifier|*
 name|o_sha
 parameter_list|,
+name|unsigned
+name|o_mode
+parameter_list|,
 specifier|const
 name|unsigned
 name|char
 modifier|*
 name|a_sha
+parameter_list|,
+name|unsigned
+name|a_mode
 parameter_list|,
 name|int
 name|renormalize
@@ -8798,6 +8804,15 @@ init|=
 literal|0
 decl_stmt|;
 comment|/* assume changed for safety */
+if|if
+condition|(
+name|a_mode
+operator|!=
+name|o_mode
+condition|)
+return|return
+literal|0
+return|;
 if|if
 condition|(
 name|sha_eq
@@ -9930,7 +9945,11 @@ name|blob_unchanged
 argument_list|(
 name|o_sha
 argument_list|,
+name|o_mode
+argument_list|,
 name|a_sha
+argument_list|,
+name|a_mode
 argument_list|,
 name|normalize
 argument_list|,
@@ -9946,7 +9965,11 @@ name|blob_unchanged
 argument_list|(
 name|o_sha
 argument_list|,
+name|o_mode
+argument_list|,
 name|b_sha
+argument_list|,
+name|b_mode
 argument_list|,
 name|normalize
 argument_list|,
