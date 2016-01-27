2b2ccf62c8c5041d549e5f0131ba13df0d1bd09a
Junio C Hamano <gitster@pobox.com>
2016-01-26 22:52:02 -0800

    git: simplify environment save/restore logic
    The only code that cares about the value of the global variable
    saved_env_before_alias after the previous fix is handle_builtin()
    that turns into a glorified no-op when the variable is true, so the
    logic could safely be lifted to its caller, i.e. the caller can
    refrain from calling it when the variable is set.
    
    This variable tells us if save_env_before_alias() was called (with
    or without matching restore_env()), but the sole caller of the
    function, handle_alias(), always calls it as the first thing, so we
    can consider that the variable essentially keeps track of the fact
    that handle_alias() has ever been called.
    
    It turns out that handle_builtin() and handle_alias() are called
    only from one function in a way that the value of the variable
    matters, which is run_argv(), and it already keeps track of the
    fact that it already called handle_alias().
    
    So we can simplify the whole thing by:
    
    - Change handle_builtin() to always make a direct call to the
      builtin implementation it finds, and make sure the caller
      refrains from calling it if handle_alias() has ever been
      called;
    
    - Remove saved_env_before_alias variable, and instead use the
      local "done_alias" variable maintained inside run_argv() to
      make the same decision.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 441981bc85ea2b648d7ffb2515b371071208e657

--- git.c
+++ git.c
@@ -117,14 +117,6 @@ decl_stmt|;
 end_decl_stmt
 
 begin_decl_stmt
-DECL|variable|saved_env_before_alias
-specifier|static
-name|int
-name|saved_env_before_alias
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
 DECL|variable|save_restore_env_balance
 specifier|static
 name|int
@@ -144,10 +136,6 @@ block|{
 name|int
 name|i
 decl_stmt|;
-name|saved_env_before_alias
-operator|=
-literal|1
-expr_stmt|;
 name|assert
 argument_list|(
 name|save_restore_env_balance
@@ -3358,13 +3346,6 @@ if|if
 condition|(
 name|builtin
 condition|)
-block|{
-comment|/* 		 * XXX: if we can figure out cases where it is _safe_ 		 * to do, we can avoid spawning a new process when 		 * saved_env_before_alias is true 		 * (i.e. setup_git_dir* has been run once) 		 */
-if|if
-condition|(
-operator|!
-name|saved_env_before_alias
-condition|)
 name|exit
 argument_list|(
 name|run_builtin
@@ -3377,7 +3358,6 @@ name|argv
 argument_list|)
 argument_list|)
 expr_stmt|;
-block|}
 block|}
 end_function
 
@@ -3536,7 +3516,12 @@ condition|(
 literal|1
 condition|)
 block|{
-comment|/* See if it's a builtin */
+comment|/* 		 * If we tried alias and futzed with our environment, 		 * it no longer is safe to invoke builtins directly in 		 * general.  We have to spawn them as dashed externals. 		 * 		 * NEEDSWORK: if we can figure out cases 		 * where it is safe to do, we can avoid spawning a new 		 * process. 		 */
+if|if
+condition|(
+operator|!
+name|done_alias
+condition|)
 name|handle_builtin
 argument_list|(
 operator|*
