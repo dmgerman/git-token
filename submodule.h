aeef2a13e2c9cc172e5cdc6cd08ecf657fc86be1
Stefan Beller <sbeller@google.com>
2016-02-29 18:07:13 -0800

    fetching submodules: respect `submodule.fetchJobs` config option
    This allows to configure fetching and updating in parallel
    without having the command line option.
    
    This moved the responsibility to determine how many parallel processes
    to start from builtin/fetch to submodule.c as we need a way to communicate
    "The user did not specify the number of parallel processes in the command
    line options" in the builtin fetch. The submodule code takes care of
    the precedence (CLI > config > default).
    
    Reviewed-by: Jonathan Nieder <jrnieder@gmail.com>
    Signed-off-by: Stefan Beller <sbeller@google.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: a028a1930c6b4b848e8fb47cc92c30b23d99a75e

--- submodule.h
+++ submodule.h
@@ -111,6 +111,14 @@ block|}
 struct|;
 end_struct
 
+begin_define
+DECL|macro|SUBMODULE_UPDATE_STRATEGY_INIT
+define|#
+directive|define
+name|SUBMODULE_UPDATE_STRATEGY_INIT
+value|{SM_UPDATE_UNSPECIFIED, NULL}
+end_define
+
 begin_function_decl
 name|int
 name|is_staging_gitmodules_ok
@@ -488,6 +496,15 @@ specifier|const
 name|char
 modifier|*
 name|git_dir
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
+name|int
+name|parallel_submodules
+parameter_list|(
+name|void
 parameter_list|)
 function_decl|;
 end_function_decl
