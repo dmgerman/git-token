dmgerman/git-token

commit 98fae13eb6dfc4aa0c54b00ef5a8359f129e2e06

submodule: rewrite `module_list` shell function in C
Most of the submodule operations work on a set of submodules.
Calculating and using this set is usually done via:

       module_list "$@" | {
           while read mode sha1 stage sm_path
           do
                # the actual operation
           done
       }

Currently the function `module_list` is implemented in the
git-submodule.sh as a shell script wrapping a perl script.
The rewrite is in C, such that it is faster and can later be
easily adapted when other functions are rewritten in C.

git-submodule.sh, similar to the builtin commands, will navigate
to the top-most directory of the repository and keep the
subdirectory as a variable. As the helper is called from
within the git-submodule.sh script, we are already navigated
to the root level, but the path arguments are still relative
to the subdirectory we were in when calling git-submodule.sh.
That's why there is a `--prefix` option pointing to an alternative
path which to anchor relative path arguments.

Signed-off-by: Stefan Beller <sbeller@google.com>
Signed-off-by: Junio C Hamano <gitster@pobox.com>

Former-commit-id: 74703a1e4dfc5affcb8944e78b53f0817b492246

builtin/submodule--helper.c
@@ -1,0 +1,719 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_include
+include|#
+directive|include
+file|"builtin.h"
+end_include
+
+begin_include
+include|#
+directive|include
+file|"cache.h"
+end_include
+
+begin_include
+include|#
+directive|include
+file|"parse-options.h"
+end_include
+
+begin_include
+include|#
+directive|include
+file|"quote.h"
+end_include
+
+begin_include
+include|#
+directive|include
+file|"pathspec.h"
+end_include
+
+begin_include
+include|#
+directive|include
+file|"dir.h"
+end_include
+
+begin_include
+include|#
+directive|include
+file|"utf8.h"
+end_include
+
+begin_struct
+DECL|struct|module_list
+struct|struct
+name|module_list
+block|{
+DECL|member|entries
+specifier|const
+name|struct
+name|cache_entry
+modifier|*
+modifier|*
+name|entries
+decl_stmt|;
+DECL|member|alloc
+DECL|member|nr
+name|int
+name|alloc
+decl_stmt|,
+name|nr
+decl_stmt|;
+block|}
+struct|;
+end_struct
+
+begin_define
+DECL|macro|MODULE_LIST_INIT
+define|#
+directive|define
+name|MODULE_LIST_INIT
+value|{ NULL, 0, 0 }
+end_define
+
+begin_function
+DECL|function|module_list_compute
+specifier|static
+name|int
+name|module_list_compute
+parameter_list|(
+name|int
+name|argc
+parameter_list|,
+specifier|const
+name|char
+modifier|*
+modifier|*
+name|argv
+parameter_list|,
+specifier|const
+name|char
+modifier|*
+name|prefix
+parameter_list|,
+name|struct
+name|pathspec
+modifier|*
+name|pathspec
+parameter_list|,
+name|struct
+name|module_list
+modifier|*
+name|list
+parameter_list|)
+block|{
+name|int
+name|i
+decl_stmt|,
+name|result
+init|=
+literal|0
+decl_stmt|;
+name|char
+modifier|*
+name|max_prefix
+decl_stmt|,
+modifier|*
+name|ps_matched
+init|=
+name|NULL
+decl_stmt|;
+name|int
+name|max_prefix_len
+decl_stmt|;
+name|parse_pathspec
+argument_list|(
+name|pathspec
+argument_list|,
+literal|0
+argument_list|,
+name|PATHSPEC_PREFER_FULL
+operator||
+name|PATHSPEC_STRIP_SUBMODULE_SLASH_CHEAP
+argument_list|,
+name|prefix
+argument_list|,
+name|argv
+argument_list|)
+expr_stmt|;
+comment|/* Find common prefix for all pathspec's */
+name|max_prefix
+operator|=
+name|common_prefix
+argument_list|(
+name|pathspec
+argument_list|)
+expr_stmt|;
+name|max_prefix_len
+operator|=
+name|max_prefix
+condition|?
+name|strlen
+argument_list|(
+name|max_prefix
+argument_list|)
+else|:
+literal|0
+expr_stmt|;
+if|if
+condition|(
+name|pathspec
+operator|->
+name|nr
+condition|)
+name|ps_matched
+operator|=
+name|xcalloc
+argument_list|(
+name|pathspec
+operator|->
+name|nr
+argument_list|,
+literal|1
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|read_cache
+argument_list|()
+operator|<
+literal|0
+condition|)
+name|die
+argument_list|(
+name|_
+argument_list|(
+literal|"index file corrupt"
+argument_list|)
+argument_list|)
+expr_stmt|;
+for|for
+control|(
+name|i
+operator|=
+literal|0
+init|;
+name|i
+operator|<
+name|active_nr
+condition|;
+name|i
+operator|++
+control|)
+block|{
+specifier|const
+name|struct
+name|cache_entry
+modifier|*
+name|ce
+init|=
+name|active_cache
+index|[
+name|i
+index|]
+decl_stmt|;
+if|if
+condition|(
+operator|!
+name|S_ISGITLINK
+argument_list|(
+name|ce
+operator|->
+name|ce_mode
+argument_list|)
+operator|||
+operator|!
+name|match_pathspec
+argument_list|(
+name|pathspec
+argument_list|,
+name|ce
+operator|->
+name|name
+argument_list|,
+name|ce_namelen
+argument_list|(
+name|ce
+argument_list|)
+argument_list|,
+name|max_prefix_len
+argument_list|,
+name|ps_matched
+argument_list|,
+literal|1
+argument_list|)
+condition|)
+continue|continue;
+name|ALLOC_GROW
+argument_list|(
+name|list
+operator|->
+name|entries
+argument_list|,
+name|list
+operator|->
+name|nr
+operator|+
+literal|1
+argument_list|,
+name|list
+operator|->
+name|alloc
+argument_list|)
+expr_stmt|;
+name|list
+operator|->
+name|entries
+index|[
+name|list
+operator|->
+name|nr
+operator|++
+index|]
+operator|=
+name|ce
+expr_stmt|;
+while|while
+condition|(
+name|i
+operator|+
+literal|1
+operator|<
+name|active_nr
+operator|&&
+operator|!
+name|strcmp
+argument_list|(
+name|ce
+operator|->
+name|name
+argument_list|,
+name|active_cache
+index|[
+name|i
+operator|+
+literal|1
+index|]
+operator|->
+name|name
+argument_list|)
+condition|)
+comment|/* 			 * Skip entries with the same name in different stages 			 * to make sure an entry is returned only once. 			 */
+name|i
+operator|++
+expr_stmt|;
+block|}
+name|free
+argument_list|(
+name|max_prefix
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|ps_matched
+operator|&&
+name|report_path_error
+argument_list|(
+name|ps_matched
+argument_list|,
+name|pathspec
+argument_list|,
+name|prefix
+argument_list|)
+condition|)
+name|result
+operator|=
+operator|-
+literal|1
+expr_stmt|;
+name|free
+argument_list|(
+name|ps_matched
+argument_list|)
+expr_stmt|;
+return|return
+name|result
+return|;
+block|}
+end_function
+
+begin_function
+DECL|function|module_list
+specifier|static
+name|int
+name|module_list
+parameter_list|(
+name|int
+name|argc
+parameter_list|,
+specifier|const
+name|char
+modifier|*
+modifier|*
+name|argv
+parameter_list|,
+specifier|const
+name|char
+modifier|*
+name|prefix
+parameter_list|)
+block|{
+name|int
+name|i
+decl_stmt|;
+name|struct
+name|pathspec
+name|pathspec
+decl_stmt|;
+name|struct
+name|module_list
+name|list
+init|=
+name|MODULE_LIST_INIT
+decl_stmt|;
+name|struct
+name|option
+name|module_list_options
+index|[]
+init|=
+block|{
+name|OPT_STRING
+argument_list|(
+literal|0
+argument_list|,
+literal|"prefix"
+argument_list|,
+operator|&
+name|prefix
+argument_list|,
+name|N_
+argument_list|(
+literal|"path"
+argument_list|)
+argument_list|,
+name|N_
+argument_list|(
+literal|"alternative anchor for relative paths"
+argument_list|)
+argument_list|)
+block|,
+name|OPT_END
+argument_list|()
+block|}
+decl_stmt|;
+specifier|const
+name|char
+modifier|*
+specifier|const
+name|git_submodule_helper_usage
+index|[]
+init|=
+block|{
+name|N_
+argument_list|(
+literal|"git submodule--helper list [--prefix=<path>] [<path>...]"
+argument_list|)
+block|,
+name|NULL
+block|}
+decl_stmt|;
+name|argc
+operator|=
+name|parse_options
+argument_list|(
+name|argc
+argument_list|,
+name|argv
+argument_list|,
+name|prefix
+argument_list|,
+name|module_list_options
+argument_list|,
+name|git_submodule_helper_usage
+argument_list|,
+literal|0
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|module_list_compute
+argument_list|(
+name|argc
+argument_list|,
+name|argv
+argument_list|,
+name|prefix
+argument_list|,
+operator|&
+name|pathspec
+argument_list|,
+operator|&
+name|list
+argument_list|)
+operator|<
+literal|0
+condition|)
+block|{
+name|printf
+argument_list|(
+literal|"#unmatched\n"
+argument_list|)
+expr_stmt|;
+return|return
+literal|1
+return|;
+block|}
+for|for
+control|(
+name|i
+operator|=
+literal|0
+init|;
+name|i
+operator|<
+name|list
+operator|.
+name|nr
+condition|;
+name|i
+operator|++
+control|)
+block|{
+specifier|const
+name|struct
+name|cache_entry
+modifier|*
+name|ce
+init|=
+name|list
+operator|.
+name|entries
+index|[
+name|i
+index|]
+decl_stmt|;
+if|if
+condition|(
+name|ce_stage
+argument_list|(
+name|ce
+argument_list|)
+condition|)
+name|printf
+argument_list|(
+literal|"%06o %s U\t"
+argument_list|,
+name|ce
+operator|->
+name|ce_mode
+argument_list|,
+name|sha1_to_hex
+argument_list|(
+name|null_sha1
+argument_list|)
+argument_list|)
+expr_stmt|;
+else|else
+name|printf
+argument_list|(
+literal|"%06o %s %d\t"
+argument_list|,
+name|ce
+operator|->
+name|ce_mode
+argument_list|,
+name|sha1_to_hex
+argument_list|(
+name|ce
+operator|->
+name|sha1
+argument_list|)
+argument_list|,
+name|ce_stage
+argument_list|(
+name|ce
+argument_list|)
+argument_list|)
+expr_stmt|;
+name|utf8_fprintf
+argument_list|(
+name|stdout
+argument_list|,
+literal|"%s\n"
+argument_list|,
+name|ce
+operator|->
+name|name
+argument_list|)
+expr_stmt|;
+block|}
+return|return
+literal|0
+return|;
+block|}
+end_function
+
+begin_struct
+DECL|struct|cmd_struct
+struct|struct
+name|cmd_struct
+block|{
+DECL|member|cmd
+specifier|const
+name|char
+modifier|*
+name|cmd
+decl_stmt|;
+DECL|member|fn
+name|int
+function_decl|(
+modifier|*
+name|fn
+function_decl|)
+parameter_list|(
+name|int
+parameter_list|,
+specifier|const
+name|char
+modifier|*
+modifier|*
+parameter_list|,
+specifier|const
+name|char
+modifier|*
+parameter_list|)
+function_decl|;
+block|}
+struct|;
+end_struct
+
+begin_decl_stmt
+DECL|variable|commands
+specifier|static
+name|struct
+name|cmd_struct
+name|commands
+index|[]
+init|=
+block|{
+block|{
+literal|"list"
+block|,
+name|module_list
+block|}
+block|, }
+decl_stmt|;
+end_decl_stmt
+
+begin_function
+DECL|function|cmd_submodule__helper
+name|int
+name|cmd_submodule__helper
+parameter_list|(
+name|int
+name|argc
+parameter_list|,
+specifier|const
+name|char
+modifier|*
+modifier|*
+name|argv
+parameter_list|,
+specifier|const
+name|char
+modifier|*
+name|prefix
+parameter_list|)
+block|{
+name|int
+name|i
+decl_stmt|;
+if|if
+condition|(
+name|argc
+operator|<
+literal|2
+condition|)
+name|die
+argument_list|(
+name|_
+argument_list|(
+literal|"fatal: submodule--helper subcommand must be "
+literal|"called with a subcommand"
+argument_list|)
+argument_list|)
+expr_stmt|;
+for|for
+control|(
+name|i
+operator|=
+literal|0
+init|;
+name|i
+operator|<
+name|ARRAY_SIZE
+argument_list|(
+name|commands
+argument_list|)
+condition|;
+name|i
+operator|++
+control|)
+if|if
+condition|(
+operator|!
+name|strcmp
+argument_list|(
+name|argv
+index|[
+literal|1
+index|]
+argument_list|,
+name|commands
+index|[
+name|i
+index|]
+operator|.
+name|cmd
+argument_list|)
+condition|)
+return|return
+name|commands
+index|[
+name|i
+index|]
+operator|.
+name|fn
+argument_list|(
+name|argc
+operator|-
+literal|1
+argument_list|,
+name|argv
+operator|+
+literal|1
+argument_list|,
+name|prefix
+argument_list|)
+return|;
+name|die
+argument_list|(
+name|_
+argument_list|(
+literal|"fatal: '%s' is not a valid submodule--helper "
+literal|"subcommand"
+argument_list|)
+argument_list|,
+name|argv
+index|[
+literal|1
+index|]
+argument_list|)
+expr_stmt|;
+block|}
+end_function
+
+end_unit
+

.gitignore
@@ -155,6 +155,7 @@
 /git-status
 /git-stripspace
 /git-submodule
+/git-submodule--helper
 /git-svn
 /git-symbolic-ref
 /git-tag

Makefile
@@ -901,6 +901,7 @@ BUILTIN_OBJS += builtin/shortlog.o
 BUILTIN_OBJS += builtin/show-branch.o
 BUILTIN_OBJS += builtin/show-ref.o
 BUILTIN_OBJS += builtin/stripspace.o
+BUILTIN_OBJS += builtin/submodule--helper.o
 BUILTIN_OBJS += builtin/symbolic-ref.o
 BUILTIN_OBJS += builtin/tag.o
 BUILTIN_OBJS += builtin/unpack-file.o

builtin.h
@@ -2165,6 +2165,28 @@ end_function_decl
 begin_function_decl
 specifier|extern
 name|int
+name|cmd_submodule__helper
+parameter_list|(
+name|int
+name|argc
+parameter_list|,
+specifier|const
+name|char
+modifier|*
+modifier|*
+name|argv
+parameter_list|,
+specifier|const
+name|char
+modifier|*
+name|prefix
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
+specifier|extern
+name|int
 name|cmd_symbolic_ref
 parameter_list|(
 name|int

git-submodule.sh
@@ -145,48 +145,6 @@ relative_path ()
 	echo "$result$target"
 }
 
-#
-# Get submodule info for registered submodules
-# $@ = path to limit submodule list
-#
-module_list()
-{
-	eval "set $(git rev-parse --sq --prefix "$wt_prefix" -- "$@")"
-	(
-		git ls-files -z --error-unmatch --stage -- "$@" ||
-		echo "unmatched pathspec exists"
-	) |
-	@@PERL@@ -e '
-	my %unmerged = ();
-	my ($null_sha1) = ("0" x 40);
-	my @out = ();
-	my $unmatched = 0;
-	$/ = "\0";
-	while (<STDIN>) {
-		if (/^unmatched pathspec/) {
-			$unmatched = 1;
-			next;
-		}
-		chomp;
-		my ($mode, $sha1, $stage, $path) =
-			/^([0-7]+) ([0-9a-f]{40}) ([0-3])\t(.*)$/;
-		next unless $mode eq "160000";
-		if ($stage ne "0") {
-			if (!$unmerged{$path}++) {
-				push @out, "$mode $null_sha1 U\t$path\n";
-			}
-			next;
-		}
-		push @out, "$_\n";
-	}
-	if ($unmatched) {
-		print "#unmatched\n";
-	} else {
-		print for (@out);
-	}
-	'
-}
-
 die_if_unmatched ()
 {
 	if test "$1" = "#unmatched"
@@ -532,7 +490,7 @@ cmd_foreach()
 	# command in the subshell (and a recursive call to this function)
 	exec 3<&0
 
-	module_list |
+	git submodule--helper list --prefix "$wt_prefix"|
 	while read mode sha1 stage sm_path
 	do
 		die_if_unmatched "$mode"
@@ -592,7 +550,7 @@ cmd_init()
 		shift
 	done
 
-	module_list "$@" |
+	git submodule--helper list --prefix "$wt_prefix" "$@" |
 	while read mode sha1 stage sm_path
 	do
 		die_if_unmatched "$mode"
@@ -674,7 +632,7 @@ cmd_deinit()
 		die "$(eval_gettext "Use '.' if you really want to deinitialize all submodules")"
 	fi
 
-	module_list "$@" |
+	git submodule--helper list --prefix "$wt_prefix" "$@" |
 	while read mode sha1 stage sm_path
 	do
 		die_if_unmatched "$mode"
@@ -790,7 +748,7 @@ cmd_update()
 	fi
 
 	cloned_modules=
-	module_list "$@" | {
+	git submodule--helper list --prefix "$wt_prefix" "$@" | {
 	err=
 	while read mode sha1 stage sm_path
 	do
@@ -1222,7 +1180,7 @@ cmd_status()
 		shift
 	done
 
-	module_list "$@" |
+	git submodule--helper list --prefix "$wt_prefix" "$@" |
 	while read mode sha1 stage sm_path
 	do
 		die_if_unmatched "$mode"
@@ -1299,7 +1257,7 @@ cmd_sync()
 		esac
 	done
 	cd_to_toplevel
-	module_list "$@" |
+	git submodule--helper list --prefix "$wt_prefix" "$@" |
 	while read mode sha1 stage sm_path
 	do
 		die_if_unmatched "$mode"

git.c
@@ -2963,6 +2963,14 @@ name|cmd_stripspace
 block|}
 block|,
 block|{
+literal|"submodule--helper"
+block|,
+name|cmd_submodule__helper
+block|,
+name|RUN_SETUP
+block|}
+block|,
+block|{
 literal|"symbolic-ref"
 block|,
 name|cmd_symbolic_ref

commit 123e626137f0e2b7f6341a0f87ee759a26d0e742

submodule: fix regression for deinit without submodules
Per Cederqvist wrote:
> It used to be possible to run
>
>    git submodule deinit -f .
>
> to remove any submodules, no matter how many submodules you had.  That
> is no longer possible in projects that don't have any submodules at
> all.  The command will fail with:
>
>     error: pathspec '.' did not match any file(s) known to git.

This regression was introduced in 98fae13eb6df [formerly 74703a1e4dfc] (submodule: rewrite
`module_list` shell function in C, 2015-09-02), as we changed the
order of checking in new module listing to first check whether it is
a gitlin before feeding it to match_pathspec().  It used to be that
a pathspec that does not match any path were diagnosed as an error,
but the new code complains for a pathspec that does not match any
submodule path.

Arguably the new behaviour may give us a better diagnosis, but that
is inconsistent with the suggestion "deinit" gives, and also this
was an unintended accident.  The new behaviour hopefully can be
redesigned and implemented better in future releases, but for now,
switch these two checks to restore the same behavior as before.  In
an empty repository, giving the pathspec '.' will still get the same
"did not match" error, but that is the same bug we had before 1.7.0.

Reported-by: Per Cederqvist <cederp@opera.com>
Signed-off-by: Stefan Beller <sbeller@google.com>
Signed-off-by: Junio C Hamano <gitster@pobox.com>

Former-commit-id: 84ba959bbdf083ff7036619fa08152e46b9473a5

builtin/submodule--helper.c
@@ -216,14 +216,6 @@ decl_stmt|;
 if|if
 condition|(
 operator|!
-name|S_ISGITLINK
-argument_list|(
-name|ce
-operator|->
-name|ce_mode
-argument_list|)
-operator|||
-operator|!
 name|match_pathspec
 argument_list|(
 name|pathspec
@@ -242,6 +234,14 @@ argument_list|,
 name|ps_matched
 argument_list|,
 literal|1
+argument_list|)
+operator|||
+operator|!
+name|S_ISGITLINK
+argument_list|(
+name|ce
+operator|->
+name|ce_mode
 argument_list|)
 condition|)
 continue|continue;

t/t7400-submodule-basic.sh
@@ -849,6 +849,19 @@ test_expect_success 'set up a second submodule' '
 	git commit -m "submodule example2 added"
 '
 
+test_expect_success 'submodule deinit works on repository without submodules' '
+	test_when_finished "rm -rf newdirectory" &&
+	mkdir newdirectory &&
+	(
+		cd newdirectory &&
+		git init &&
+		>file &&
+		git add file &&
+		git commit -m "repo should not be empty"
+		git submodule deinit .
+	)
+'
+
 test_expect_success 'submodule deinit should remove the whole submodule section from .git/config' '
 	git config submodule.example.foo bar &&
 	git config submodule.example2.frotz nitfol &&