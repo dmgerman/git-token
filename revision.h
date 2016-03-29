09db7d423d26004085206361826d911ef7843bdd
Junio C Hamano <gitster@pobox.com>
2016-03-29 15:49:24 -0700

    pretty: enable --expand-tabs by default for selected pretty formats
    "git log --pretty={medium,full,fuller}" and "git log" by default
    prepend 4 spaces to the log message, so it makes sense to enable
    the new "expand-tabs" facility by default for these formats.
    Add --no-expand-tabs option to override the new default.
    
    The change alone breaks a test in t4201 that runs "git shortlog"
    on the output from "git log", and expects that the output from
    "git log" does not do such a tab expansion.  Adjust the test to
    explicitly disable expand-tabs with --no-expand-tabs.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 0893eec85fca0f76039a96cbbcd3592ff8571c24

--- revision.h
+++ revision.h
@@ -627,11 +627,6 @@ name|pretty_given
 range|:
 literal|1
 decl_stmt|,
-DECL|member|expand_tabs_in_log
-name|expand_tabs_in_log
-range|:
-literal|1
-decl_stmt|,
 DECL|member|abbrev_commit
 name|abbrev_commit
 range|:
@@ -703,6 +698,15 @@ DECL|member|date_mode
 name|struct
 name|date_mode
 name|date_mode
+decl_stmt|;
+DECL|member|expand_tabs_in_log
+name|int
+name|expand_tabs_in_log
+decl_stmt|;
+comment|/* unset if negative */
+DECL|member|expand_tabs_in_log_default
+name|int
+name|expand_tabs_in_log_default
 decl_stmt|;
 DECL|member|abbrev
 name|unsigned
