513f4e15d5a391d463f5b6a6179b657fdeadc249
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2016-05-08 16:47:41 +0700

    credential-cache--daemon.c: use warning_errno()
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 26604f9f621b7b5710309671168d98722b8f5f33

--- credential-cache--daemon.c
+++ credential-cache--daemon.c
@@ -934,14 +934,9 @@ operator|<
 literal|0
 condition|)
 block|{
-name|warning
+name|warning_errno
 argument_list|(
-literal|"accept failed: %s"
-argument_list|,
-name|strerror
-argument_list|(
-name|errno
-argument_list|)
+literal|"accept failed"
 argument_list|)
 expr_stmt|;
 return|return
@@ -962,14 +957,9 @@ operator|<
 literal|0
 condition|)
 block|{
-name|warning
+name|warning_errno
 argument_list|(
-literal|"dup failed: %s"
-argument_list|,
-name|strerror
-argument_list|(
-name|errno
-argument_list|)
+literal|"dup failed"
 argument_list|)
 expr_stmt|;
 name|close
