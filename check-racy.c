e260d5a37a47ad7d124686fb983bbfde17db31a3
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2016-05-08 16:47:35 +0700

    check-racy.c: use error_errno()
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: eb031a580163b396ff0e4ee0f219758b517380d5

--- check-racy.c
+++ check-racy.c
@@ -81,18 +81,13 @@ name|st
 argument_list|)
 condition|)
 block|{
-name|error
+name|error_errno
 argument_list|(
-literal|"lstat(%s): %s"
+literal|"lstat(%s)"
 argument_list|,
 name|ce
 operator|->
 name|name
-argument_list|,
-name|strerror
-argument_list|(
-name|errno
-argument_list|)
 argument_list|)
 expr_stmt|;
 continue|continue;
