e46d6697f6ef4e952a581a18545eb12ad8a7f097
Christian Couder <christian.couder@gmail.com>
2016-05-24 10:10:48 +0200

    builtin/apply: move 'check' global into 'struct apply_state'
    To libify the apply functionality the 'check' variable should
    not be static and global to the file. Let's move it into
    'struct apply_state'.
    
    Reviewed-by: Stefan Beller <sbeller@google.com>
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 22a7233584b1983e2a7337d3b0f2c13f0cc34651

--- builtin/apply.c
+++ builtin/apply.c
@@ -102,6 +102,12 @@ DECL|member|prefix_length
 name|int
 name|prefix_length
 decl_stmt|;
+comment|/* These control what gets looked at and modified */
+DECL|member|check
+name|int
+name|check
+decl_stmt|;
+comment|/* preimage must match working tree, don't actually apply */
 comment|/* These boolean parameters control how the apply is done */
 DECL|member|unidiff_zero
 name|int
@@ -112,7 +118,7 @@ struct|;
 end_struct
 
 begin_comment
-comment|/*  *  --check turns on checking that the working tree matches the  *    files that are being modified, but doesn't apply the patch  *  --stat does just a diffstat, and doesn't actually apply  *  --numstat does numeric diffstat, and doesn't actually apply  *  --index-info shows the old and new index info for paths if available.  *  --index updates the cache as well.  *  --cached updates only the cache without ever touching the working tree.  */
+comment|/*  *  --stat does just a diffstat, and doesn't actually apply  *  --numstat does numeric diffstat, and doesn't actually apply  *  --index-info shows the old and new index info for paths if available.  *  --index updates the cache as well.  *  --cached updates only the cache without ever touching the working tree.  */
 end_comment
 
 begin_decl_stmt
@@ -189,14 +195,6 @@ DECL|variable|summary
 specifier|static
 name|int
 name|summary
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-DECL|variable|check
-specifier|static
-name|int
-name|check
 decl_stmt|;
 end_decl_stmt
 
@@ -10180,6 +10178,8 @@ condition|(
 operator|(
 name|apply
 operator|||
+name|state
+operator|->
 name|check
 operator|)
 operator|&&
@@ -21806,6 +21806,8 @@ block|}
 if|if
 condition|(
 operator|(
+name|state
+operator|->
 name|check
 operator|||
 name|apply
@@ -22510,6 +22512,8 @@ argument_list|,
 literal|"check"
 argument_list|,
 operator|&
+name|state
+operator|.
 name|check
 argument_list|,
 name|N_
@@ -22936,6 +22940,8 @@ name|numstat
 operator|||
 name|summary
 operator|||
+name|state
+operator|.
 name|check
 operator|||
 name|fake_ancestor
