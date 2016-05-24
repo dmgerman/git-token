4d37022d37cd656234017467464ed57149cbaafb
Christian Couder <christian.couder@gmail.com>
2016-05-24 10:10:57 +0200

    builtin/apply: move 'numstat' global into 'struct apply_state'
    To libify the apply functionality the 'numstat' variable should
    not be static and global to the file. Let's move it into
    'struct apply_state'.
    
    Reviewed-by: Stefan Beller <sbeller@google.com>
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: 179070b91c0ae1baf889f083596398a4f72286ea

--- builtin/apply.c
+++ builtin/apply.c
@@ -129,6 +129,11 @@ name|int
 name|diffstat
 decl_stmt|;
 comment|/* just show a diffstat, and don't actually apply */
+DECL|member|numstat
+name|int
+name|numstat
+decl_stmt|;
+comment|/* just show a numeric diffstat, and don't actually apply */
 comment|/* These boolean parameters control how the apply is done */
 DECL|member|allow_overlap
 name|int
@@ -155,7 +160,7 @@ struct|;
 end_struct
 
 begin_comment
-comment|/*  *  --numstat does numeric diffstat, and doesn't actually apply  *  --index-info shows the old and new index info for paths if available.  */
+comment|/*  *  --index-info shows the old and new index info for paths if available.  */
 end_comment
 
 begin_decl_stmt
@@ -184,14 +189,6 @@ DECL|variable|p_value_known
 specifier|static
 name|int
 name|p_value_known
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-DECL|variable|numstat
-specifier|static
-name|int
-name|numstat
 decl_stmt|;
 end_decl_stmt
 
@@ -22073,6 +22070,8 @@ argument_list|)
 expr_stmt|;
 if|if
 condition|(
+name|state
+operator|->
 name|numstat
 condition|)
 name|numstat_patch_list
@@ -22681,6 +22680,8 @@ argument_list|,
 literal|"numstat"
 argument_list|,
 operator|&
+name|state
+operator|.
 name|numstat
 argument_list|,
 name|N_
@@ -23159,6 +23160,8 @@ name|state
 operator|.
 name|diffstat
 operator|||
+name|state
+operator|.
 name|numstat
 operator|||
 name|summary
