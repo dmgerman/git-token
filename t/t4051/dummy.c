0156e5c07e365154881144f78ab9462a3d4909a7
René Scharfe <l.s.r@web.de>
2016-05-31 22:00:38 +0200

    t4051: rewrite, add more tests
    Remove the tests that checked against a fixed result and replace them
    with more focused checks of desired properties of the created diffs.
    That way we get more detailed and meaningful diagnostics.
    
    Store test file contents in files in a subdirectory in order to avoid
    cluttering the test script with them.
    
    Use tagged commits to store the changes to test diff -W against instead
    of using changes to the worktree.  Use the worktree instead to try and
    apply the generated patch in order to validate it.
    
    Document unwanted features: trailing empty lines, too much context for
    appended functions, insufficient context at the end with -U0.
    
    Helped-by: Junio C Hamano <gitster@pobox.com>
    Signed-off-by: Rene Scharfe <l.s.r@web.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: d3621de789ab57739f48b065751089d828b50240

---
+++ t/t4051/dummy.c
@@ -0,0 +1,28 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_function
+DECL|function|dummy
+specifier|static
+name|int
+name|dummy
+parameter_list|(
+name|void
+parameter_list|)
+comment|// Begin of dummy
+block|{
+name|int
+name|rc
+init|=
+literal|0
+decl_stmt|;
+return|return
+name|rc
+return|;
+block|}
+end_function
+
+begin_comment
+comment|// End of dummy
+end_comment
+
+end_unit
+
