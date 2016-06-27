dmgerman/git-token

commit d3c15793e3cb9ef04529d19725f20949083385f4

xread: poll on non blocking fds
The man page of read(2) says:

  EAGAIN The file descriptor fd refers to a file other than a socket
	 and has been marked nonblocking (O_NONBLOCK), and the read
	 would block.

  EAGAIN or EWOULDBLOCK
	 The file descriptor fd refers to a socket and has been marked
	 nonblocking (O_NONBLOCK), and the read would block.  POSIX.1-2001
	 allows either error to be returned for this case, and does not
	 require these constants to have the same value, so a portable
	 application should check for both possibilities.

If we get an EAGAIN or EWOULDBLOCK the fd must have set O_NONBLOCK.
As the intent of xread is to read as much as possible either until the
fd is EOF or an actual error occurs, we can ease the feeder of the fd
by not spinning the whole time, but rather wait for it politely by not
busy waiting.

We should not care if the call to poll failed, as we're in an infinite
loop and can only get out with the correct read().

Signed-off-by: Stefan Beller <sbeller@google.com>
Acked-by: Johannes Sixt <j6t@kdbg.org>
Signed-off-by: Junio C Hamano <gitster@pobox.com>

Former-commit-id: 1079c4be0b72003668df647f8a520fa137c7e158

wrapper.c
@@ -1093,23 +1093,59 @@ argument_list|)
 expr_stmt|;
 if|if
 condition|(
-operator|(
 name|nr
 operator|<
 literal|0
-operator|)
-operator|&&
-operator|(
+condition|)
+block|{
+if|if
+condition|(
+name|errno
+operator|==
+name|EINTR
+condition|)
+continue|continue;
+if|if
+condition|(
 name|errno
 operator|==
 name|EAGAIN
 operator|||
 name|errno
 operator|==
-name|EINTR
-operator|)
+name|EWOULDBLOCK
 condition|)
-continue|continue;
+block|{
+name|struct
+name|pollfd
+name|pfd
+decl_stmt|;
+name|pfd
+operator|.
+name|events
+operator|=
+name|POLLIN
+expr_stmt|;
+name|pfd
+operator|.
+name|fd
+operator|=
+name|fd
+expr_stmt|;
+comment|/* 				 * it is OK if this poll() failed; we 				 * want to leave this infinite loop 				 * only when read() returns with 				 * success, or an expected failure, 				 * which would be checked by the next 				 * call to read(2). 				 */
+name|poll
+argument_list|(
+operator|&
+name|pfd
+argument_list|,
+literal|1
+argument_list|,
+operator|-
+literal|1
+argument_list|)
+expr_stmt|;
+block|}
+block|}
 return|return
 name|nr
 return|;

commit dfa1576ffd927572cff5ce63b8e26552ce15bd3e

xread: retry after poll on EAGAIN/EWOULDBLOCK
We should continue to loop after EAGAIN/EWOULDBLOCK as the
intent of xread is to try until there is available data,
EOF, or an unrecoverable error.

Fixes: d3c15793e3cb9 [formerly 1079c4be0b720] ("xread: poll on non blocking fds")

Signed-off-by: Eric Wong <e@80x24.org>
Signed-off-by: Junio C Hamano <gitster@pobox.com>

Former-commit-id: c22f6202052ca84c68df4fbb16e42c826d429558

wrapper.c
@@ -1144,6 +1144,7 @@ operator|-
 literal|1
 argument_list|)
 expr_stmt|;
+continue|continue;
 block|}
 block|}
 return|return