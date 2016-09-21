ab453a59b311bbed02956ccf60a155fde858e4cc
Johannes Schindelin <johannes.schindelin@gmx.de>
2016-09-21 20:24:14 +0200

    regex: use regexec_buf()
    The new regexec_buf() function operates on buffers with an explicitly
    specified length, rather than NUL-terminated strings.
    
    We need to use this function whenever the buffer we want to pass to
    regexec(3) may have been mmap(2)ed (and is hence not NUL-terminated).
    
    Note: the original motivation for this patch was to fix a bug where
    `git diff -G <regex>` would crash. This patch converts more callers,
    though, some of which allocated to construct NUL-terminated strings,
    or worse, modified buffers to temporarily insert NULs while calling
    regexec(3).  By converting them to use regexec_buf(), the code has
    become much cleaner.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    
    Former-commit-id: b7d36ffca02c23f545d6e098d78180e6e72dfd8d

--- diff.c
+++ diff.c
@@ -5519,7 +5519,7 @@ decl_stmt|;
 if|if
 condition|(
 operator|!
-name|regexec
+name|regexec_buf
 argument_list|(
 name|word_regex
 argument_list|,
@@ -5527,6 +5527,13 @@ name|buffer
 operator|->
 name|ptr
 operator|+
+operator|*
+name|begin
+argument_list|,
+name|buffer
+operator|->
+name|size
+operator|-
 operator|*
 name|begin
 argument_list|,
